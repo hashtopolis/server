1fa80e84a844f50ac42c54d2cbc12e8ff4a6b01a
s3inlc <s3inlc@hashes.org>
2016-03-02 15:56:35 +0100

    index page added

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <?php
+use Bricky\Template;
 require_once(dirname(__FILE__)."/inc/load.php");
+
+//manually force the system to think it is installed
+//this should be removed in release!!!
+$INSTALL = 'DONE';
 
 if($INSTALL != 'DONE'){
 	header("Location: install/");
 	die("Forward to <a href='install/'>Install</a>");
 }
 
+$TEMPLATE = new Template("index");
 
+echo $TEMPLATE->render($OBJECTS);
 
 
 
