75c29e56936634147add1fafed15af4a618ef64b
s3inlc <s3inlc@hashes.org>
2016-03-03 21:37:01 +0100

    test

--- hashlists.php
+++ hashlists.php
@@ -76,8 +76,7 @@ if(isset($_POST['action'])){
 						$total=0;
 			
 						// create temporary hell to handle all that crack/crap
-						$FACTORIES::getagentsFactory()->getDB()->query("CREATE TEMPORARY TABLE tmphlcracks (hashlist INT NOT NULL, zaps BIT(1) DEFAULT 0, PRIMARY KEY (hashlist))");
-						print_r($FACTORIES::getagentsFactory()->getDB()->errorInfo());
+						$FACTORIES::getagentsFactory()->getDB()->query("CREATE TEMPORARY TABLE tmphlcracks (hashlist INT NOT NULL, zaps TINYINT DEFAULT 0, PRIMARY KEY (hashlist))");
 						$FACTORIES::getagentsFactory()->getDB()->query("INSERT INTO tmphlcracks (hashlist) SELECT id FROM hashlists WHERE id IN ($hlisty)");
 			
 			
