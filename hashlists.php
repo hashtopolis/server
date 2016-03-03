68728b22c1c1ca53fdcb4fa8f30ea8a0b54c598b
s3inlc <s3inlc@hashes.org>
2016-03-03 19:48:33 +0100

    added hash list zap

--- hashlists.php
+++ hashlists.php
@@ -9,7 +9,114 @@ $message = "";
 //catch agents actions here...
 if(isset($_POST['action'])){
 	switch($_POST['action']){
-		//TODO:
+		case 'export':
+			break;
+		case 'hashlistzap':
+			$hlist = intval($_POST["hashlist"]);
+			$res = $FACTORIES::getagentsFactory()->getDB()->query("SELECT hashlists.*,IFNULL(hashes.salted,0) AS salted FROM hashlists LEFT JOIN (SELECT hashlist,1 AS salted FROM hashes WHERE hashlist=$hlist AND salt!='' LIMIT 1) hashes ON hashlists.format=0 AND hashes.hashlist=hashlists.id WHERE hashlists.id=$hlist");
+			$list = $res->fetch();
+			if($list){
+				$listSet = new DataSet();
+				$listSet->setValues($list);
+				$OBJECTS['list'] = $listSet;
+				$OBJECTS['zap'] = true;
+				$impfiles = array();
+        		if(file_exists("import") && is_dir("import")){
+			        $impdir = opendir("import");
+			        $impfiles = array();
+			        while($f=readdir($impdir)){
+			        	if (($f!=".") && ($f!="..") && (!is_dir($f))) {
+			        		$impfiles[] = $f;
+			        	}
+			        }
+			        $OBJECTS['impfiles'] = $impfiles;
+        		}
+			}
+			else{
+				$message = "<div class='alert alert-danger'>Invalid hashlist!</div>";
+			}
+			break;
+		case 'hashlistdelete':
+			// delete hashlist
+			$message = "<div class='alert alert-neutral'>";
+			$hlist = intval($_POST["hashlist"]);
+			$FACTORIES::getagentsFactory()->getDB()->exec("START TRANSACTION");
+			$res = $FACTORIES::getagentsFactory()->getDB()->query("SELECT hashlists.format,hashlists.hashcount FROM hashlists WHERE hashlists.id=$hlist");
+			$list = $res->fetch();
+			$hcount = $list["hashcount"];
+			
+			// decrease supercount by that of deleted hashlist
+			$ans0 = $FACTORIES::getagentsFactory()->getDB()->exec("UPDATE hashlists JOIN superhashlists ON superhashlists.id=hashlists.id AND hashlists.format=3 AND superhashlists.hashlist=$hlist JOIN hashlists hashlists2 ON hashlists2.id=superhashlists.hashlist SET hashlists.cracked=hashlists.cracked-hashlists2.cracked,hashlists.hashcount=hashlists.hashcount-hashlists2.hashcount");
+			
+			// then actually delete the list
+			$ans1 = $ans0 && $FACTORIES::getagentsFactory()->getDB()->exec("DELETE FROM hashlists WHERE id=$hlist");
+			$ans2 = $ans1 && $FACTORIES::getagentsFactory()->getDB()->exec("DELETE FROM hashlistusers WHERE hashlist=$hlist");
+			$ans3 = $ans2 && $FACTORIES::getagentsFactory()->getDB()->exec("DELETE FROM zapqueue WHERE hashlist=$hlist");
+			
+			// and its tasks
+			$ans4 = $ans3 && $FACTORIES::getagentsFactory()->getDB()->exec("DELETE FROM taskfiles WHERE task IN (SELECT id FROM tasks WHERE hashlist=$hlist)");
+			$ans5 = $ans4 && $FACTORIES::getagentsFactory()->getDB()->exec("DELETE FROM assignments WHERE task IN (SELECT id FROM tasks WHERE hashlist=$hlist)");
+			$ans6 = $ans5 && $FACTORIES::getagentsFactory()->getDB()->exec("DELETE FROM chunks WHERE task IN (SELECT id FROM tasks WHERE hashlist=$hlist)");
+			$ans7 = $ans6 && $FACTORIES::getagentsFactory()->getDB()->exec("DELETE FROM tasks WHERE hashlist=$hlist");
+			
+			$ans8 = $ans7 && $FACTORIES::getagentsFactory()->getDB()->exec("DELETE FROM superhashlists WHERE hashlist=$hlist");
+			
+			if($ans8){
+				$FACTORIES::getagentsFactory()->getDB()->exec("COMMIT");
+				$message .= "Deleted hashlist and associated zaps.<br>";
+				switch($list["format"]) {
+					case 0:
+						$res = $FACTORIES::getagentsFactory()->getDB()->query("SELECT 1 FROM hashlists WHERE format=0");
+						if($res->rowCount() > 0){
+							$message .= "Deleting the actual rows (this is going to take A LONG TIME!)...<br>";
+							$hdelete = 0;
+							$kolik = 1;
+							$cas_pinfo = time();
+							$cas_start = time();
+							$FACTORIES::getagentsFactory()->getDB()->exec("START TRANSACTION");
+							while($kolik > 0) {
+								$kver = "DELETE FROM ".$formattables[$list["format"]]." WHERE hashlist=$hlist LIMIT 1000";
+								$ans1 = $FACTORIES::getagentsFactory()->getDB()->exec($kver);
+								$kolik = $ans1->rowCount();
+								$hdelete += $kolik;
+								if(time() >= $cas_pinfo + 10){
+									$message .= "Progress: $hdelete/$hcount, time spent: ".(time()-$cas_start)." sec<br>";
+									$FACTORIES::getagentsFactory()->getDB()->exec("COMMIT");
+									$FACTORIES::getagentsFactory()->getDB()->exec("START TRANSACTION");
+									$cas_pinfo = time();
+								}
+							}
+							$FACTORIES::getagentsFactory()->getDB()->exec("COMMIT");
+						} 
+						else {
+							$message .= "This was the last hashlist, truncating the table.";
+							$FACTORIES::getagentsFactory()->getDB()->exec("TRUNCATE TABLE ".$formattables[$list["format"]]);
+           	 			}
+           	 			header("Location: hashlists.php");
+           	 			die();
+						break;
+			
+					case 1:
+					case 2:
+            			$message .= "Deleting binary hashes...<br>";
+						$FACTORIES::getagentsFactory()->getDB()->exec("DELETE FROM hashes_binary WHERE hashlist=$hlist");
+						header("Location: hashlists.php");
+						die();
+						break;
+         	 		case 3:
+						$message .= "Deleting superhashlist links...<br>";
+						$FACTORIES::getagentsFactory()->getDB()->exec("DELETE FROM superhashlists WHERE id=$hlist");
+						header("Location: hashlists.php");
+						die();
+						break;
+				}
+			} 
+			else {
+        		$FACTORIES::getagentsFactory()->getDB()->exec("ROLLBACK");
+			    $message .= "Problems deleting hashlist!";
+			}
+			$message .= "</div>";
+			break;
 	}
 }
 
