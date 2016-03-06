da0c5bb0e728c9d072c0b38a5e7ccc158913949f
Sein Coray <s3inlc@hashes.org>
2016-03-06 21:23:34 +0100

    added hashcat releases

--- chunks.php
+++ chunks.php
@@ -4,14 +4,6 @@ require_once(dirname(__FILE__)."/inc/load.php");
 
 $TEMPLATE = new Template("chunks");
 $MENU->setActive("chunks");
-$message = "";
-
-//catch agents actions here...
-if(isset($_POST['action'])){
-	switch($_POST['action']){
-		//TODO:
-	}
-}
 
 $res = $FACTORIES::getagentsFactory()->getDB()->query("SELECT chunks.*,assignments.speed,GREATEST(chunks.dispatchtime,chunks.solvetime)-chunks.dispatchtime AS spent,agents.name AS aname,tasks.name AS tname FROM chunks JOIN tasks ON chunks.task=tasks.id LEFT JOIN agents ON chunks.agent=agents.id LEFT JOIN assignments ON assignments.task=tasks.id AND assignments.agent=agents.id ORDER BY chunks.dispatchtime DESC");
 $res = $res->fetchAll();
@@ -23,7 +15,6 @@ foreach($res as $chunk){
 }
 
 $OBJECTS['chunks'] = $chunks;
-$OBJECTS['message'] = $message;
 
 echo $TEMPLATE->render($OBJECTS);
 
