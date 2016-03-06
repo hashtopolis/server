b0862c253f461017f5eae98c59b52b28f41c4779
Sein Coray <s3inlc@hashes.org>
2016-03-06 21:27:08 +0100

    debug

--- inc/template.class.php
+++ inc/template.class.php
@@ -189,6 +189,7 @@ class Template {
 				$partial = $this->str_replace_first($matches[1][0], str_replace("\n", "---NEWLINE---", $val), $partial);
 			}
 			else{
+				echo $val;
 				$partial = $this->str_replace_first($matches[1][0], str_replace("\n", "---NEWLINE---", eval("return ".$val.";")), $partial);
 			}
 			preg_match_all("/^(?:(?!\{\{).)*?(\[\[(.*?)\]\])/is", $partial, $matches, PREG_PATTERN_ORDER);
