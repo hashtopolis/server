c2e2cb84f57c61ebe495dd85dd1b7ea17bd3a504
Sein Coray <s3inlc@hashes.org>
2016-03-06 21:52:48 +0100

    added hashcat creation

--- newhashcat.php
+++ newhashcat.php
@@ -9,7 +9,42 @@ $message = "";
 //catch agents actions here...
 if(isset($_POST['action'])){
 	switch($_POST['action']){
-		//TODO:
+		case 'newhashcatp':
+			// new hashcat release creator
+			$DB = $FACTORIES::getagentsFactory()->getDB();
+			$message = "<div class='alert alert-neutral'>";
+			
+			$version = $DB->quote($_POST["version"]);
+			$url["1"] = $DB->quote($_POST["url_nvidia"]);
+			$url["2"] = $DB->quote($_POST["url_amd"]);
+			$common_files = $DB->quote($_POST["common_files"]);
+			$files["1"]["32"] = $DB->quote($_POST["32_nvidia"]);
+			$files["1"]["64"] = $DB->quote($_POST["64_nvidia"]);
+			$files["2"]["32"] = $DB->quote($_POST["32_amd"]);
+			$files["2"]["64"] = $DB->quote($_POST["64_amd"]);
+			$minver["1"] = floatval($_POST["minver_nvidia"]);
+			$minver["2"] = floatval($_POST["minver_amd"]);
+			$rootdir["1"] = $DB->quote($_POST["rootdir_nvidia"]);
+			$rootdir["2"] = $DB->quote($_POST["rootdir_amd"]);
+			if ($version == "") {
+				$message .= "You must specify the version";
+			} 
+			else {
+				$message .= "Creating release in the DB...";
+				$res = $DB->query("INSERT INTO hashcatreleases (version,time,url_nvidia,url_amd,common_files,32_nvidia,64_nvidia,32_amd,64_amd,rootdir_nvidia,rootdir_amd,minver_nvidia,minver_amd) VALUES ($version,".time().",".$url["1"].",".$url["2"].",$common_files,".$files["1"]["32"].",".$files["1"]["64"].",".$files["2"]["32"].",".$files["2"]["64"].",".$rootdir["1"].",".$rootdir["2"].",".$minver["1"].",".$minver["2"].")");
+				if ($res) {
+					// insert succeeded
+					$message .= "OK";
+					header("Location: hashcat.php");
+					die();
+				} 
+				else {
+					$message .= "ERROR: ".mysqli_error($dblink);
+				}
+				$message .= "<br>";
+			}
+			$message .= "</div>";
+			break;
 	}
 }
 
