1afec4624e5269acdd8c3cb59d001d431e0bf14e
Sein Coray <s3inlc@hashes.org>
2016-03-09 16:27:01 +0100

    implemented user account settings

--- account.php
+++ account.php
@@ -25,6 +25,42 @@ if(isset($_POST['action'])){
 			$FACTORIES::getUserFactory()->update($user);
 			header("Location: account.php");
 			die();
+		case 'updatelifetime':
+			$lifetime = intval($_POST['lifetime']);
+			if($lifetime < 60 || $lifetime > 24*3600){
+				$message = "<div class='alert alert-danger'>Lifetime must be larger than 1 minute and smaller than 2 days!</div>";
+				break;
+			}
+			$user = $LOGIN->getUser();
+			$user->setSessionLifetime($lifetime);
+			$FACTORIES::getUserFactory()->update($user);
+			header("Location: account.php");
+			die();
+		case 'changepass':
+			$oldpass = $_POST['oldpass'];
+			$newpass = $_POST['newpass'];
+			$reppass = $_POST['reppass'];
+			$user = $LOGIN->getUser();
+			if(!Encryption::passwordVerify($user->getUsername(), $oldpass, $user->getPasswordSalt(), $user->getPasswordHash())){
+				$message = "<div class='alert alert-danger'>Your old password is wrong!</div>";
+				break;
+			}
+			else if(strlen($newpass) < 4){
+				$message = "<div class='alert alert-danger'>Your password is too short!</div>";
+				break;
+			}
+			else if($newpass != $reppass){
+				$message = "<div class='alert alert-danger'>Your new passwords do not match!</div>";
+				break;
+			}
+			$newSalt = Util::randomString(20);
+			$newHash = Encryption::passwordHash($user->getUsername(), $newpass, $newSalt);
+			$user->setPasswordHash($newHash);
+			$user->setPasswordSalt($newSalt);
+			$user->setIsComputedPassword(0);
+			$FACTORIES::getUserFactory()->update($user);
+			header("Location: account.php");
+			die();
 	}
 }
 
