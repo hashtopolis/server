f66750370ab8e82f1b99c642f6a18a31da47cb22
Sein Coray <s3inlc@hashes.org>
2016-03-09 22:35:37 +0100

    added logout message

--- index.php
+++ index.php
@@ -7,13 +7,22 @@ if($INSTALL != 'DONE'){
 	die("Forward to <a href='install/'>Install</a>");
 }
 
+$TEMPLATE = new Template("index");
+$message = "";
+
 if(isset($_GET['err'])){
 	//TODO: handle error messages
 }
+else if(isset($_GET['logout'])){
+	$logout = $_GET['logout'];
+	$time = substr($logout, 1);
+	if(time() - $time < 10){
+		$message = "<div class='alert alert-success'>You logged out successfully!</div>";
+	}
+}
 
+$OBJECTS['message'] = $message;
 $OBJECTS['fw'] = @$_GET['fw'];
-
-$TEMPLATE = new Template("index");
 
 echo $TEMPLATE->render($OBJECTS);
 
