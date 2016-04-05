25a371c087935524b88238eb6ae72d1de100c4db
Sein Coray <s3inlc@hashes.org>
2016-04-05 18:59:52 +0200

    hashtypes completed

--- hashtypes.php
+++ hashtypes.php
@@ -18,7 +18,37 @@ $message = "";
 //catch agents actions here...
 if(isset($_POST['action'])){
 	switch($_POST['action']){
-		
+		case 'delete':
+			$id = $_POST['type'];
+			$res = $DB->query("SELECT * FROM hashtypes WHERE id=".$DB->quote($id));
+			$type = $res->fetch();
+			if(!$type){
+				$message = "<div class='alert alert-danger'>This hashtype does not exist!</div>";
+				break;
+			}
+			$res = $DB->query("SELECT * FROM hashlists WHERE hashtype=".$DB->quote($type['id']));
+			if($res->rowCount() > 0){
+				$message = "<div class='alert alert-danger'>You cannot delete this hashtype! There are hashlists present which are of this type.</div>";
+				break;
+			}
+			$DB->query("DELETE FROM hashtypes WHERE id=".$type['id']);
+			$message = "<div class='alert alert-success'>Hashtype was successfully deleted!</div>";
+			break;
+		case 'add':
+			$id = intval($_POST['id']);
+			$desc = htmlentities($_POST['description']);
+			$res = $DB->query("SELECT * FROM hashtypes WHERE id=".$DB->quote($id));
+			$test = $res->fetch();
+			if($test){
+				$message = "<div class='alert alert-danger'>This hashtype already exists!</div>";
+				break;
+			}
+			else if(strlen($desc) || $id < 0){
+				break;
+			}
+			$DB->query("INSERT INTO hashtypes (id, description) VALUES (".$DB->quote($id).", ".$DB->quote($desc).")");
+			$message = "<div class='alert alert-success'>New hashtype was added!</div>";
+			break;
 	}
 }
 
