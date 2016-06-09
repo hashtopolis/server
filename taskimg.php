410bca62926485eb0917beaf47985a9a49ac6b4a
Sein Coray <s3inlc@hashes.org>
2016-06-09 19:22:28 +0200

    debugging

--- taskimg.php
+++ taskimg.php
@@ -58,26 +58,33 @@ foreach($res as $chunk){
 	
 	if($end - $start < 3){
 		if($chunk['state'] >= 6){
+			echo "fillrectangle red: ".$start."-0:$end-".($size[1] - 1)."<br>\n";
 			imagefilledrectangle($image, $start, 0, $end, $size[1] - 1, $red);
 		}
 		else if($chunk['cracked'] > 0){
+			echo "fillrectangle green: ".$start."-0:$end-".($size[1] - 1)."<br>\n";
 			imagefilledrectangle($image, $start, 0, $end, $size[1] - 1, $green);
 		}
 		else{
+			echo "fillrectangle yellow: ".$start."-0:$end-".($size[1] - 1)."<br>\n";
 			imagefilledrectangle($image, $start, 0, $end, $size[1] - 1, $yellow);
 		}
 	}
 	else{
 		if($chunk['state'] >= 6){
+			echo "rectangle red: ".$start."-0:$end-".($size[1] - 1)."<br>\n";
 			imagerectangle($image, $start, 0, $end, ($size[1] - 1), $red);
 		}
 		else{
+			echo "rectangle grey: ".$start."-0:$end-".($size[1] - 1)."<br>\n";
 			imagerectangle($image, $start, 0, $end, ($size[1] - 1), $grey);
 		}
 		if($chunk['cracked'] > 0){
+			echo "fillrectangle green: ".($start + 1)."-1:".($current - 1)."-".($size[1] - 2)."<br>\n";
 			imagefilledrectangle($image, $start + 1, 1, $current - 1, $size[1] - 2, $green);
 		}
 		else{
+			echo "fillrectangle yellow: ".($start + 1)."-1:".($current - 1)."-".($size[1] - 2)."<br>\n";
 			imagefilledrectangle($image, $start + 1, 1, $current - 1, $size[1] - 2, $yellow);
 		}
 	}
