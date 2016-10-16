1008869fbf52d9c1848f30186cecdb902ab47d57
Sein Coray <sein.coray@gmail.com>
2016-10-16 20:18:31 +0200

    test

--- server.php
+++ server.php
@@ -2,7 +2,7 @@
 require_once(dirname(__FILE__)."/inc/load.php");
 set_time_limit(0);
 
-$QUERY = json_decode(@$_POST['query'], true);
+$QUERY = json_decode(@$_POST, true);
 header("Content-Type: application/json");
 
 switch($QUERY['action']){
