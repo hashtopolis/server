3a9f863ec151396691fd5f307bc467ad84ebc6b0
Sein Coray <sein.coray@gmail.com>
2016-11-29 15:30:21 +0100

    fixed small problem with hashlists downloading on the api added chunks view

--- src/agents.php
+++ src/agents.php
@@ -23,7 +23,6 @@ if (isset($_POST['action'])) {
 
 $allTasks = $FACTORIES::getTaskFactory()->filter(array());
 
-//TODO: put code for new agent here
 if (isset($_GET['id'])) {
   //show agent detail
   $TEMPLATE = new Template("agents/detail");
