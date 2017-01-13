0eee6f6f670265c16fb1fa10558e5f52aa59fbc6
Sein Coray <sein.coray@gmail.com>
2017-01-13 20:48:40 +0100

    updated comparison filter

--- src/dba/ComparisonFilter.class.php
+++ src/dba/ComparisonFilter.class.php
@@ -14,16 +14,26 @@ class ComparisonFilter extends Filter {
   private $key2;
   private $operator;
   
-  function __construct($key1, $key2, $operator) {
+  /**
+   * @var AbstractModelFactory
+   */
+  private $overrideFactory;
+  
+  function __construct($key1, $key2, $operator, $overrideFactory = null) {
     $this->key1 = $key1;
     $this->key2 = $key2;
     $this->operator = $operator;
+    $this->overrideFactory = $overrideFactory;
   }
   
   function getQueryString($table = "") {
     if ($table != "") {
       $table = $table . ".";
     }
+    if ($this->overrideFactory != null) {
+      $table = $this->overrideFactory->getModelTable() . ".";
+    }
+    
     return $table . $this->key1 . $this->operator . $table . $this->key2;
   }
   
