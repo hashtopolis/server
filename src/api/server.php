2c9d0f893d6a6539c5c036cf582ea01b905ca403
Sein Coray <sein.coray@gmail.com>
2017-01-23 19:36:15 +0100

    added more defines

--- src/api/server.php
+++ src/api/server.php
@@ -2,72 +2,72 @@
 require_once(dirname(__FILE__) . "/../inc/load.php");
 set_time_limit(0);
 
-$QUERY = json_decode(@$_POST['query'], true);
+$QUERY = json_decode(@$_POST[PQueryValues::QUERY], true);
 header("Content-Type: application/json");
 
 //debug logging
 //TODO: remove later
-file_put_contents("../query.log", Util::getIP() . "=" . $_POST['query'] . "\n", FILE_APPEND);
+file_put_contents("../query.log", Util::getIP() . "=" . $_POST[PQueryValues::QUERY] . "\n", FILE_APPEND);
 
-switch ($QUERY['action']) {
-  case "register":
+switch ($QUERY[PQueryValues::ACTION]) {
+  case PActions::REGISTER:
     API::registerAgent($QUERY);
     break;
-  case "login":
+  case PActions::LOGIN:
     API::loginAgent($QUERY);
     break;
-  case "update":
+  case PActions::UPDATE:
     API::checkClientUpdate($QUERY);
     break;
-  case "download":
+  case PActions::DOWNLOAD:
     if (API::checkToken($QUERY)) {
       API::sendErrorResponse('download', "Invalid token!");
     }
     API::downloadApp($QUERY);
     break;
-  case 'error':
+  case PActions::ERROR:
     if (API::checkToken($QUERY)) {
       API::sendErrorResponse('error', "Invalid token!");
     }
     API::agentError($QUERY);
     break;
-  case 'file':
+  case PActions::FILE:
     if (API::checkToken($QUERY)) {
       API::sendErrorResponse('file', "Invalid token!");
     }
     API::getFile($QUERY);
     break;
-  case "hashes":
+  case PActions::HASHES:
     if (API::checkToken($QUERY)) {
       API::sendErrorResponse('hashes', "Invalid token!");
     }
     API::getHashes($QUERY);
     break;
-  case "task":
+  case PActions::TASK:
     if (API::checkToken($QUERY)) {
       API::sendErrorResponse('task', "Invalid token!");
     }
     API::getTask($QUERY);
     break;
-  case "chunk":
+  case PActions::CHUNK:
     if (API::checkToken($QUERY)) {
       API::sendErrorResponse('task', "Invalid token!");
     }
     API::getChunk($QUERY);
     break;
-  case "keyspace":
+  case PActions::KEYSPACE:
     if (API::checkToken($QUERY)) {
       API::sendErrorResponse('task', "Invalid token!");
     }
     API::setKeyspace($QUERY);
     break;
-  case "bench":
+  case PActions::BENCHMARK:
     if (API::checkToken($QUERY)) {
       API::sendErrorResponse('bench', "Invalid token!");
     }
     API::setBenchmark($QUERY);
     break;
-  case "solve":
+  case PActions::SOLVE:
     if (API::checkToken($QUERY)) {
       API::sendErrorResponse('solve', "Invalid token!");
     }
