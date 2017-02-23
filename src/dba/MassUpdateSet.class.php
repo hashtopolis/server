1fe52eb47fc41ad24422343e96e498c175e1dfa8
Sein Coray <sein.coray@gmail.com>
2017-02-23 20:22:20 +0100

    updated as key is obsolete

--- src/dba/MassUpdateSet.class.php
+++ src/dba/MassUpdateSet.class.php
@@ -1,18 +1,16 @@
 <?php
+
 /**
  * Created by IntelliJ IDEA.
  * User: sein
  * Date: 23.02.17
  * Time: 20:11
  */
-
 class MassUpdateSet {
-  private $key;
   private $matchValue;
   private $updateValue;
   
-  function __construct($key, $matchValue, $updateValue) {
-    $this->key = $key;
+  function __construct($matchValue, $updateValue) {
     $this->matchValue = $matchValue;
     $this->updateValue = $updateValue;
   }
@@ -25,7 +23,7 @@ class MassUpdateSet {
     return $this->updateValue;
   }
   
-  function getMassQuery($table = ""){
-    return "WHEN ".$table. $this->key ." = ? THEN ?";
+  function getMassQuery($key) {
+    return "WHEN " . $key . " = ? THEN ?";
   }
 }
