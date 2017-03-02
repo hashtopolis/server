bef86168f2fd669945a7de9975b05385e8c080ec
Sein Coray <sein.coray@gmail.com>
2017-03-02 15:36:24 +0100

    added agent binary version 0.34

--- src/install/hashtopussy.sql
+++ src/install/hashtopussy.sql
@@ -461,7 +461,7 @@ INSERT INTO `RightGroup` (`rightGroupId`, `groupName`, `level`) VALUES
   (5, 'Administrator', 50);
 
 INSERT INTO `AgentBinary` (`agentBinaryId`, `type`, `operatingSystems`, `filename`, `version`)
-VALUES (1, 'csharp', 'Windows', 'hashtopussy.exe', '0.33');
+VALUES (1, 'csharp', 'Windows', 'hashtopussy.exe', '0.34');
 
 CREATE TABLE `Session` (
   `sessionId`        INT(11)      NOT NULL,
