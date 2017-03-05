cad764dea179b7366e34c863dba7e8721ade10e4
Sein Coray <sein.coray@gmail.com>
2017-03-05 14:45:59 +0100

    updated update script included checking if csharp version can be updated

--- src/install/updates/update_v0.2.0-beta_v0.2.0.php
+++ src/install/updates/update_v0.2.0-beta_v0.2.0.php
@@ -6,6 +6,28 @@
  * Time: 14:54
  */
 
+use DBA\AgentBinary;
+use DBA\QueryFilter;
+
 require_once(dirname(__FILE__) . "/../../inc/load.php");
 
+echo "Apply updates...\n";
+
+echo "Change logEntry level lengt... ";
 $FACTORIES::getAgentFactory()->getDB()->query("ALTER TABLE `LogEntry` CHANGE `level` `level` VARCHAR(20) NOT NULL");
+echo "OK\n";
+
+echo "Check csharp binary... ";
+$qF = new QueryFilter(AgentBinary::TYPE, "csharp", "=");
+$binary = $FACTORIES::getAgentBinaryFactory()->filter(array($FACTORIES::FILTER => $qF), true);
+if($binary != null){
+  if($binary->getVersion() < "0.37"){
+    echo "update version... ";
+    $binary->setVersion("0.37");
+    $FACTORIES::getAgentBinaryFactory()->update($binary);
+    echo "OK";
+  }
+}
+echo "\n";
+
+echo "Update complete!\n";
