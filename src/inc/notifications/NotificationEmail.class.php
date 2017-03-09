1e4a2a19d9664f1f60adcf9318878929e28edb2c
Sein Coray <sein.coray@gmail.com>
2017-03-09 22:50:24 +0100

    added email notification

---
+++ src/inc/notifications/NotificationEmail.class.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: sein
+ * Date: 09.03.17
+ * Time: 14:00
+ */
+class HashtopussyNotificationEmail extends HashtopussyNotification {
+  protected     $receiver;
+  public static $name = "Email";
+  
+  function getTemplateName() {
+    return "notifications/email";
+  }
+  
+  function getObjects() {
+    return array();
+  }
+  
+  function sendMessage($message) {
+    Util::sendMail($this->receiver, "Hashtopussy Notification", $message);
+  }
+}
+
+$NOTIFICATIONS['Email'] = new HashtopussyNotificationEmail();
+
