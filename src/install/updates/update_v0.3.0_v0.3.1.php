32a93e22cd7e1fb359ee0f7f6ef33fe4eaf6defb
Sein Coray <sein.coray@gmail.com>
2017-03-30 13:57:43 +0200

    client update to version 0.43.12

--- src/install/updates/update_v0.3.0_v0.3.1.php
+++ src/install/updates/update_v0.3.0_v0.3.1.php
@@ -15,9 +15,9 @@ echo "Check csharp binary... ";
 $qF = new QueryFilter(AgentBinary::TYPE, "csharp", "=");
 $binary = $FACTORIES::getAgentBinaryFactory()->filter(array($FACTORIES::FILTER => $qF), true);
 if($binary != null){
-  if(Util::versionComparison($binary->getVersion(), "0.43.8") == 1){
+  if(Util::versionComparison($binary->getVersion(), "0.43.12") == 1){
     echo "update version... ";
-    $binary->setVersion("0.43.8");
+    $binary->setVersion("0.43.12");
     $FACTORIES::getAgentBinaryFactory()->update($binary);
     echo "OK";
   }
