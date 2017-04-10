046fade8e8908f516ff326ee7d2f297b28bea8d8
Sein Coray <sein.coray@gmail.com>
2017-04-10 18:16:43 +0200

    added progress image for supertask

--- src/api/taskimg.php
+++ src/api/taskimg.php
@@ -28,7 +28,6 @@ if ($task == null) {
   die("Not a valid task!");
 }
 
-
 //create image
 $image = imagecreatetruecolor($size[0], $size[1]);
 imagesavealpha($image, true);
@@ -47,43 +46,61 @@ $progress = $task->getProgress();
 $keyspace = max($task->getKeyspace(), 1);
 $taskid = $task->getId();
 
+if ($task->getTaskType() == DTaskTypes::SUPERTASK) {
+  // handle supertask progress drawing here
+  $subTasks = Util::getSubTasks($task);
+  $numTasks = sizeof($subTasks);
+  for($i=0;$i<sizeof($subTasks);$i++){
+    $qF = new QueryFilter(Chunk::TASK_ID, $subTasks[$i]->getId(), "=");
+    $chunks = $FACTORIES::getChunkFactory()->filter(array($FACTORIES::FILTER => $qF));
+    $progress = 0;
+    foreach($chunks as $chunk){
+      $progress += $chunk->getProgress();
+    }
+    if($subTasks[$i]->getKeyspace() > 0 && $progress >= $subTasks[$i]->getKeyspace()) {
+      imagefilledrectangle($image, $i * $size[0] / $numTasks, 0, ($i + 1) * $size[0] / $numTasks, $size[1] - 1, $green);
+    }
+  }
+}
+else {
 //load chunks
-$qF = new QueryFilter(Task::TASK_ID, $task->getId(), "=");
-$chunks = $FACTORIES::getChunkFactory()->filter(array($FACTORIES::FILTER => $qF));
-foreach ($chunks as $chunk) {
-  $chunk = Util::cast($chunk, Chunk::class);
-  $start = floor(($size[0] - 1) * $chunk->getSkip() / $keyspace);
-  $end = floor(($size[0] - 1) * ($chunk->getSkip() + $chunk->getLength()) / $keyspace) - 1;
-  //division by 10000 is required because rprogress is saved in percents with two decimals
-  $current = floor(($size[0] - 1) * ($chunk->getSkip() + $chunk->getLength() * $chunk->getRprogress() / 10000) / $keyspace) - 1;
-  
-  if ($current > $end) {
-    $current = $end;
-  }
-  
-  if ($end - $start < 3) {
-    if ($chunk->getState() >= 6) {
-      imagefilledrectangle($image, $start, 0, $end, $size[1] - 1, $red);
+  $qF = new QueryFilter(Task::TASK_ID, $task->getId(), "=");
+  $chunks = $FACTORIES::getChunkFactory()->filter(array($FACTORIES::FILTER => $qF));
+  foreach ($chunks as $chunk) {
+    $chunk = Util::cast($chunk, Chunk::class);
+    $start = floor(($size[0] - 1) * $chunk->getSkip() / $keyspace);
+    $end = floor(($size[0] - 1) * ($chunk->getSkip() + $chunk->getLength()) / $keyspace) - 1;
+    //division by 10000 is required because rprogress is saved in percents with two decimals
+    $current = floor(($size[0] - 1) * ($chunk->getSkip() + $chunk->getLength() * $chunk->getRprogress() / 10000) / $keyspace) - 1;
+    
+    if ($current > $end) {
+      $current = $end;
     }
-    else if ($chunk->getCracked() > 0) {
-      imagefilledrectangle($image, $start, 0, $end, $size[1] - 1, $green);
-    }
-    else {
-      imagefilledrectangle($image, $start, 0, $end, $size[1] - 1, $yellow);
-    }
-  }
-  else {
-    if ($chunk->getState() >= 6) {
-      imagerectangle($image, $start, 0, $end, ($size[1] - 1), $red);
+    
+    if ($end - $start < 3) {
+      if ($chunk->getState() >= 6) {
+        imagefilledrectangle($image, $start, 0, $end, $size[1] - 1, $red);
+      }
+      else if ($chunk->getCracked() > 0) {
+        imagefilledrectangle($image, $start, 0, $end, $size[1] - 1, $green);
+      }
+      else {
+        imagefilledrectangle($image, $start, 0, $end, $size[1] - 1, $yellow);
+      }
     }
     else {
-      imagerectangle($image, $start, 0, $end, ($size[1] - 1), $grey);
-    }
-    if ($chunk->getCracked() > 0) {
-      imagefilledrectangle($image, $start + 1, 1, $current - 1, $size[1] - 2, $green);
-    }
-    else {
-      imagefilledrectangle($image, $start + 1, 1, $current - 1, $size[1] - 2, $yellow);
+      if ($chunk->getState() >= 6) {
+        imagerectangle($image, $start, 0, $end, ($size[1] - 1), $red);
+      }
+      else {
+        imagerectangle($image, $start, 0, $end, ($size[1] - 1), $grey);
+      }
+      if ($chunk->getCracked() > 0) {
+        imagefilledrectangle($image, $start + 1, 1, $current - 1, $size[1] - 2, $green);
+      }
+      else {
+        imagefilledrectangle($image, $start + 1, 1, $current - 1, $size[1] - 2, $yellow);
+      }
     }
   }
 }
