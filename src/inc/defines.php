da207ce132ca89258a79e6aeeb414311abded880
Sein Coray <sein.coray@gmail.com>
2017-04-12 16:19:47 +0200

    added basic status page giving some info

--- src/inc/defines.php
+++ src/inc/defines.php
@@ -25,6 +25,16 @@ class DHashcatStatus {
   const STATUS_ABORTED_RUNTIME = 10;
 }
 
+class DStats {
+  const AGENTS_ONLINE      = "agentsOnline";
+  const AGENTS_ACTIVE      = "agentsActive";
+  const AGENTS_TOTAL_SPEED = "agentsTotalSpeed";
+  const TASKS_TOTAL        = "tasksTotal";
+  const TASKS_FINISHED     = "tasksFinished";
+  const TASKS_RUNNING      = "tasksRunning";
+  const TASKS_QUEUED       = "tasksQueued";
+}
+
 // operating systems
 class DOperatingSystem {
   const LINUX   = 0;
