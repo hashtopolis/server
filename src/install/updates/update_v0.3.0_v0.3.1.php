df50e096ca33fa8f68d7861c18d30f3f0546b591
Sein Coray <s3inlc@hashes.org>
2017-04-24 23:05:50 +0200

    completed update chain

---
+++ src/install/updates/update_v0.3.0_v0.3.1.php
@@ -0,0 +1,14 @@
+<?php
+
+use DBA\AgentBinary;
+use DBA\QueryFilter;
+
+require_once(dirname(__FILE__) . "/../../inc/load.php");
+
+echo "Apply updates...\n";
+
+echo "Update Zap table... ";
+$FACTORIES::getAgentFactory()->getDB()->query("ALTER TABLE `Zap` CHANGE `agentId` `agentId` INT(11) NULL");
+echo "OK\n";
+
+echo "Update complete!\n";
