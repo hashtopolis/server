0c8712cfac38ae9735c303b50e66623f44fe97aa
djban <djbante@gmail.com>
2017-07-03 18:12:47 +0200

    Add Yubikey OTP Auth

---
+++ src/inc/db.php
@@ -0,0 +1,10 @@
+<?php
+
+//START CONFIG
+$CONN['user'] = '';
+$CONN['pass'] = '';
+$CONN['server'] = 'localhost';
+$CONN['db'] = '';
+$CONN['port'] = '3306';
+$INSTALL = false; //set this to true if you config the mysql and setup manually
+//END CONFIG
