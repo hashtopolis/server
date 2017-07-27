223d257c9b5f72063444372819839d01e6db17d5
Sein Coray <sein.coray@gmail.com>
2017-07-27 19:20:32 +0200

    moved csrf check

--- src/users.php
+++ src/users.php
@@ -18,8 +18,7 @@ $TEMPLATE = new Template("users/index");
 $MENU->setActive("users_list");
 
 //catch actions here...
-if (isset($_POST['action'])) {
-  Util::checkCSRF($_POST['csrf']);
+if (isset($_POST['action']) && Util::checkCSRF($_POST['csrf'])) {
   $usersHandler = new UsersHandler();
   $usersHandler->handle($_POST['action']);
   if (UI::getNumMessages() == 0) {
