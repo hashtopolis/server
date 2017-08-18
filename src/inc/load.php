e56f49709b1efa1b4e4884ea4c32eb18549093c6
Sein Coray <s3inlc@hashes.org>
2017-08-18 19:45:28 +0200

    added dev flag

--- src/inc/load.php
+++ src/inc/load.php
@@ -9,7 +9,7 @@ session_start();
 
 $OBJECTS = array();
 
-$VERSION = "0.4.3";
+$VERSION = "dev";
 $HOST = @$_SERVER['HTTP_HOST'];
 if (strpos($HOST, ":") !== false) {
   $HOST = substr($HOST, 0, strpos($HOST, ":"));
