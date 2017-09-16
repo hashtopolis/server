87fb2f27773c61f37f03c3c785a63a29885c28c2
Sein Coray <sein.coray@gmail.com>
2017-09-16 16:45:31 +0200

    completed page for creating preconfigured tasks

--- src/inc/defines/pretasks.php
+++ src/inc/defines/pretasks.php
@@ -8,4 +8,5 @@ class DPretaskAction {
   const SET_COLOR      = "setColor";
   const SET_CPU_TASK   = "setCpuTask";
   const SET_SMALL_TASK = "setSmallTask";
+  const CREATE_TASK    = "createTask";
 }
