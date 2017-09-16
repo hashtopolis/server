25b911fa4a6c1daf2f42ff9803c43bff4fd1515d
Sein Coray <sein.coray@gmail.com>
2017-09-16 16:04:37 +0200

    added detail page for pretasks

--- src/inc/defines/pretasks.php
+++ src/inc/defines/pretasks.php
@@ -3,4 +3,9 @@
 class DPretaskAction {
   const SET_PRIORITY   = "setPriority";
   const DELETE_PRETASK = "deletePretask";
+  const RENAME_PRETASK = "renamePretask";
+  const SET_TIME       = "setTime";
+  const SET_COLOR      = "setColor";
+  const SET_CPU_TASK   = "setCpuTask";
+  const SET_SMALL_TASK = "setSmallTask";
 }
