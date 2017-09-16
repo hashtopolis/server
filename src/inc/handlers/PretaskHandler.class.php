4fa99083f949f9b9b7fed9975358d1104c2eb8c7
Sein Coray <sein.coray@gmail.com>
2017-09-17 00:24:37 +0200

    added handling function for creating pretasks

--- src/inc/handlers/PretaskHandler.class.php
+++ src/inc/handlers/PretaskHandler.class.php
@@ -1,6 +1,7 @@
 <?php
 
 use DBA\FilePretask;
+use DBA\Pretask;
 use DBA\QueryFilter;
 use DBA\SupertaskPretask;
 use DBA\TaskFile;
@@ -57,10 +58,51 @@ class PretaskHandler implements Handler {
         }
         $this->setSmallTask($_POST['isSmall'], $_POST['pretaskId']);
         break;
+      case DPretaskAction::CREATE_TASK:
+        if ($LOGIN->getLevel() < DAccessLevel::USER) {
+          UI::printError("ERROR", "You have no rights to execute this action!");
+        }
+        $this->createPretask($_POST['name'], $_POST['cmdline'], $_POST['chunk'], $_POST['status'], $_POST['color'], $_POST['cpuOnly'], $_POST['isSmall']);
+        break;
       default:
         UI::addMessage(UI::ERROR, "Invalid action!");
         break;
     }
+  }
+  
+  private function createPretask($name, $cmdLine, $chunkTime, $statusTimer, $color, $cpuOnly, $isSmall) {
+    /** @var $CONFIG DataSet */
+    global $FACTORIES, $CONFIG;
+    
+    if (strlen($name) == 0) {
+      UI::addMessage(UI::ERROR, "Name cannot be empty!");
+      return;
+    }
+    else if (strpos($cmdLine, $CONFIG->getVal(DConfig::HASHLIST_ALIAS)) === false) {
+      UI::addMessage(UI::ERROR, "The attack command does not contain the hashlist alias!");
+      return;
+    }
+    $chunkTime = intval($chunkTime);
+    $statusTimer = intval($statusTimer);
+    if (strlen($color) > 0 && preg_match("/[0-9A-Fa-f]{6}/", $color) == 0) {
+      $color = "";
+    }
+    else if ($cpuOnly < 0 || $cpuOnly > 1) {
+      $cpuOnly = 0;
+    }
+    else if ($isSmall < 0 || $isSmall > 1) {
+      $isSmall = 0;
+    }
+    else if ($chunkTime <= 0) {
+      $chunkTime = $CONFIG->getVal(DConfig::CHUNK_DURATION);
+    }
+    else if ($statusTimer <= 0) {
+      $statusTimer = $CONFIG->getVal(DConfig::STATUS_TIMER);
+    }
+    $pretask = new Pretask(0, htmlentities($name, ENT_QUOTES, "UTF-8"), $cmdLine, $chunkTime, $statusTimer, $color, $isSmall, $cpuOnly, 0, 0, 0);
+    $FACTORIES::getPretaskFactory()->save($pretask);
+    header("Location: pretasks.php");
+    die();
   }
   
   private function delete($pretaskId) {
