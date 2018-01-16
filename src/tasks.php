9fb110f64acc2daf1377c492fa3e774848a2b24a
Sein Coray <sein.coray@gmail.com>
2018-01-16 15:24:30 +0100

    fixed keyspace progress on tasks page

--- src/tasks.php
+++ src/tasks.php
@@ -121,14 +121,14 @@ if (isset($_GET['id'])) {
     if ($chunk->getDispatchTime() > 0 && $chunk->getSolveTime() > 0) {
       $chunkIntervals[] = array("start" => $chunk->getDispatchTime(), "stop" => $chunk->getSolveTime());
     }
-    $cProgress += $chunk->getProgress();
+    $cProgress += $chunk->getCheckpoint();
     if (!$agentsProgress->getVal($chunk->getAgentId())) {
-      $agentsProgress->addValue($chunk->getAgentId(), $chunk->getProgress());
+      $agentsProgress->addValue($chunk->getAgentId(), $chunk->getCheckpoint());
       $agentsCracked->addValue($chunk->getAgentId(), $chunk->getCracked());
       $agentsSpent->addValue($chunk->getAgentId(), max($chunk->getSolveTime() - $chunk->getDispatchTime(), 0));
     }
     else {
-      $agentsProgress->addValue($chunk->getAgentId(), $agentsProgress->getVal($chunk->getAgentId()) + $chunk->getProgress());
+      $agentsProgress->addValue($chunk->getAgentId(), $agentsProgress->getVal($chunk->getAgentId()) + $chunk->getCheckpoint());
       $agentsCracked->addValue($chunk->getAgentId(), $agentsCracked->getVal($chunk->getAgentId()) + $chunk->getCracked());
       $agentsSpent->addValue($chunk->getAgentId(), $agentsSpent->getVal($chunk->getAgentId()) + max($chunk->getSolveTime() - $chunk->getDispatchTime(), 0));
     }
