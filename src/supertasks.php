021b40bc3eccc0ba57bba969e703945dbb88ea95
Sein Coray <s3inlc@hashes.org>
2018-01-19 20:09:23 +0100

    fixed usort call

--- src/supertasks.php
+++ src/supertasks.php
@@ -50,7 +50,7 @@ else if (isset($_GET['id']) && isset($_GET['new'])) {
   $OBJECTS['orig'] = $supertask->getId();
   $OBJECTS['lists'] = $FACTORIES::getHashlistFactory()->filter(array());
   $OBJECTS['binaries'] = $FACTORIES::getCrackerBinaryTypeFactory()->filter(array());
-  $OBJECTS['versions'] = usort($FACTORIES::getCrackerBinaryFactory()->filter(array()), array("Util", "versionComparisonCracker"));
+  $OBJECTS['versions'] = usort($FACTORIES::getCrackerBinaryFactory()->filter(array()), array("Util", "versionComparisonBinary"));
 }
 else if (isset($_GET['id'])) {
   $TEMPLATE = new Template("supertasks/detail");
