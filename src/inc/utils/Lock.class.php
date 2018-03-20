4b14af483c55031defda4aebdecbea45840c5a14
Sein Coray <s3inlc@hashes.org>
2018-03-20 14:52:14 +0100

    added Lock and LockUtils made dbh static for DB connections

---
+++ src/inc/utils/Lock.class.php
@@ -0,0 +1,47 @@
+<?php
+
+class Lock {
+  const CHUNKING = "chunking.lock";
+  
+  private $lockFile;
+  private $lock;
+  
+  /**
+   * Lock constructor.
+   * @param $lockFile
+   * @throws Exception
+   */
+  public function __construct($lockFile) {
+    $lockFile = dirname(__FILE__) . "/locks/" . $lockFile;
+    $lock = fopen($lockFile, 'a');
+    if ($lock === false) {
+      throw new Exception("Could not open lockfile '$lockFile'!");
+    }
+    $this->lockFile = $lockFile;
+    $this->lock = $lock;
+  }
+  
+  /**
+   * @throws Exception
+   */
+  public function getLock() {
+    // Get exclusive lock (blocking)
+    $ret = flock($this->lock, LOCK_EX);
+    if ($ret === false) {
+      throw new Exception("Could not get lock on lockfile '" . $this->lockFile . "'!");
+    }
+  }
+  
+  /**
+   * @throws Exception
+   */
+  public
+  function release() {
+    /* Release the lock */
+    $ret = flock($this->lock, LOCK_UN);
+    if ($ret === false) {
+      throw new Exception("Could not release lock on lockfile '" . $this->lockFile . "'.");
+    }
+    fclose($this->lock);
+  }
+}
