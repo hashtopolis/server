077bee032caf633cba7bf482547fc68a0b006039
Sein Coray <s3inlc@hashes.org>
2018-03-22 11:32:38 +0100

    preparation for release v0.5.0

--- src/inc/load.php
+++ src/inc/load.php
@@ -2,14 +2,14 @@
 
 use DBA\Factory;
 
-//set to 1 for debugging
+// set to 1 for debugging
 ini_set("display_errors", "0");
 
 session_start();
 
 $OBJECTS = array();
 
-$VERSION = "0.5.0-rc1";
+$VERSION = "0.5.0";
 $HOST = @$_SERVER['HTTP_HOST'];
 if (strpos($HOST, ":") !== false) {
   $HOST = substr($HOST, 0, strpos($HOST, ":"));
