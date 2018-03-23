cc114863d03115118e8d0dee67b037431c6845d1
Sein Coray <s3inlc@hashes.org>
2018-03-23 14:52:03 +0100

    added admin permission check

--- src/inc/utils/AccessControl.class.php
+++ src/inc/utils/AccessControl.class.php
@@ -4,13 +4,19 @@ use DBA\User;
 
 class AccessControl {
   private $user;
+  private $rightGroup;
   
   /**
    * AccessControl constructor.
    * @param $user User
    */
   public function __construct($user = null) {
+    global $FACTORIES;
+    
     $this->user = $user;
+    if ($this->user != null) {
+      $this->rightGroup = $FACTORIES::getRightGroupFactory()->get($this->user->getRightGroupId());
+    }
   }
   
   /**
@@ -26,6 +32,12 @@ class AccessControl {
    * @return bool true if access is granted
    */
   public function hasPermission($perm) {
+    if($this->rightGroup == null){
+      return false;
+    }
+    else if($this->rightGroup->getPermissions() == 'ALL'){
+      return true; // ALL denotes admin permissions which are independant of which access variables exactly exist
+    }
     // TODO: check if the user has the requested permission
     return false;
   }
