7c69902c1e2dc5c0a0e3af200aa648784b37035d
Sein Coray <s3inlc@hashes.org>
2018-03-23 11:58:07 +0100

    added config access control

--- src/inc/defines/config.php
+++ src/inc/defines/config.php
@@ -8,10 +8,17 @@ class DConfigType {
 }
 
 class DConfigAction {
-  const UPDATE_CONFIG = "updateConfig";
-  const REBUILD_CACHE = "rebuildCache";
-  const RESCAN_FILES  = "rescanFiles";
-  const CLEAR_ALL     = "clearAll";
+  const UPDATE_CONFIG      = "updateConfig";
+  const UPDATE_CONFIG_PERM = DAccessControl::SERVER_CONFIG_ACCESS;
+  
+  const REBUILD_CACHE      = "rebuildCache";
+  const REBUILD_CACHE_PERM = DAccessControl::SERVER_CONFIG_ACCESS;
+  
+  const RESCAN_FILES      = "rescanFiles";
+  const RESCAN_FILES_PERM = DAccessControl::SERVER_CONFIG_ACCESS;
+  
+  const CLEAR_ALL      = "clearAll";
+  const CLEAR_ALL_PERM = DAccessControl::SERVER_CONFIG_ACCESS;
 }
 
 // used config values
