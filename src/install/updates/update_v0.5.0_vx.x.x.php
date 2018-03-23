85848ec0b897869380bc82ecc3a0b49623392520
Sein Coray <s3inlc@hashes.org>
2018-03-23 14:14:36 +0100

    updated mysql for new permissions

--- src/install/updates/update_v0.5.0_vx.x.x.php
+++ src/install/updates/update_v0.5.0_vx.x.x.php
@@ -7,6 +7,9 @@ require_once(dirname(__FILE__) . "/../../inc/load.php");
 
 echo "Apply updates...\n";
 
-
+echo "Create new permissions... ";
+// TODO: update columns and create default no-rights group to put all other users there
+// Maybe have some default permissions groups
+echo "OK\n";
 
 echo "Update complete!\n";
