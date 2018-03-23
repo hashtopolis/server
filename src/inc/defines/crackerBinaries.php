4d536a38366282b45d91fb91f6400c74aa08a304
Sein Coray <s3inlc@hashes.org>
2018-03-23 12:00:45 +0100

    added permissions for cracker binaries

--- src/inc/defines/crackerBinaries.php
+++ src/inc/defines/crackerBinaries.php
@@ -1,11 +1,20 @@
 <?php
 
 class DCrackerBinaryAction {
-  const DELETE_BINARY_TYPE = "deleteBinaryType";
+  const DELETE_BINARY_TYPE      = "deleteBinaryType";
+  const DELETE_BINARY_TYPE_PERM = DAccessControl::CRACKER_BINARY_ACCESS;
+  
   const DELETE_BINARY      = "deleteBinary";
-  const CREATE_BINARY_TYPE = "createBinaryType";
+  const DELETE_BINARY_PERM = DAccessControl::CRACKER_BINARY_ACCESS;
+  
+  const CREATE_BINARY_TYPE      = "createBinaryType";
+  const CREATE_BINARY_TYPE_PERM = DAccessControl::CRACKER_BINARY_ACCESS;
+  
   const CREATE_BINARY      = "createBinary";
-  const EDIT_BINARY        = "editBinary";
+  const CREATE_BINARY_PERM = DAccessControl::CRACKER_BINARY_ACCESS;
+  
+  const EDIT_BINARY      = "editBinary";
+  const EDIT_BINARY_PERM = DAccessControl::CRACKER_BINARY_ACCESS;
 }
 
 class DPlatforms {
