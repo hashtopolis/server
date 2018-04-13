fac3bbcf5d725fc81759c20a04e19419b6203450
Sein Coray <s3inlc@hashes.org>
2018-04-13 15:12:52 +0200

    reworked pretask pages

--- src/inc/defines/tasks.php
+++ src/inc/defines/tasks.php
@@ -2,25 +2,25 @@
 
 class DPretaskAction {
   const SET_PRIORITY      = "setPriority";
-  const SET_PRIORITY_PERM = DAccessControl::CREATE_PRETASK_ACCESS;
+  const SET_PRIORITY_PERM = DAccessControl::MANAGE_PRETASK_ACCESS;
   
   const DELETE_PRETASK      = "deletePretask";
-  const DELETE_PRETASK_PERM = DAccessControl::CREATE_PRETASK_ACCESS;
+  const DELETE_PRETASK_PERM = DAccessControl::MANAGE_PRETASK_ACCESS;
   
   const RENAME_PRETASK      = "renamePretask";
-  const RENAME_PRETASK_PERM = DAccessControl::CREATE_PRETASK_ACCESS;
+  const RENAME_PRETASK_PERM = DAccessControl::MANAGE_PRETASK_ACCESS;
   
   const SET_TIME      = "setTime";
-  const SET_TIME_PERM = DAccessControl::CREATE_PRETASK_ACCESS;
+  const SET_TIME_PERM = DAccessControl::MANAGE_PRETASK_ACCESS;
   
   const SET_COLOR      = "setColor";
-  const SET_COLOR_PERM = DAccessControl::CREATE_PRETASK_ACCESS;
+  const SET_COLOR_PERM = DAccessControl::MANAGE_PRETASK_ACCESS;
   
   const SET_CPU_TASK      = "setCpuTask";
-  const SET_CPU_TASK_PERM = DAccessControl::CREATE_PRETASK_ACCESS;
+  const SET_CPU_TASK_PERM = DAccessControl::MANAGE_PRETASK_ACCESS;
   
   const SET_SMALL_TASK      = "setSmallTask";
-  const SET_SMALL_TASK_PERM = DAccessControl::CREATE_PRETASK_ACCESS;
+  const SET_SMALL_TASK_PERM = DAccessControl::MANAGE_PRETASK_ACCESS;
   
   const CREATE_TASK      = "createTask";
   const CREATE_TASK_PERM = DAccessControl::RUN_TASK_ACCESS;
@@ -47,40 +47,40 @@ class DSupertaskAction {
 
 class DTaskAction {
   const SET_BENCHMARK      = "setBenchmark";
-  const SET_BENCHMARK_PERM = DAccessControl::CREATE_TASK_ACCESS;
+  const SET_BENCHMARK_PERM = DAccessControl::MANAGE_TASK_ACCESS;
   
   const SET_SMALL_TASK      = "setSmallTask";
-  const SET_SMALL_TASK_PERM = DAccessControl::CREATE_TASK_ACCESS;
+  const SET_SMALL_TASK_PERM = DAccessControl::MANAGE_TASK_ACCESS;
   
   const SET_CPU_TASK      = "setCpuTask";
-  const SET_CPU_TASK_PERM = DAccessControl::CREATE_TASK_ACCESS;
+  const SET_CPU_TASK_PERM = DAccessControl::MANAGE_TASK_ACCESS;
   
   const ABORT_CHUNK      = "abortChunk";
-  const ABORT_CHUNK_PERM = DAccessControl::CREATE_TASK_ACCESS;
+  const ABORT_CHUNK_PERM = DAccessControl::MANAGE_TASK_ACCESS;
   
   const RESET_CHUNK      = "resetChunk";
-  const RESET_CHUNK_PERM = DAccessControl::CREATE_TASK_ACCESS;
+  const RESET_CHUNK_PERM = DAccessControl::MANAGE_TASK_ACCESS;
   
   const PURGE_TASK      = "purgeTask";
-  const PURGE_TASK_PERM = DAccessControl::CREATE_TASK_ACCESS;
+  const PURGE_TASK_PERM = DAccessControl::MANAGE_TASK_ACCESS;
   
   const SET_COLOR      = "setColor";
-  const SET_COLOR_PERM = DAccessControl::CREATE_TASK_ACCESS;
+  const SET_COLOR_PERM = DAccessControl::MANAGE_TASK_ACCESS;
   
   const SET_TIME      = "setTime";
-  const SET_TIME_PERM = DAccessControl::CREATE_TASK_ACCESS;
+  const SET_TIME_PERM = DAccessControl::MANAGE_TASK_ACCESS;
   
   const RENAME_TASK      = "renameTask";
-  const RENAME_TASK_PERM = DAccessControl::CREATE_TASK_ACCESS;
+  const RENAME_TASK_PERM = DAccessControl::MANAGE_TASK_ACCESS;
   
   const DELETE_FINISHED      = "deleteFinished";
-  const DELETE_FINISHED_PERM = DAccessControl::CREATE_TASK_ACCESS;
+  const DELETE_FINISHED_PERM = DAccessControl::MANAGE_TASK_ACCESS;
   
   const DELETE_TASK      = "deleteTask";
-  const DELETE_TASK_PERM = DAccessControl::CREATE_TASK_ACCESS;
+  const DELETE_TASK_PERM = DAccessControl::MANAGE_TASK_ACCESS;
   
   const SET_PRIORITY      = "setPriority";
-  const SET_PRIORITY_PERM = DAccessControl::CREATE_TASK_ACCESS;
+  const SET_PRIORITY_PERM = DAccessControl::MANAGE_TASK_ACCESS;
   
   const CREATE_TASK      = "createTask";
   const CREATE_TASK_PERM = DAccessControl::CREATE_TASK_ACCESS;
