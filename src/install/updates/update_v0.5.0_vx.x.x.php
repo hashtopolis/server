3471b0a3f126fd3507c8c5c230afc7f03fecae18
Sein Coray <s3inlc@hashes.org>
2018-05-03 14:25:30 +0200

    updated python agent binary

--- src/install/updates/update_v0.5.0_vx.x.x.php
+++ src/install/updates/update_v0.5.0_vx.x.x.php
@@ -7,6 +7,17 @@ require_once(dirname(__FILE__) . "/../../inc/load.php");
 
 echo "Apply updates...\n";
 
-
+echo "Check agent binaries... ";
+$qF = new QueryFilter(AgentBinary::TYPE, "python", "=");
+$binary = $FACTORIES::getAgentBinaryFactory()->filter(array($FACTORIES::FILTER => $qF), true);
+if ($binary != null) {
+  if (Util::versionComparison($binary->getVersion(), "0.1.4") == 1) {
+    echo "update version... ";
+    $binary->setVersion("0.1.4");
+    $FACTORIES::getAgentBinaryFactory()->update($binary);
+    echo "OK";
+  }
+}
+echo "\n";
 
 echo "Update complete!\n";
