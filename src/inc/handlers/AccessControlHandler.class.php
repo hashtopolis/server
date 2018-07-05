7d4222884ebebd6ca14dfb349054654408909325
Sein Coray <s3inlc@hashes.org>
2018-07-05 15:51:42 +0200

    added missing saving action of permission update

--- src/inc/handlers/AccessControlHandler.class.php
+++ src/inc/handlers/AccessControlHandler.class.php
@@ -8,10 +8,10 @@ class AccessControlHandler implements Handler {
   public function __construct($groupId = null) {
     //we need nothing to load
   }
-  
+
   public function handle($action) {
     global $ACCESS_CONTROL;
-    
+
     switch ($action) {
       case DAccessControlAction::CREATE_GROUP:
         $ACCESS_CONTROL->checkPermission(DAccessControlAction::CREATE_GROUP_PERM);
@@ -30,21 +30,21 @@ class AccessControlHandler implements Handler {
         break;
     }
   }
-  
+
   private function saveEdit($groupId, $perm) {
     global $FACTORIES, $ACCESS_CONTROL;
-    
+
     $group = $FACTORIES::getRightGroupFactory()->get($groupId);
     if ($group === null) {
       UI::addMessage(UI::ERROR, "Invalid group!");
       return;
     }
-    
+
     if ($group->getPermissions() == 'ALL') {
       UI::addMessage(UI::ERROR, "Administrator group cannot be changed!");
       return;
     }
-    
+
     $newArr = [];
     foreach ($perm as $p) {
       $split = explode("-", $p);
@@ -73,36 +73,38 @@ class AccessControlHandler implements Handler {
         $newArr[$constant] = true;
       }
     }
+    $group->setPermissions(json_encode($newArr));
+    $FACTORIES::getRightGroupFactory()->update($group);
   }
-  
+
   private function deleteGroup($groupId) {
     global $FACTORIES;
-    
+
     $group = $FACTORIES::getRightGroupFactory()->get($groupId);
     if ($group === null) {
       UI::addMessage(UI::ERROR, "Invalid group!");
       return;
     }
-    
+
     $qF = new QueryFilter(User::RIGHT_GROUP_ID, $group->getId(), "=");
     $count = $FACTORIES::getUserFactory()->countFilter(array($FACTORIES::FILTER => $qF));
     if ($count > 0) {
       UI::addMessage(UI::ERROR, "You cannot delete a group which has still users belonging to it!");
       return;
     }
-    
+
     // delete permission group
     $FACTORIES::getRightGroupFactory()->delete($group);
   }
-  
+
   private function createGroup($groupName) {
     global $FACTORIES;
-    
+
     if (strlen($groupName) == 0 || strlen($groupName) > DLimits::ACCESS_GROUP_MAX_LENGTH) {
       UI::addMessage(UI::ERROR, "Permission group name is too short or too long!");
       return;
     }
-    
+
     $qF = new QueryFilter(RightGroup::GROUP_NAME, $groupName, "=");
     $check = $FACTORIES::getRightGroupFactory()->filter(array($FACTORIES::FILTER => $qF), true);
     if ($check !== null) {
