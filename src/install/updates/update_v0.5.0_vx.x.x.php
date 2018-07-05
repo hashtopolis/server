3451efe4abbdc6398de4da3b0973340471cfab91
Sein Coray <s3inlc@hashes.org>
2018-07-05 18:00:51 +0200

    added update script to change permission structure during update

--- src/install/updates/update_v0.5.0_vx.x.x.php
+++ src/install/updates/update_v0.5.0_vx.x.x.php
@@ -2,6 +2,8 @@
 
 use DBA\AgentBinary;
 use DBA\QueryFilter;
+use DBA\RightGroup;
+use DBA\User;
 
 require_once(dirname(__FILE__) . "/../../inc/load.php");
 
@@ -21,8 +23,36 @@ if ($binary != null) {
 echo "\n";
 
 echo "Create new permissions... ";
-// TODO: update columns and create default no-rights group to put all other users there
-// Maybe have some default permissions groups
+// load all users and set permission group to null
+$users = $FACTORIES::getUserFactory()->filter(array());
+$uS = new UpdateSet(User::RIGHT_GROUP_ID, null);
+$qF = new QueryFilter(User::USER_ID, 0, ">");
+$FACTORIES::getUserFactory()->massUpdate(array($FACTORIES::FILTER => $qF, $FACTORIES::UPDATE => $uS));
+// apply table changes
+$FACTORIES::getAgentFactory()->getDB()->query("ALTER TABLE `RightGroup` ADD `permissions` TEXT NOT NULL");
+$FACTORIES::getAgentFactory()->getDB()->query("ALTER TABLE `RightGroup` DROP `level`");
+$qF = new QueryFilter(RightGroup::GROUP_NAME, 'Administrator', "=");
+$adminGroup = $FACTORIES::getRightGroupFactory()->filter(array($FACTORIES::FILTER => $qF));
+// delete all right groups
+$qF = new QueryFilter(RightGroup::RIGHT_GROUP_ID, 0, ">");
+$FACTORIES::getRightGroupFactory()->massDeletion(array($qF));
+// add administrator group
+$admins = new RightGroup(0, 'Administrators', 'ALL');
+$admins = $FACTORIES::getRightGroupFactory()->save($admins);
+// add other default group (with no permissions)
+$others = new RightGroup(0, 'Other Users', '{}');
+$others = $FACTORIES::getRightGroupFactory()->save($others);
+// go through users and assign them to correct group
+foreach($users as $user){
+  if($user->getRightGroupId() == $adminGroup->getId()){
+    // user was admin
+    $user->setRightGroupId($admins->getId());
+  }
+  else{
+    $user->setRightGroupId($others->getId());
+  }
+  $FACTORIES::getUserFactory()->update($user);
+}
 echo "OK\n";
 
 echo "Update complete!\n";
