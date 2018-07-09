ec8f5fdbe188a6257048526fc8082bd9e9a8c9c7
Sein Coray <s3inlc@hashes.org>
2018-07-09 12:00:01 +0200

    trying to fix awesome icons

--- src/inc/Util.class.php
+++ src/inc/Util.class.php
@@ -644,7 +644,7 @@ class Util {
     );
     $oses = array(
       '<span class="fas fa-linux" aria-hidden="true"></span>',
-      '<span class="fas fa-windows" aria-hidden="true"></span>',
+      '<span class="fab fa-windows" aria-hidden="true"></span>',
       '<span class="fas fa-apple" aria-hidden="true"></span>'
     );
     $formats = array(
