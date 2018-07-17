b48b0794bde23234cc7d2de972d7e6fb94c516ac
Sein Coray <s3inlc@hashes.org>
2018-07-17 16:08:06 +0200

    rewrote access control function

--- src/inc/handlers/AccessControlHandler.class.php
+++ src/inc/handlers/AccessControlHandler.class.php
@@ -1,9 +1,5 @@
 <?php
 
-use DBA\QueryFilter;
-use DBA\RightGroup;
-use DBA\User;
-
 class AccessControlHandler implements Handler {
   public function __construct($groupId = null) {
     //we need nothing to load
@@ -15,115 +11,23 @@ class AccessControlHandler implements Handler {
     switch ($action) {
       case DAccessControlAction::CREATE_GROUP:
         $ACCESS_CONTROL->checkPermission(DAccessControlAction::CREATE_GROUP_PERM);
-        $this->createGroup($_POST['groupName']);
-        break;
+        $group = AccessControlUtils::createGroup($_POST['groupName']);
+        header("Location: access.php?id=" . $group->getId());
+        die();
       case DAccessControlAction::DELETE_GROUP:
         $ACCESS_CONTROL->checkPermission(DAccessControlAction::DELETE_GROUP_PERM);
-        $this->deleteGroup($_POST['groupId']);
+        AccessControlUtils::deleteGroup($_POST['groupId']);
         break;
       case DAccessControlAction::EDIT:
         $ACCESS_CONTROL->checkPermission(DAccessControlAction::EDIT_PERM);
-        $this->saveEdit($_POST['groupId'], $_POST['perm']);
+        $changes = AccessControlUtils::updateGroupPermissions($_POST['groupId'], $_POST['perm']);
+        if($changes){
+          UI::addMessage(UI::WARN, "NOTE: Some permissions were additionally allowed because of dependencies!");
+        }
         break;
       default:
         UI::addMessage(UI::ERROR, "Invalid action!");
         break;
     }
-  }
-
-  private function saveEdit($groupId, $perm) {
-    global $FACTORIES;
-
-    $group = $FACTORIES::getRightGroupFactory()->get($groupId);
-    if ($group === null) {
-      UI::addMessage(UI::ERROR, "Invalid group!");
-      return;
-    }
-
-    if ($group->getPermissions() == 'ALL') {
-      UI::addMessage(UI::ERROR, "Administrator group cannot be changed!");
-      return;
-    }
-
-    $newArr = [];
-    foreach ($perm as $p) {
-      $split = explode("-", $p);
-      if (sizeof($split) != 2 || !in_array($split[1], array("0", "1"))) {
-        continue; // ignore invalid submits
-      }
-      $constants = DAccessControl::getConstants();
-      foreach ($constants as $constant) {
-        if (is_array($constant)) {
-          $constant = $constant[0];
-        }
-        if ($split[0] == $constant) {
-          $newArr[$constant] = ($split[1] == "1") ? true : false;
-        }
-      }
-    }
-    $group->setPermissions(json_encode($newArr));
-    $FACTORIES::getRightGroupFactory()->update($group);
-
-    $acl = new AccessControl(null, $group->getId());
-    $arr = $newArr;
-    $changes = false;
-    foreach($newArr as $constant => $set){
-      if($set == true){
-        continue;
-      }
-      else if($acl->givenByDependency($constant)){
-        $arr[$constant] = true;
-        $changes = true;
-      }
-    }
-    $group->setPermissions(json_encode($arr));
-    $FACTORIES::getRightGroupFactory()->update($group);
-
-    if($changes){
-      UI::addMessage(UI::WARN, "NOTE: Some permissions were additionally allowed because of dependencies!");
-    }
-  }
-
-  private function deleteGroup($groupId) {
-    global $FACTORIES;
-
-    $group = $FACTORIES::getRightGroupFactory()->get($groupId);
-    if ($group === null) {
-      UI::addMessage(UI::ERROR, "Invalid group!");
-      return;
-    }
-
-    $qF = new QueryFilter(User::RIGHT_GROUP_ID, $group->getId(), "=");
-    $count = $FACTORIES::getUserFactory()->countFilter(array($FACTORIES::FILTER => $qF));
-    if ($count > 0) {
-      UI::addMessage(UI::ERROR, "You cannot delete a group which has still users belonging to it!");
-      return;
-    }
-
-    // delete permission group
-    $FACTORIES::getRightGroupFactory()->delete($group);
-  }
-
-  private function createGroup($groupName) {
-    global $FACTORIES;
-
-    if (strlen($groupName) == 0 || strlen($groupName) > DLimits::ACCESS_GROUP_MAX_LENGTH) {
-      UI::addMessage(UI::ERROR, "Permission group name is too short or too long!");
-      return;
-    }
-
-    $qF = new QueryFilter(RightGroup::GROUP_NAME, $groupName, "=");
-    $check = $FACTORIES::getRightGroupFactory()->filter(array($FACTORIES::FILTER => $qF), true);
-    if ($check !== null) {
-      UI::addMessage(UI::ERROR, "There is already an permission group with the same name!");
-      return;
-    }
-    $group = new RightGroup(0, $groupName, "[]");
-    $group = $FACTORIES::getRightGroupFactory()->save($group);
-    if ($group !== null) {
-      header("Location: access.php?id=" . $group->getId());
-      die();
-    }
-    UI::addMessage(UI::ERROR, "Something went wrong when creating the permission group!");
   }
 }
---
+++ src/inc/utils/AccessControlUtils.class.php
@@ -0,0 +1,112 @@
+<?php
+use DBA\User;
+use DBA\QueryFilter;
+use DBA\RightGroup;
+
+class AccessControlUtils {
+  /**
+   * @param int $groupId 
+   * @param array $perm 
+   * @throws HTException 
+   * @return boolean
+   */
+  public static function updateGroupPermissions($groupId, $perm) {
+    global $FACTORIES;
+
+    $group = AccessControlUtils::getGroup($groupId);
+    if ($group->getPermissions() == 'ALL') {
+      throw new HTException("Administrator group cannot be changed!");
+    }
+
+    $newArr = [];
+    foreach ($perm as $p) {
+      $split = explode("-", $p);
+      if (sizeof($split) != 2 || !in_array($split[1], array("0", "1"))) {
+        continue; // ignore invalid submits
+      }
+      $constants = DAccessControl::getConstants();
+      foreach ($constants as $constant) {
+        if (is_array($constant)) {
+          $constant = $constant[0];
+        }
+        if ($split[0] == $constant) {
+          $newArr[$constant] = ($split[1] == "1") ? true : false;
+        }
+      }
+    }
+    $group->setPermissions(json_encode($newArr));
+    $FACTORIES::getRightGroupFactory()->update($group);
+
+    $acl = new AccessControl(null, $group->getId());
+    $arr = $newArr;
+    $changes = false;
+    foreach($newArr as $constant => $set){
+      if($set == true){
+        continue;
+      }
+      else if($acl->givenByDependency($constant)){
+        $arr[$constant] = true;
+        $changes = true;
+      }
+    }
+    $group->setPermissions(json_encode($arr));
+    $FACTORIES::getRightGroupFactory()->update($group);
+
+    return $changes;
+  }
+
+  /**
+   * @param string $groupName
+   * @throws HTException
+   * @return RightGroup
+   */
+  public static function createGroup($groupName) {
+    global $FACTORIES;
+
+    if (strlen($groupName) == 0 || strlen($groupName) > DLimits::ACCESS_GROUP_MAX_LENGTH) {
+      throw new HTException("Permission group name is too short or too long!");
+    }
+
+    $qF = new QueryFilter(RightGroup::GROUP_NAME, $groupName, "=");
+    $check = $FACTORIES::getRightGroupFactory()->filter(array($FACTORIES::FILTER => $qF), true);
+    if ($check !== null) {
+      throw new HTException("There is already an permission group with the same name!");
+    }
+    $group = new RightGroup(0, $groupName, "[]");
+    $group = $FACTORIES::getRightGroupFactory()->save($group);
+    return $group;
+  }
+
+  /**
+   * @param int $groupId
+   * @throws HTException
+   */
+  public static function deleteGroup($groupId){
+    global $FACTORIES;
+
+    $group = AccessControlUtils::getGroup($groupId);
+    $qF = new QueryFilter(User::RIGHT_GROUP_ID, $group->getId(), "=");
+    $count = $FACTORIES::getUserFactory()->countFilter(array($FACTORIES::FILTER => $qF));
+    if ($count > 0) {
+      throw new HTException("You cannot delete a group which has still users belonging to it!");
+    }
+
+    // delete permission group
+    $FACTORIES::getRightGroupFactory()->delete($group);
+  }
+
+  /**
+   * @param int $groupId
+   * @throws HTException
+   * @return RightGroup
+   */
+  public static function getGroup($groupId){
+    global $FACTORIES;
+
+    $group = $FACTORIES::getRightGroupFactory()->get($groupId);
+    if ($group === null) {
+      throw new HTException("Invalid group!");
+    }
+    return $group;
+  }
+}
