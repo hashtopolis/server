9e4d7bd11f82e5c1b850fe3847cd8295557013ee
Sein Coray <s3inlc@hashes.org>
2018-07-23 11:17:59 +0200

    added descriptions for all user api requests

--- src/inc/defines/userApi.php
+++ src/inc/defines/userApi.php
@@ -151,8 +151,6 @@ abstract class UValues {
   const ERROR   = "ERROR";
 }
 
-
-
 ########################
 # Response definitions #
 ########################
@@ -421,6 +419,8 @@ class UResponseAccount extends UResponse {
 ###############################
 
 abstract class UApi {
+  abstract function describe($constant);
+
   static function getConstants() {
     try {
       $oClass = new ReflectionClass(static::class);
@@ -464,8 +464,11 @@ abstract class UApi {
   }
 
   static function getDescription($section, $constant){
-    // TODO: add descriptions for sections and constants
-    return "__" . $section . "_" . $constant . "__";
+    $section = UApi::getSection($section);
+    if($section == null){
+      return "__" . $section . "_" . $constant . "__";
+    }
+    return $section->describe($constant);
   }
 }
 
@@ -484,11 +487,27 @@ class USection extends UApi {
   const GROUP         = "group";
   const ACCESS        = "access";
   const ACCOUNT       = "account";
+
+  public function describe($section){
+    // placeholder
+    return $section;
+  }
 }
 
 class USectionTest extends UApi {
   const CONNECTION = "connection";
   const ACCESS     = "access";
+
+  public function describe($constant){
+    switch($constant){
+      case USectionTest::CONNECTION:
+        return "Connection testing";
+      case USectionTest::ACCESS:
+        return "Verifying the API key and test if user has access to the API";
+      default:
+        return "__".$constant."__";
+    }
+  }
 }
 
 class USectionAgent extends UApi {
@@ -506,6 +525,39 @@ class USectionAgent extends UApi {
   const SET_EXTRA_PARAMS = "setExtraParams";
   const SET_ERROR_FLAG   = "setErrorFlag";
   const SET_TRUSTED      = "setTrusted";
+
+  public function describe($constant){
+    switch($constant){
+      case USectionAgent::CREATE_VOUCHER:
+        return "Creating new vouchers";
+      case USectionAgent::GET_BINARIES:
+        return "Get a list of available agent binaries";
+      case USectionAgent::LIST_VOUCHERS:
+        return "List existing vouchers";
+      case USectionAgent::DELETE_VOUCHER:
+        return "Delete an existing voucher";
+      case USectionAgent::LIST_AGENTS:
+        return "List all agents";
+      case USectionAgent::GET:
+        return "Get details about an agent";
+      case USectionAgent::SET_ACTIVE:
+        return "Set an agent active/inactive";
+      case USectionAgent::CHANGE_OWNER:
+        return "Change the owner of an agent";
+      case USectionAgent::SET_NAME:
+        return "Set the name of an agent";
+      case USectionAgent::SET_CPU_ONLY:
+        return "Set if an agent is CPU only or not";
+      case USectionAgent::SET_EXTRA_PARAMS:
+        return "Set extra flags for an agent";
+      case USectionAgent::SET_ERROR_FLAG:
+        return "Set how errors from an agent should be handled";
+      case USectionAgent::SET_TRUSTED:
+        return "Set if an agent is trusted or not";
+      default:
+        return "__".$constant."__";
+    }
+  }
 }
 
 class USectionTask extends UApi {
@@ -530,6 +582,49 @@ class USectionTask extends UApi {
 
   const SET_SUPERTASK_NAME = "setSupertaskName";
   const DELETE_SUPERTASK = "deleteSupertask";
+
+  public function describe($constant){
+    switch($constant){
+      case USectionTask::LIST_TASKS:
+        return "List all tasks";
+      case USectionTask::GET_TASK:
+        return "Get details of a task";
+      case USectionTask::LIST_SUBTASKS:
+        return "List subtasks of a running supertask";
+      case USectionTask::GET_CHUNK:
+        return "Get details of a chunk";
+      case USectionTask::CREATE_TASK:
+        return "Create a new task";
+      case USectionTask::RUN_PRETASK:
+        return "Run an existing preconfigured task with a hashlist";
+      case USectionTask::RUN_SUPERTASK:
+        return "Run a configured supertask with a hashlist";
+      case USectionTask::SET_TASK_PRIORITY:
+        return "Set the priority of a task";
+      case USectionTask::SET_SUPERTASK_PRIORITY:
+        return "Set the priority of a supertask";
+      case USectionTask::SET_TASK_NAME:
+        return "Rename a task";
+      case USectionTask::SET_TASK_COLOR:
+        return "Set the color of a task";
+      case USectionTask::SET_TASK_CPU_ONLY:
+        return "Set if a task is CPU only or not";
+      case USectionTask::SET_TASK_SMALL:
+        return "Set if a task is small or not";
+      case USectionTask::TASK_UNASSIGN_AGENT:
+        return "Unassign an agent from a task";
+      case USectionTask::DELETE_TASK:
+        return "Delete a task";
+      case USectionTask::PURGE_TASK:
+        return "Purge a task";
+      case USectionTask::SET_SUPERTASK_NAME:
+        return "Set the name of a supertask";
+      case USectionTask::DELETE_SUPERTASK:
+        return "Delete a supertask";
+      default:
+        return "__".$constant."__";
+    }
+  }
 }
 
 class USectionPretask extends UApi {
@@ -544,6 +639,33 @@ class USectionPretask extends UApi {
   const SET_PRETASK_CPU_ONLY = "setPretaskCpuOnly";
   const SET_PRETASK_SMALL = "setPretaskSmall";
   const DELETE_PRETASK = "deletePretask";
+
+  public function describe($constant){
+    switch($constant){
+      case USectionPretask::LIST_PRETASKS:
+        return "List all preconfigured tasks";
+      case USectionPretask::GET_PRETASK:
+        return "Get details about a preconfigured task";
+      case USectionPretask::CREATE_PRETASK:
+        return "Create preconfigured tasks";
+      case USectionPretask::SET_PRETASK_PRIORITY:
+        return "Set preconfigured tasks priorities";
+      case USectionPretask::SET_PRETASK_NAME:
+        return "Rename preconfigured tasks";
+      case USectionPretask::SET_PRETASK_COLOR:
+        return "Set the color of a preconfigured task";
+      case USectionPretask::SET_PRETASK_CHUNKSIZE:
+        return "Change the chunk size for a preconfigured task";
+      case USectionPretask::SET_PRETASK_CPU_ONLY:
+        return "Set if a preconfigured task is CPU only or not";
+      case USectionPretask::SET_PRETASK_SMALL:
+        return "Set if a preconfigured task is small or not";
+      case USectionPretask::DELETE_PRETASK:
+        return "Delete preconfigured tasks";
+      default:
+        return "__".$constant."__";
+    }
+  }
 }
 
 class USectionSupertask extends UApi {
@@ -553,6 +675,25 @@ class USectionSupertask extends UApi {
   const IMPORT_SUPERTASK = "importSupertask";
   const SET_SUPERTASK_NAME = "setSupertaskName";
   const DELETE_SUPERTASK = "deleteSupertask";
+
+  public function describe($constant){
+    switch($constant){
+      case USectionSupertask::LIST_SUPERTASKS:
+        return "List all supertasks";
+      case USectionSupertask::GET_SUPERTASK:
+        return "Get details of a supertask";
+      case USectionSupertask::CREATE_SUPERTASK:
+        return "Create a supertask";
+      case USectionSupertask::IMPORT_SUPERTASK:
+        return "Import a supertask from masks";
+      case USectionSupertask::SET_SUPERTASK_NAME:
+        return "Rename a configured supertask";
+      case USectionSupertask::DELETE_SUPERTASK:
+        return "Delete a supertask";
+      default:
+        return "__".$constant."__";
+    }
+  }
 }
 
 class USectionHashlist extends UApi {
@@ -568,6 +709,33 @@ class USectionHashlist extends UApi {
   const EXPORT_LEFT = "exportLeft";
 
   const DELETE_HASHLIST = "deleteHashlist";
+
+  public function describe($constant){
+    switch($constant){
+      case USectionHashlist::LIST_HASLISTS:
+        return "List all hashlists";
+      case USectionHashlist::GET_HASHLIST:
+        return "Get details of a hashlist";
+      case USectionHashlist::CREATE_HASHLIST:
+        return "Create a new hashlist";
+      case USectionHashlist::SET_HASHLIST_NAME:
+        return "Rename hashlists";
+      case USectionHashlist::SET_SECRET:
+        return "Set if a hashlist is secret or not";
+      case USectionHashlist::IMPORT_CRACKED:
+        return "Import cracked hashes";
+      case USectionHashlist::EXPORT_CRACKED:
+        return "Export cracked hashes";
+      case USectionHashlist::GENERATE_WORDLIST:
+        return "Generate wordlist from founds";
+      case USectionHashlist::EXPORT_LEFT:
+        return "Export a left list of uncracked hashes";
+      case USectionHashlist::DELETE_HASHLIST:
+        return "Delete hashlists";
+      default:
+        return "__".$constant."__";
+    }
+  }
 }
 
 class USectionSuperhashlist extends UApi {
@@ -575,6 +743,21 @@ class USectionSuperhashlist extends UApi {
   const GET_SUPERHASHLIST = "getSuperhashlist";
   const CREATE_SUPERHASHLIST = "createSuperhashlist";
   const DELETE_SUPERHASHLIST = "deleteSuperhashlist";
+
+  public function describe($constant){
+    switch($constant){
+      case USectionSuperhashlist::LIST_SUPERHASHLISTS:
+        return "List all superhashlists";
+      case USectionSuperhashlist::GET_SUPERHASHLIST:
+        return "Get details about a superhashlist";
+      case USectionSuperhashlist::CREATE_SUPERHASHLIST:
+        return "Create superhashlists";
+      case USectionSuperhashlist::DELETE_SUPERHASHLIST:
+        return "Delete superhashlists";
+      default:
+        return "__".$constant."__";
+    }
+  }
 }
 
 class USectionFile extends UApi {
@@ -586,6 +769,27 @@ class USectionFile extends UApi {
   const SET_SECRET = "setSecret";
   const DELETE_FILE = "deleteFile";
   const SET_FILE_TYPE = "setFileType";
+
+  public function describe($constant){
+    switch($constant){
+      case USectionFile::LIST_FILES:
+        return "List all files";
+      case USectionFile::GET_FILE:
+        return "Get details of a file";
+      case USectionFile::ADD_FILE:
+        return "Add new files";
+      case USectionFile::RENAME_FILE:
+        return "Rename files";
+      case USectionFile::SET_SECRET:
+        return "Set if a file is secret or not";
+      case USectionFile::DELETE_FILE:
+        return "Delete files";
+      case USectionFile::SET_FILE_TYPE:
+        return "Change type of files";
+      default:
+        return "__".$constant."__";
+    }
+  }
 }
 
 class USectionCracker extends UApi {
@@ -597,6 +801,27 @@ class USectionCracker extends UApi {
   const CREATE_CRACKER = "createCracker";
   const ADD_VERSION = "addVersion";
   const UPDATE_VERSION = "updateVersion";
+
+  public function describe($constant){
+    switch($constant){
+      case USectionCracker::LIST_CRACKERS:
+        return "List all crackers";
+      case USectionCracker::GET_CRACKER:
+        return "Get details of a cracker";
+      case USectionCracker::DELETE_VERSION:
+        return "Delete a specific version of a cracker";
+      case USectionCracker::DELETE_CRACKER:
+        return "Deleting crackers";
+      case USectionCracker::CREATE_CRACKER:
+        return "Create new crackers";
+      case USectionCracker::ADD_VERSION:
+        return "Add new cracker versions";
+      case USectionCracker::UPDATE_VERSION:
+        return "Update cracker versions";
+      default:
+        return "__".$constant."__";
+    }
+  }
 }
 
 class USectionConfig extends UApi {
@@ -604,6 +829,21 @@ class USectionConfig extends UApi {
   const LIST_CONFIG = "listConfig";
   const GET_CONFIG = "getConfig";
   const SET_CONFIG = "setConfig";
+
+  public function describe($constant){
+    switch($constant){
+      case USectionConfig::LIST_SECTIONS:
+        return "List available sections in config";
+      case USectionConfig::LIST_CONFIG:
+        return "List config options of a given section";
+      case USectionConfig::GET_CONFIG:
+        return "Get current value of a config";
+      case USectionConfig::SET_CONFIG:
+        return "Change values of configs";
+      default:
+        return "__".$constant."__";
+    }
+  }
 }
 
 class USectionUser extends UApi {
@@ -614,6 +854,27 @@ class USectionUser extends UApi {
   const ENABLE_USER = "enableUser";
   const SET_USER_PASSWORD = "setUserPassword";
   const SET_USER_RIGHT_GROUP = "setUserRightGroup";
+
+  public function describe($constant){
+    switch($constant){
+      case USectionUser::LIST_USERS:
+        return "List all users";
+      case USectionUser::GET_USER:
+        return "Get details of a user";
+      case USectionUser::CREATE_USER:
+        return "Create new users";
+      case USectionUser::DISABLE_USER:
+        return "Disable a user account";
+      case USectionUser::ENABLE_USER:
+        return "Enable a user account";
+      case USectionUser::SET_USER_PASSWORD:
+        return "Set a user's password";
+      case USectionUser::SET_USER_RIGHT_GROUP:
+        return "Change the permission group for a user";
+      default:
+        return "__".$constant."__";
+    }
+  }
 }
 
 class USectionGroup extends UApi {
@@ -626,6 +887,29 @@ class USectionGroup extends UApi {
   const ADD_USER = "addUser";
   const REMOVE_AGENT = "removeAgent";
   const REMOVE_USER = "removeUser";
+
+  public function describe($constant){
+    switch($constant){
+      case USectionGroup::LIST_GROUPS:
+        return "List all groups";
+      case USectionGroup::GET_GROUP:
+        return "Get details of a group";
+      case USectionGroup::CREATE_GROUP:
+        return "Create new groups";
+      case USectionGroup::DELETE_GROUP:
+        return "Delete groups";
+      case USectionGroup::ADD_AGENT:
+        return "Add agents to groups";
+      case USectionGroup::ADD_USER:
+        return "Add users to groups";
+      case USectionGroup::REMOVE_AGENT:
+        return "Remove agents from groups";
+      case USectionGroup::REMOVE_USER:
+        return "Remove users from groups";
+      default:
+        return "__".$constant."__";
+    }
+  }
 }
 
 class USectionAccess extends UApi {
@@ -634,6 +918,23 @@ class USectionAccess extends UApi {
   const CREATE_GROUP = "createGroup";
   const DELETE_GROUP = "deleteGroup";
   const SET_PERMISSIONS = "setPermissions";
+
+  public function describe($constant){
+    switch($constant){
+      case USectionAccess::LIST_GROUPS:
+        return "List permission groups";
+      case USectionAccess::GET_GROUP:
+        return "Get details of a permission group";
+      case USectionAccess::CREATE_GROUP:
+        return "Create a new permission group";
+      case USectionAccess::DELETE_GROUP:
+        return "Delete permission groups";
+      case USectionAccess::SET_PERMISSIONS:
+        return "Update permissions of a group";
+      default:
+        return "__".$constant."__";
+    }
+  }
 }
 
 class USectionAccount extends UApi {
@@ -641,4 +942,19 @@ class USectionAccount extends UApi {
   const SET_EMAIL = "setEmail";
   const SET_SESSION_LENGTH = "setSessionLength";
   const CHANGE_PASSWORD = "changePassword";
+
+  public function describe($constant){
+    switch($constant){
+      case USectionAccount::GET_INFORMATION:
+        return "Get account information";
+      case USectionAccount::SET_EMAIL:
+        return "Change email";
+      case USectionAccount::SET_SESSION_LENGTH:
+        return "Update session length";
+      case USectionAccount::CHANGE_PASSWORD:
+        return "Change password";
+      default:
+        return "__".$constant."__";
+    }
+  }
 }
