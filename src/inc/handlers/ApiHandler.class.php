653079ef59b48bdf54c4f6959948a46b74c9f186
Sein Coray <s3inlc@hashes.org>
2018-07-24 17:28:39 +0200

    added some messages for handler

--- src/inc/handlers/ApiHandler.class.php
+++ src/inc/handlers/ApiHandler.class.php
@@ -12,21 +12,26 @@ class ApiHandler implements Handler {
       switch ($action) {
         case DApiAction::DELETE_GROUP:
           ApiUtils::deleteGroup($_POST['groupId']);
+          UI::addMessage(UI::SUCCESS, "API group was deleted!");
           break;
         case DApiAction::EDIT:
           // TODO:
           break;
         case DApiAction::CREATE_GROUP:
           ApiUtils::createGroup($_POST['groupName']);
-          break;
+          header("Location: api.php");
+          die();
         case DApiAction::DELETE_KEY:
           ApiUtils::deleteKey($_POST['keyId']);
+          UI::addMessage(UI::SUCCESS, "API key was deleted!");
           break;
         case DApiAction::CREATE_KEY:
           ApiUtils::createKey($_POST['userId'], $_POST['groupId']);
-          break;
+          header("Location: api.php");
+          die();
         case DApiAction::EDIT_KEY:
           // TODO:
+          UI::addMessage(UI::SUCCESS, "Saved changes!");
           break;
         default:
           UI::addMessage(UI::ERROR, "Invalid action!");
