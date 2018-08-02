766bab9bbb539c3ec37b86e38a9bd746b3989dbe
Sein Coray <s3inlc@hashes.org>
2018-08-02 17:35:01 +0200

    fixed active detection in case of prince tasks without keyspace

--- src/inc/Util.class.php
+++ src/inc/Util.class.php
@@ -185,7 +185,7 @@ class Util {
     }
 
     $isActive = false;
-    if (time() - $maxTime < $CONFIG->getVal(DConfig::CHUNK_TIMEOUT) && ($progress < $task->getKeyspace() || $task->getIsPrince() && $task->getKeyspace() == 0)) {
+    if (time() - $maxTime < $CONFIG->getVal(DConfig::CHUNK_TIMEOUT) && ($progress < $task->getKeyspace() || $task->getIsPrince() && $task->getKeyspace() == DPrince::PRINCE_KEYSPACE)) {
       $isActive = true;
     }
     return array($progress, $cracked, $isActive, sizeof($chunks), ($totalTimeSpent > 0) ? round($cracked * 60 / $totalTimeSpent, 2) : 0);
