55f2427cb388639db5c59e223acb393c9fb1f6dc
Sein Coray <s3inlc@hashes.org>
2018-08-08 16:28:46 +0200

    added hashtopolis python client v0.1.7

--- src/install/hashtopolis.sql
+++ src/install/hashtopolis.sql
@@ -123,7 +123,7 @@ CREATE TABLE `AgentBinary` (
 
 INSERT INTO `AgentBinary` (`agentBinaryId`, `type`, `version`, `operatingSystems`, `filename`) VALUES
   (1, 'csharp', '0.52.2', 'Windows, Linux(mono), OS X(mono)', 'hashtopolis.exe'),
-  (2, 'python', '0.1.6', 'Windows, Linux, OS X', 'hashtopolis.zip');
+  (2, 'python', '0.1.7', 'Windows, Linux, OS X', 'hashtopolis.zip');
 
 -- --------------------------------------------------------
 
