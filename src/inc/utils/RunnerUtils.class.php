6c0e6823dbafe15638769ac622c2d7c7b770e297
Sein Coray <s3inlc@hashes.org>
2018-08-20 15:33:05 +0200

    removed empty if

--- src/inc/utils/RunnerUtils.class.php
+++ src/inc/utils/RunnerUtils.class.php
@@ -46,9 +46,6 @@ class RunnerUtils {
     $dir = self::getDir();
     $out = [];
     exec("cd '$dir' && python3 runner.zip stop", $out);
-    if(sizeof($out) > 0){
-      //throw new HTException("There maybe was trouble stopping the runner: [" . implode(", ", $out) . "]");
-    }
   }
 
   /**
