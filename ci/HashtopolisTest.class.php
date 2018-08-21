44f64bf9ed06af0c2b0762bd7939a855cca9f303
Sein Coray <s3inlc@hashes.org>
2018-08-21 13:03:54 +0200

    fixed access group id

--- ci/HashtopolisTest.class.php
+++ ci/HashtopolisTest.class.php
@@ -26,7 +26,7 @@ abstract class HashtopolisTest{
     Factory::getAgentFactory()->getDB()->query(file_get_contents(dirname(__FILE__)."/files/db_".$version.".sql"));
 
     // insert user and api key
-    $this->user = new User(0, 'testuser', '', '', '', 1, 0, 0, 0, 3600, AccessUtils::getOrCreateDefaultAccessGroup(), 0, '', '', '', '');
+    $this->user = new User(0, 'testuser', '', '', '', 1, 0, 0, 0, 3600, AccessUtils::getOrCreateDefaultAccessGroup()->getId(), 0, '', '', '', '');
     $this->user = Factory::getUserFactory()->save($this->user);
     $this->apiKey = new ApiKey(0, 0, time() + 3600, 'mykey', 0, $this->user->getId(), 1);
     $this->apiKey = Factory::getApiKeyFactory()->save($this->apiKey);
