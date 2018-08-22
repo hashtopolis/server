b99aba7f9b67551a551e2e1796f571d05231d269
Sein Coray <s3inlc@hashes.org>
2018-08-22 09:09:49 +0200

    extended file test, fixed issue with filetype on user api creation

--- ci/tests/FileTest.class.php
+++ ci/tests/FileTest.class.php
@@ -15,6 +15,7 @@ class FileTest extends HashtopolisTest {
     $this->testListFilesEmpty();
     $this->testCreatingInlineFile();
     $this->testCreatedInlineFile();
+    $this->testCreatingFileTwice();
     HashtopolisTestFramework::log(HashtopolisTestFramework::LOG_INFO, $this->getTestName()." completed");
   }
 
@@ -62,6 +63,28 @@ class FileTest extends HashtopolisTest {
     }
   }
 
+  private function testCreatingFileTwice(){
+    $testFile = base64_encode("This is a test file content!");
+    $response = HashtopolisTestFramework::doRequest([
+      "section" => "file",
+      "request" => "addFile",
+      "filename" => "test.txt",
+      "fileType" => 0,
+      "source" => "inline",
+      "data" => $testFile,
+      "accessGroupId" => 1,
+      "accessKey" => "mykey"], HashtopolisTestFramework::REQUEST_UAPI);
+    if($response === false){
+      $this->testFailed("FileTest:testCreatingFileTwice", "Empty response");
+    }
+    else if($response['response'] != 'ERROR'){
+      $this->testFailed("FileTest:testCreatingFileTwice", "Response not ERROR");
+    }
+    else{
+      $this->testSuccess("FileTest:testCreatingFileTwice");
+    }
+  }
+
   private function testCreatedInlineFile(){
     $response = HashtopolisTestFramework::doRequest([
       "section" => "file",
@@ -80,6 +103,9 @@ class FileTest extends HashtopolisTest {
       $found = false;
       foreach($response['files'] as $file){
         if($file['filename'] == "test.txt"){
+          if($file['fileType'] != 0){
+            $this->testFailed("FileTest:testCreatedInlineFile", "Created file does not have same fileType");
+          }
           $found = true;
         }
       }
