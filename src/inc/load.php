47e4444c22cbfae08f8e8f974fb6ca6bfa0e944d
Sein Coray <s3inlc@hashes.org>
2018-08-31 10:54:26 +0200

    Changed version back to 0.8.0 on master to keep it separated from dev

--- src/inc/load.php
+++ src/inc/load.php
@@ -5,7 +5,7 @@ ini_set("display_errors", "0");
 
 session_start();
 
-$VERSION = "0.8.0+dev";
+$VERSION = "0.8.0";
 $HOST = @$_SERVER['HTTP_HOST'];
 if (strpos($HOST, ":") !== false) {
   $HOST = substr($HOST, 0, strpos($HOST, ":"));
