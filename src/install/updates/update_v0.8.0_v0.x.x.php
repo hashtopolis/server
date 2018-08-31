9102d528fd89c5e6c5a3e46a2d8e14a8cdaa8b03
Sein Coray <s3inlc@hashes.org>
2018-08-31 10:56:10 +0200

    added moving of telegram token

--- src/install/updates/update_v0.8.0_v0.x.x.php
+++ src/install/updates/update_v0.8.0_v0.x.x.php
@@ -1,6 +1,8 @@
 <?php
 use DBA\ConfigSection;
 use DBA\Factory;
+use DBA\Config;
+use DBA\QueryFilter;
 
 require_once(dirname(__FILE__) . "/../../inc/db.php");
 require_once(dirname(__FILE__) . "/../../dba/init.php");
@@ -14,5 +16,20 @@ echo "Check agent binaries... ";
 Util::checkAgentVersion("python", "0.2.0");
 Util::checkAgentVersion("csharp", "0.52.4");
 echo "\n";
+
+echo "Add new config section for noticiations... ";
+$configSection = new ConfigSection(7, 'Notifications');
+Factory::getConfigSectionFactory()->save($configSection);
+
+// moving telegram bot token setting
+$qF = new QueryFilter(Config::ITEM, DConfig::TELEGRAM_BOT_TOKEN, "=");
+$config = Factory::getConfigFactory()->filter([Factory::FILTER => $qF], true);
+if($config != null){ // just to be sure we check
+  $config->setConfigSectionId(7);
+  Factory::getConfigFactory()->update($config);
+}
+
+//$config = new Config(null, 7, DConfig::TELEGRAM_);
+echo "OK\n";
 
 echo "Update complete!\n";
