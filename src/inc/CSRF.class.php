f00415676024f7cfef6f2d47b9a93a5390400cac
Sein Coray <s3inlc@hashes.org>
2018-09-03 16:19:14 +0200

    adjusted csrf loading

--- src/inc/CSRF.class.php
+++ src/inc/CSRF.class.php
@@ -1,32 +1,34 @@
 <?php
 
 class CSRF {
-  const CSRF_SECRET = $PEPPER[3];
-  
   public static function init() {
-    if (!isset($_SESSION[CSRF::CSRF_SECRET])) {
+		global $PEPPER;
+
+    if (!isset($_SESSION[$PEPPER[3]])) {
       // generate a secret
-      $_SESSION[CSRF::CSRF_SECRET] = Util::randomString(40);
+      $_SESSION[$PEPPER[3]] = Util::randomString(40);
     }
     
     // set a token
     $key = Util::randomString(30);
-    UI::add('csrf', $key . ":" . base64_encode(hash("sha256", $key . $_SESSION[CSRF::CSRF_SECRET] . $key, true)));
+    UI::add('csrf', $key . ":" . base64_encode(hash("sha256", $key . $_SESSION[$PEPPER[3]] . $key, true)));
   }
   
   public static function check($csrf) {
+		global $PEPPER;
+
     $csrf = explode(":", $csrf);
     if (sizeof($csrf) != 2) {
       UI::addMessage(UI::ERROR, "Invalid form submission!");
       return false;
     }
-    else if (!isset($_SESSION[CSRF::CSRF_SECRET])) {
+    else if (!isset($_SESSION[$PEPPER[3]])) {
       UI::addMessage(UI::ERROR, "Invalid form submission!");
       return false;
     }
     
     $key = $csrf[0];
-    $check = base64_encode(hash("sha256", $key . $_SESSION[CSRF::CSRF_SECRET] . $key, true));
+    $check = base64_encode(hash("sha256", $key . $_SESSION[$PEPPER[3]] . $key, true));
     if ($check == $csrf[1]) {
       return true;
     }
