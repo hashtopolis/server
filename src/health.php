6deb81f00137d0d8e276359f9fae400725997c44
Sein Coray <s3inlc@hashes.org>
2018-09-05 13:58:41 +0200

    added overview page for health checks

--- src/health.php
+++ src/health.php
@@ -1,4 +1,7 @@
 <?php
+use DBA\Factory;
+use DBA\OrderFilter;
+use DBA\HealthCheck;
 
 require_once(dirname(__FILE__) . "/inc/load.php");
 
@@ -13,7 +16,13 @@ Template::loadInstance("health");
 UI::add('pageTitle', "Health Checks");
 Menu::get()->setActive("config_health");
 
-// TODO: load
+if(isset($_GET['id'])){
+  // TODO: implement
+}
+else{
+  $oF = new OrderFilter(HealthCheck::TIME, "DESC");
+  UI::add('checks', Factory::getHealthCheckFactory()->filter([Factory::ORDER => $oF]));
+}
 
 echo Template::getInstance()->render(UI::getObjects());
 
