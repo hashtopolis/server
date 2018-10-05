34407099031b3cfe708ebdb9c92c6e90a8187157
Sein Coray <s3inlc@hashes.org>
2018-10-05 13:53:24 +0200

    moving access utils include in upgrade script to right place

--- src/install/updates/update_v0.8.0_v0.x.x.php
+++ src/install/updates/update_v0.8.0_v0.x.x.php
@@ -8,8 +8,8 @@ if(!isset($TEST)){
   require_once(dirname(__FILE__) . "/../../inc/db.php");
   require_once(dirname(__FILE__) . "/../../dba/init.php");
   require_once(dirname(__FILE__) . "/../../inc/Util.class.php");
+  require_once(dirname(__FILE__) . "/../../inc/utils/AccessUtils.class.php");
 }
-require_once(dirname(__FILE__) . "/../../inc/utils/AccessUtils.class.php");
 require_once(dirname(__FILE__) . "/../../inc/defines/config.php");
 
 echo "NOTICE: After this update the Peppers for Encryption.class.php and CSRF.class.php are stored in the new config file. So if you didn't merge them you would have to put the old pepper values into inc/conf.php to make the log in working again. Read more on this on the specific release information.\n";
