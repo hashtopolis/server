2733ae5b45954a6d60c73deedb0e5c6f9e15957d
Sein Coray <s3inlc@hashes.org>
2018-10-30 10:16:04 +0100

    Currently removing csharp agent from list to avoid problems because it's not up-to-date

--- src/install/hashtopolis.sql
+++ src/install/hashtopolis.sql
@@ -112,8 +112,7 @@ CREATE TABLE `AgentBinary` (
 --
 
 INSERT INTO `AgentBinary` (`agentBinaryId`, `type`, `version`, `operatingSystems`, `filename`) VALUES
-  (1, 'csharp', '0.52.2', 'Windows, Linux(mono), OS X(mono)', 'hashtopolis.exe'),
-  (2, 'python', '0.2.0', 'Windows, Linux, OS X', 'hashtopolis.zip');
+  (1, 'python', '0.2.0', 'Windows, Linux, OS X', 'hashtopolis.zip');
 
 -- --------------------------------------------------------
 
