fc9f0daf0d6c23904b96e2721bb2a4c8c1770562
Sein Coray <s3inlc@hashes.org>
2018-11-12 14:51:24 +0100

    only showing graph if data available

--- src/tasks.php
+++ src/tasks.php
@@ -241,8 +241,9 @@ if (isset($_GET['id'])) {
   UI::add('fullAgents', $fullAgents);
   UI::add('pageTitle', "Task details for " . $task->getTaskName());
 
-    // load graph data
-    $data = Util::getSpeedDataSet($task->getId());
+  // load graph data
+  $data = Util::getSpeedDataSet($task->getId());
+  if(sizeof($data) > 0){
     $xlabels = [];
     $rawData = [];
     foreach($data as $key => $val){
@@ -259,6 +260,8 @@ if (isset($_GET['id'])) {
     ];
     UI::add("taskSpeedXLabels", json_encode($xlabels));
     UI::add("taskSpeed", json_encode($datasets));
+  }
+  UI::add('taskGraph', (sizeof($data) > 0)?1:0);
 }
 else if (isset($_GET['new'])) {
   AccessControl::getInstance()->checkPermission(array_merge(DAccessControl::RUN_TASK_ACCESS, DAccessControl::CREATE_TASK_ACCESS));
