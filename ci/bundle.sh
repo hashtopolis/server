7625d73a2218971701bbca200791e9a609460662
Sein Coray <s3inlc@hashes.org>
2018-11-15 17:46:46 +0100

    updated bundle script

--- ci/bundle.sh
+++ ci/bundle.sh
@@ -7,12 +7,12 @@ fi
 count=$(git log $(git describe --tags --abbrev=0)..HEAD --oneline | wc -l)
 if [ ${count} \> 0 ];
 then
-    sed -i -E 's/VERSION = "([0-9]+)\.([0-9]+)\.([0-9]+)"/VERSION = "\1.\2.\3\+'$count'"/g' ../src/inc/info.php
+    sed -i -E 's/BUILD = "repository"/BUILD = "'$count'"/g' ../src/inc/info.php
 fi;
 cd ../src/
 zip -r ../ci/server.zip * -x "*.gitignore" -x "*.txt" -x "*README.md" -x "*generator.php"
 cd ../ci/
 if [ ${count} \> 0 ];
 then
-    sed -i -E 's/VERSION = "([0-9]+)\.([0-9]+)\.([0-9]+)\+([0-9]+)"/VERSION = "\1.\2.\3"/g' ../src/inc/info.php
+    sed -i -E 's/BUILD = "'$count'"/BUILD = "repository"/g' ../src/inc/info.php
 fi;
