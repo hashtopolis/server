124c650cf80e6c3b98f825004eb7f2e78f8359c2
Sein Coray <s3inlc@hashes.org>
2018-11-16 10:50:03 +0100

    fixed bundle and added correct footer display for build

--- ci/bundle.sh
+++ ci/bundle.sh
@@ -7,12 +7,12 @@ fi
 count=$(git log $(git describe --tags --abbrev=0)..HEAD --oneline | wc -l)
 if [ ${count} \> 0 ];
 then
-    sed -i -E 's/BUILD = "repository"/BUILD = "'$count'"/g' ../src/inc/info.php
+    sed -i -E 's/BUILD = "repository"/BUILD = "'"$count"'"/g' ../src/inc/info.php
 fi;
 cd ../src/
 zip -r ../ci/server.zip * -x "*.gitignore" -x "*.txt" -x "*README.md" -x "*generator.php"
 cd ../ci/
 if [ ${count} \> 0 ];
 then
-    sed -i -E 's/BUILD = "'$count'"/BUILD = "repository"/g' ../src/inc/info.php
+    sed -i -E 's/BUILD = "'"$count"'"/BUILD = "repository"/g' ../src/inc/info.php
 fi;
