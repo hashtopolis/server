53ca519921803e7888d39fc2376fc098441666a9
Sein Coray <s3inlc@hashes.org>
2018-11-16 11:11:56 +0100

    build number also needs to be set for releases

--- ci/bundle.sh
+++ ci/bundle.sh
@@ -5,14 +5,8 @@ if [ -f server.zip ]; then
 fi
 
 count=$(git log $(git describe --tags --abbrev=0)..HEAD --oneline | wc -l | tr -d ' ')
-if [ ${count} \> 0 ];
-then
-    sed -i -E 's/BUILD = "repository"/BUILD = "'"$count"'"/g' ../src/inc/info.php
-fi;
+sed -i -E 's/BUILD = "repository"/BUILD = "'"$count"'"/g' ../src/inc/info.php
 cd ../src/
 zip -r ../ci/server.zip * -x "*.gitignore" -x "*.txt" -x "*README.md" -x "*generator.php"
 cd ../ci/
-if [ ${count} \> 0 ];
-then
-    sed -i -E 's/BUILD = "'"$count"'"/BUILD = "repository"/g' ../src/inc/info.php
-fi;
+sed -i -E 's/BUILD = "'"$count"'"/BUILD = "repository"/g' ../src/inc/info.php
