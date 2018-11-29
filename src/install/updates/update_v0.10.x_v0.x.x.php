32259bd1f33c86036e889d5363be4cdd6cc436b5
Sein Coray <s3inlc@hashes.org>
2018-11-29 10:20:52 +0100

    fixed config section for update

--- src/install/updates/update_v0.10.x_v0.x.x.php
+++ src/install/updates/update_v0.10.x_v0.x.x.php
@@ -13,9 +13,9 @@ require_once(dirname(__FILE__) . "/../../inc/defines/config.php");
 require_once(dirname(__FILE__) . "/../../inc/defines/log.php");
 
 if(!isset($PRESENT["v0.10.x_conf1"])){
-  $config = new Config(null, 1, DConfig::AGENT_TEMP_THRESHOLD_1, '70');
+  $config = new Config(null, 4, DConfig::AGENT_TEMP_THRESHOLD_1, '70');
   Factory::getConfigFactory()->save($config);
-  $config = new Config(null, 1, DConfig::AGENT_TEMP_THRESHOLD_2, '80');
+  $config = new Config(null, 4, DConfig::AGENT_TEMP_THRESHOLD_2, '80');
   Factory::getConfigFactory()->save($config);
   $EXECUTED["v0.10.x_conf1"] = true;
 }
