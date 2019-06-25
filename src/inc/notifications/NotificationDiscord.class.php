2ab3265733c79a8e388e3950eb18338afb96ce92
Sein Coray <s3inlc@hashes.org>
2019-06-25 15:22:30 +0200

    fixed code formatting and removed username as it is not needed

--- src/inc/notifications/NotificationDiscord.class.php
+++ src/inc/notifications/NotificationDiscord.class.php
@@ -3,29 +3,28 @@
 class HashtopolisNotificationDiscordWebhook extends HashtopolisNotification {
   protected     $receiver;
   public static $name = "Discord Webhook";
-
+  
   function getTemplateName() {
     return "notifications/discord";
   }
-
+  
   function getObjects() {
     return array();
   }
-
+  
   function sendMessage($message, $subject = "") {
-    $username = APP_NAME;
     $data = json_encode(array(
-          "content" => $message
-        )
-      );
-
+        "content" => $message
+      )
+    );
+    
     $ch = curl_init($this->receiver);
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
     curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $result = curl_exec($ch);
     curl_close($ch);
-
+    
     return $result;
   }
 }
