b203deca9c0a88b66a5eb4dad670eadbbc7c00ea
Sein Coray <s3inlc@hashes.org>
2019-06-26 10:41:23 +0200

    cosmetic updated on preprocessors page

--- src/inc/handlers/PreprocessorHandler.class.php
+++ src/inc/handlers/PreprocessorHandler.class.php
@@ -11,10 +11,12 @@ class PreprocessorHandler implements Handler {
         case DPreprocessorAction::ADD_PREPROCESSOR:
           AccessControl::getInstance()->checkPermission(DPreprocessorAction::ADD_PREPROCESSOR_PERM);
           PreprocessorUtils::addPreprocessor($_POST['name'], $_POST['binaryName'], $_POST['url'], $_POST['keyspaceCommand'], $_POST['skipCommand'], $_POST['limitCommand']);
+          UI::addMessage(UI::SUCCESS, "Added new preprocessor!");
           break;
         case DPreprocessorAction::DELETE_PREPROCESSOR:
           AccessControl::getInstance()->checkPermission(DPreprocessorAction::DELETE_PREPROCESSOR_PERM);
           PreprocessorUtils::delete($_POST['preprocessorId']);
+          UI::addMessage(UI::SUCCESS, "Deleted preprocessor successfully!");
           break;
         default:
           UI::addMessage(UI::ERROR, "Invalid action!");
