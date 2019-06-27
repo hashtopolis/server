cfb27477e9078249cf0b5e78cb8589332e8ec468
Sein Coray <s3inlc@hashes.org>
2019-06-27 14:49:54 +0200

    added new config value to sql setup

--- src/install/hashtopolis.sql
+++ src/install/hashtopolis.sql
@@ -170,7 +170,8 @@ INSERT INTO `Config` (`configId`, `configSectionId`, `item`, `value`) VALUES
   (72, 4, 'agentTempThreshold1', '70'),
   (73, 4, 'agentTempThreshold2', '80'),
   (74, 4, 'agentUtilThreshold1', '90'),
-  (75, 4, 'agentUtilThreshold2', '75');
+  (75, 4, 'agentUtilThreshold2', '75'),
+  (76, 3, 'uApiSendTaskIsComplete', '0');
 
 CREATE TABLE `ConfigSection` (
   `configSectionId` INT(11)      NOT NULL,
