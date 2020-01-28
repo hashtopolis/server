53ea543f7a3f03d48871c57df0010d57162051e9
MilzoCSP <overchecking@gmail.com>
2020-01-28 13:24:08 +0000

    Update NotificationDiscord.class.php

--- src/inc/notifications/NotificationDiscord.class.php
+++ src/inc/notifications/NotificationDiscord.class.php
@@ -3,19 +3,40 @@
 class HashtopolisNotificationDiscordWebhook extends HashtopolisNotification {
   protected     $receiver;
   public static $name = "Discord Webhook";
-  
+
   function getTemplateName() {
     return "notifications/discord";
   }
-  
+
   function getObjects() {
     return array();
   }
-  
+
   function sendMessage($message, $subject = "") {
     $json_data = array(
-      'content'=>"$message"
-  );
+        "content" => $message
+    );
+
+    $ch = curl_init($this->receiver);
+
+    if (SConfig::getInstance()->getVal(DConfig::NOTIFICATIONS_PROXY_ENABLE) == 1) {
+      curl_setopt($ch, CURLOPT_PROXY, SConfig::getInstance()->getVal(DConfig::NOTIFICATIONS_PROXY_SERVER));
+      curl_setopt($ch, CURLOPT_PROXYPORT, SConfig::getInstance()->getVal(DConfig::NOTIFICATIONS_PROXY_PORT));
+      $type = CURLPROXY_HTTP;
+      switch (SConfig::getInstance()->getVal(DConfig::NOTIFICATIONS_PROXY_TYPE)) {
+        case DProxyTypes::HTTPS:
+          $type = CURLPROXY_HTTPS;
+          break;
+        case DProxyTypes::SOCKS4:
+          $type = CURLPROXY_SOCKS4;
+          break;
+        case DProxyTypes::SOCKS5:
+          $type = CURLPROXY_SOCKS5;
+          break;
+      }
+      curl_setopt($ch, CURLOPT_PROXYTYPE, $type);
+      curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, "true");
+    }
 
     $make_json = json_encode($json_data);
     $ch = curl_init($this->receiver);
@@ -34,4 +55,3 @@ class HashtopolisNotificationDiscordWebhook extends HashtopolisNotification {
 }
 
 HashtopolisNotification::add('Discord Webhook', new HashtopolisNotificationDiscordWebhook());
-
