6037ed6c0c2dcf05e606a05cc77f333fe29b147a
Sein Coray <s3inlc@hashes.org>
2020-02-03 15:53:32 +0100

    fixed formatting in discord notification

--- src/inc/notifications/NotificationDiscord.class.php
+++ src/inc/notifications/NotificationDiscord.class.php
@@ -3,22 +3,22 @@
 class HashtopolisNotificationDiscordWebhook extends HashtopolisNotification {
   protected     $receiver;
   public static $name = "Discord Webhook";
-
+  
   function getTemplateName() {
     return "notifications/discord";
   }
-
+  
   function getObjects() {
     return array();
   }
-
+  
   function sendMessage($message, $subject = "") {
     $json_data = array(
-        "content" => $message
+      "content" => $message
     );
-
+    
     $ch = curl_init($this->receiver);
-
+    
     if (SConfig::getInstance()->getVal(DConfig::NOTIFICATIONS_PROXY_ENABLE) == 1) {
       curl_setopt($ch, CURLOPT_PROXY, SConfig::getInstance()->getVal(DConfig::NOTIFICATIONS_PROXY_SERVER));
       curl_setopt($ch, CURLOPT_PROXYPORT, SConfig::getInstance()->getVal(DConfig::NOTIFICATIONS_PROXY_PORT));
@@ -37,20 +37,18 @@ class HashtopolisNotificationDiscordWebhook extends HashtopolisNotification {
       curl_setopt($ch, CURLOPT_PROXYTYPE, $type);
       curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, "true");
     }
-
+    
     $make_json = json_encode($json_data);
     $ch = curl_init($this->receiver);
-    curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
-    curl_setopt( $ch, CURLOPT_POST, 1);
-    curl_setopt( $ch, CURLOPT_POSTFIELDS, $make_json);
-    curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
-    curl_setopt( $ch, CURLOPT_HEADER, 0);
-    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
-
-    $response = curl_exec( $ch );
-
-    return $response;
-
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $make_json);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  
+    return curl_exec($ch);
+    
   }
 }
 
