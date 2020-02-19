d9845bd4ec8849bac2bfa2d75cf80cd63c75bc05
Sein Coray <s.coray@unibas.ch>
2020-02-19 09:47:16 +0100

    added missing release entry

--- ci/HashtopolisTest.class.php
+++ ci/HashtopolisTest.class.php
@@ -27,7 +27,8 @@ abstract class HashtopolisTest {
     "0.8.0"  => "47e4444c22cbfae08f8e8f974fb6ca6bfa0e944d",
     "0.9.0"  => "cd2951cd10552114c44c29962ac22efcbabf57c7",
     "0.10.0" => "cdc674f4f375115debd556feda4e7f6e4614a2c6",
-    "0.11.0" => "9cdbffcffb46da613c14d2f46266c1c3672e61e7"
+    "0.11.0" => "9cdbffcffb46da613c14d2f46266c1c3672e61e7",
+    "0.12.0" => "b53f529f664c866e4d22f5cb348d22eb6f542901"
   ];
   
   public function initAndUpgrade($fromVersion) {
