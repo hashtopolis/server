3f1f236a8a18ee608cba7c4fe2e1f7864be5df6c
Sein Coray <s.coray@unibas.ch>
2020-02-21 10:18:32 +0100

    fixing preprocessor access issue on handler

--- src/inc/defines/preprocessors.php
+++ src/inc/defines/preprocessors.php
@@ -2,13 +2,13 @@
 
 class DPreprocessorAction {
   const DELETE_PREPROCESSOR      = "deletePreprocessor";
-  const DELETE_PREPROCESSOR_PERM = DAccessControl::PREPROCESSORS_ACCESS;
+  const DELETE_PREPROCESSOR_PERM = DAccessControl::SERVER_CONFIG_ACCESS;
   
   const ADD_PREPROCESSOR      = "addPreprocessor";
-  const ADD_PREPROCESSOR_PERM = DAccessControl::PREPROCESSORS_ACCESS;
+  const ADD_PREPROCESSOR_PERM = DAccessControl::SERVER_CONFIG_ACCESS;
   
   const EDIT_PREPROCESSOR      = "editPreprocessor";
-  const EDIT_PREPROCESSOR_PERM = DAccessControl::PREPROCESSORS_ACCESS;
+  const EDIT_PREPROCESSOR_PERM = DAccessControl::SERVER_CONFIG_ACCESS;
 }
 
 
