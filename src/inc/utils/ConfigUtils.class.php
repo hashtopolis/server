d9f7af510ebd2cb70f26e98469e75d293c9f758d
Sein Coray <s3inlc@hashes.org>
2020-06-21 17:18:11 +0200

    fixing cache rebuild for superhashlists

--- src/inc/utils/ConfigUtils.class.php
+++ src/inc/utils/ConfigUtils.class.php
@@ -168,7 +168,7 @@ class ConfigUtils {
       }
       if ($cracked != $superHashlist->getCracked()) {
         $correctedHashlists++;
-        Factory::getHashlistFactory()->set($superHashlist, Hashlist::CRACKED, $count);
+        Factory::getHashlistFactory()->set($superHashlist, Hashlist::CRACKED, $cracked);
       }
     }
     Factory::getAgentFactory()->getDB()->commit();
