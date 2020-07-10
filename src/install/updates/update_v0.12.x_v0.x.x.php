83c1631879eb257b60ed68f90eff85cecaf70508
Sein Coray <s3inlc@hashes.org>
2020-07-10 13:56:53 +0200

    removing stable update track from agent binaries

--- src/install/updates/update_v0.12.x_v0.x.x.php
+++ src/install/updates/update_v0.12.x_v0.x.x.php
@@ -52,3 +52,14 @@ if (!isset($PRESENT["v0.12.x_agentBinaries"])) {
   $EXECUTED["v0.12.x_agentBinaries"] = true;
 }
 
+if (!isset($PRESENT["v0.12.x_agentBinariesTrack"])) {
+  $agentBinaries = Factory::getAgentBinaryFactory()->filter([]);
+  foreach ($agentBinaries as $agentBinary) {
+    if ($agentBinary->getUpdateTrack() == 'stable') {
+      $agentBinary->setUpdateTrack('dev');
+    }
+    Factory::getAgentBinaryFactory()->update($agentBinary);
+  }
+  $EXECUTED["v0.12.x_agentBinariesTrack"] = true;
+}
+
