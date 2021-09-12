106f2deef48402df2768218ea10b0d6614683d9e
elreydetoda <10230166+elreydetoda@users.noreply.github.com>
2021-09-11 20:38:48 -0400

    simplify gitignore and entrypoint script (based on review)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,18 +1,8 @@
 #!/usr/bin/env bash
 
-# https://elrey.casa/bash/scripting/harden
-set -${-//[sc]/}eu${DEBUG:+xv}o pipefail
-
-function main(){
-  if chown -R www-data:www-data /var/www/html/ ; then
-    echo "All files were chown'ed properly"
-  else
-    echo "There were some errors while chown'ing files"
-  fi
-  docker-php-entrypoint apache2-foreground
-}
-
-# https://elrey.casa/bash/scripting/main
-if [[ "${0}" = "${BASH_SOURCE[0]:-bash}" ]] ; then
-  main "${@}"
+if chown -R www-data:www-data /var/www/html/ ; then
+  echo "All files were chown'ed properly"
+else
+  echo "There were some errors while chown'ing files"
 fi
+docker-php-entrypoint apache2-foreground
