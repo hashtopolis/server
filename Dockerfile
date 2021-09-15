ef4262abe32c9155910cf97cd88b19a95db03c8c
elreydetoda <10230166+elreydetoda@users.noreply.github.com>
2021-09-15 01:21:19 -0400

    Apply suggestions from code review
    - moving php.ini file and adding some custom options to $PHP_INI_DIR/conf.d/custom.ini
    - bind mounting src/inc/ to handle config.php issue
    - modifying entrypoint.sh to handle chown'ing necessary volume mounts w/descriptive echos
    
    Co-authored-by: Romke van Dijk <github.com@ocbios.nl>

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,15 @@ ENTRYPOINT [ "docker-entrypoint.sh" ]
 
 COPY docker-entrypoint.sh /usr/local/bin/
 
-RUN set -ex && docker-php-ext-install pdo_mysql \
-  && ln -s /usr/local/etc/php/php.ini-production /usr/local/etc/php/php.ini
+RUN set -ex && docker-php-ext-install pdo_mysql 
+
+RUN mv "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/php.ini"
+
+RUN { \
+    touch "$PHP_INI_DIR/conf.d/custom.ini"; \
+    echo "memory_limit = 256m"; \
+    echo "upload_max_filesize = 256m"; \
+    echo "max_execution_time = 60"; \
+} > "$PHP_INI_DIR/conf.d/custom.ini"
+
 COPY --chown=www-data:www-data ./src/ /var/www/html/
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
 
-if chown -R www-data:www-data /var/www/html/ ; then
-  echo "All files were chown'ed properly"
-else
-  echo "There were some errors while chown'ing files"
+if test -f /var/www/html/inc/conf.php; then
+  if chown www-data:www-data /var/www/html/inc/conf.php ; then
+    echo "conf.php was chown'ed properly"
+  else
+    echo "There were some errors while chown'ing conf.php"
+  fi
 fi
+
+if chown -R www-data:www-data /var/www/html/import/ ; then
+  echo "import folder was chown'ed properly"
+else
+  echo "There were some errors while chown'ing the import folder"
+fi
+
+if chown -R www-data:www-data /var/www/html/files/ ; then
+  echo "files folder was chown'ed properly"
+else
+  echo "There were some errors while chown'ing files folder"
+fi
+
 docker-php-entrypoint apache2-foreground
