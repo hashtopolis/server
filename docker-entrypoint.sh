f3c2dd04c1e9a9a03a1f93cedd00f255cc3ebd22
elreydetoda <10230166+elreydetoda@users.noreply.github.com>
2021-09-15 01:26:13 -0400

    adding exits for entrypoint script errors

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,7 @@ if test -f /var/www/html/inc/conf.php; then
     echo "conf.php was chown'ed properly"
   else
     echo "There were some errors while chown'ing conf.php"
+    exit 1
   fi
 fi
 
@@ -12,12 +13,14 @@ if chown -R www-data:www-data /var/www/html/import/ ; then
   echo "import folder was chown'ed properly"
 else
   echo "There were some errors while chown'ing the import folder"
+  exit 1
 fi
 
 if chown -R www-data:www-data /var/www/html/files/ ; then
   echo "files folder was chown'ed properly"
 else
   echo "There were some errors while chown'ing files folder"
+  exit 1
 fi
 
 docker-php-entrypoint apache2-foreground
