8714cf90335e1e256bf1c814b096b78b6da09d35
Romke van Dijk <github.com@ocbios.nl>
2022-04-06 10:55:31 +0200

    Auto login and token creation

--- .devcontainer/adduser.php
+++ .devcontainer/adduser.php
@@ -18,10 +18,10 @@ require_once(dirname(__FILE__) . "/../src/inc/load.php");
 
     $pepper = array(Util::randomString(50), Util::randomString(50), Util::randomString(50));
     $key = Util::randomString(40);
-    $conf = file_get_contents(dirname(__FILE__) . "/../inc/conf.php");
+    $conf = file_get_contents(dirname(__FILE__) . "/../src/inc/conf.php");
     $conf = str_replace("__PEPPER1__", $pepper[0], str_replace("__PEPPER2__", $pepper[1], str_replace("__PEPPER3__", $pepper[2], $conf)));
     $conf = str_replace("__CSRF__", $key, $conf);
-    file_put_contents(dirname(__FILE__) . "/../inc/conf.php", $conf);
+    file_put_contents(dirname(__FILE__) . "/../src/inc/conf.php", $conf);
     
         Factory::getAgentFactory()->getDB()->beginTransaction();
         
@@ -43,5 +43,5 @@ require_once(dirname(__FILE__) . "/../src/inc/load.php");
         
         Factory::getAgentFactory()->getDB()->commit();
 
-
-
+        // Create voucher for dev agent
+        AgentUtils::createVoucher("devvoucher");
--- .devcontainer/docker-entrypoint.sh
+++ .devcontainer/docker-entrypoint.sh
@@ -1,6 +1,50 @@
 #!/bin/sh
-/usr/bin/mysql -uhashtopolis -phashtopolis -hdb -o < /var/www/html/src/install/hashtopolis.sql
 
-cd /var/www/html/src && /usr/local/bin/php ../.devcontainer/adduser.php
+if [ ! -f /var/www/html/src/inc/conf.php ];
+then
+  echo "Testing database."
+  MYSQL="mysql -uhashtopolis -phashtopolis -hdb"
+	$MYSQL -e "SELECT 1"
+	ERROR=$?
+
+  while [ $ERROR -ne 0 ];
+  do
+    echo "Failed connecting to the database. Sleeping 5s"
+    sleep 5
+    $MYSQL -e "SELECT 1"
+	  ERROR=$?
+  done
+
+  $MYSQL -Dhashtopolis --verbose < /var/www/html/src/install/hashtopolis.sql
+
+  if [ $? -eq 0 ];
+  then
+      echo "<?php
+
+//START CONFIG
+\$CONN['user'] = 'hashtopolis';
+\$CONN['pass'] = 'hashtopolis';
+\$CONN['server'] = 'db';
+\$CONN['db'] = 'hashtopolis';
+\$CONN['port'] = '3306';
+
+\$PEPPER = [
+  \"__PEPPER1__\",
+  \"__PEPPER2__\",
+  \"__PEPPER3__\",
+  \"__CSRF__\"
+];
+
+\$INSTALL = true; //set this to true if you config the mysql and setup manually
+" > /var/www/html/src/inc/conf.php
+    cd /var/www/html/src && /usr/local/bin/php ../.devcontainer/adduser.php
+  else
+    echo "Error setting up database."
+  fi
+
+
+
+fi
+
 
 docker-php-entrypoint apache2-foreground
