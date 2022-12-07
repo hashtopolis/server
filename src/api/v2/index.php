04c98eedbe57df0725b9aa7e4207b27038b1eeca
Rick van der Zwet <info@rickvanderzwet.nl>
2022-12-07 20:04:44 +0000

    Rename to extra functions structure

--- src/api/v2/index.php
+++ src/api/v2/index.php
@@ -203,6 +203,7 @@ require __DIR__ . "/../../inc/apiv2/hashlists.routes.php";
 require __DIR__ . "/../../inc/apiv2/hashtypes.routes.php";
 require __DIR__ . "/../../inc/apiv2/logentry.routes.php";
 require __DIR__ . "/../../inc/apiv2/files.routes.php";
+require __DIR__ . "/../../inc/apiv2/files.upload.routes.php";
 
 
 $errorMiddleware = $app->addErrorMiddleware(true, true, true);
