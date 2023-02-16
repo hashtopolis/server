f8bcc4ea04e6a4df2b45fd7204374c875fdeebfb
Romke van Dijk <github.com@ocbios.nl>
2023-02-16 14:31:17 +0100

    Add hashlist tests

--- ci/apiv2/test_hashlists.py
+++ ci/apiv2/test_hashlists.py
@@ -4,121 +4,77 @@
 # PoC testing/development framework for APIv2
 # Written in python to work on creation of hashtopolis APIv2 python binding.
 #
-
 import json
-import requests
 import unittest
 import datetime
 from pathlib import Path
 
-import utils
+from hashtopolis import Hashlist 
+from hashtopolis import Task
 
 
-class Hashlists(utils.TestBase):
-    def getBaseURI(self):
-        return '/ui/hashlists'
-
-    def do_create(self, payload, required_status_code):
-        uri = self.getURI()
-        headers = self._headers
-
-        r = requests.post(uri, headers=headers, data=json.dumps(payload))
-        self.assertEqual(r.status_code, required_status_code, msg=r.text)
-
-        if required_status_code < 300:
-            retval = r.json()
-            self.assertGreater(retval['hashlistId'], 0)
-            return r.json()
-        else:
-            return None
-
-
-    def do_create_one(self):
+class HashlistTest(unittest.TestCase):
+    def test_create_hashlist(self):
         p = Path(__file__).parent.joinpath('create_hashlist_001.json')
         payload = json.loads(p.read_text('UTF-8'))
-        return self.do_create(payload, 201)
+        hashlist = Hashlist(**payload)
+        obj = hashlist.save()
 
+        obj = Hashlist.objects.get(hashlistId=hashlist.id)
 
-    def test_create_wildcard(self):
-        for p in sorted(Path(__file__).parent.glob('create_hashlist_*.json')):
-            payload = json.loads(p.read_text('UTF-8'))
-            self.do_create(payload, 201)
+        assert obj.name == payload.get('name')
 
+        hashlist.delete()
+    
+    def test_patch_hashlist(self):
+        p = Path(__file__).parent.joinpath('create_hashlist_001.json')
+        payload = json.loads(p.read_text('UTF-8'))
+        hashlist = Hashlist(**payload)
+        hashlist.save()
 
-    def test_create_binary_without_body(self):
-        """ Should fail, since no sourceData is present """
-        payload = json.loads('''
-        {
-          "name": "Hashlist-with-binary-format",
-          "hashTypeId": 10,
-          "format": 2,
-          "separator": ";",
-          "isSalted": false,
-          "isHexSalt": false,
-          "accessGroupId": 1,
-          "useBrain": false,
-          "brainFeatures": 3,
-          "notes": "gj",
-          "sourceType": "paste",
-          "sourceData": "",
-          "hashCount": 0,
-          "cracked": 0,
-          "isArchived": false,
-          "isSecret": true
-        }
-        ''')
-        self.do_create(payload, 500)
-
-
-    def test_get(self):
-        uri = self.getURI()
-        headers = self._headers
-        payload = {}
-
-        r = requests.get(uri, headers=headers, data=json.dumps(payload))
-        self.assertEqual(r.status_code, 201, msg=r.text)
-
-
-    def test_create_one(self):
-        obj = self.do_create_one()
-        uri = self.getURI(obj)
-        headers = self._headers
-        payload = {}
-
-        r = requests.get(uri, headers=headers, data=json.dumps(payload))
-        self.assertEqual(r.status_code, 201, msg=r.text)
-
-
-    def do_patch(self, payload, required_status_code, hashlist_id=1):
-        obj = self.do_create_one()
-        uri = self.getURI(obj)
-        headers = self._headers
-
-        r = requests.patch(uri, headers=headers, data=json.dumps(payload))
-        self.assertEqual(r.status_code, required_status_code, msg=r.text)
-
-        if required_status_code != 500:
-            for key in payload.keys():
-                self.assertEqual(r.json()[key], payload[key], msg=r.text)
-
-
-    def test_patch(self):
         stamp = datetime.datetime.now().isoformat()
-        payload = {
-            'name': f'MyList-{stamp}',
-        }
-        self.do_patch(payload, 201)
+        obj_name = f'Dummy Hashlist - {stamp}'
+        hashlist.name = obj_name
+        hashlist.save()
 
+        obj = Hashlist.objects.get(hashlistId=hashlist.id)
+        assert obj.name == obj_name
 
-    def test_patch_null(self):
-        # Change to null 
-        payload = {
-                'name': None,
-        }
-        self.do_patch(payload, 500)
+        hashlist.delete()
 
+    def test_delete_hashlist(self):
+        p = Path(__file__).parent.joinpath('create_hashlist_001.json')
+        payload = json.loads(p.read_text('UTF-8'))
+        hashlist = Hashlist(**payload)
+        hashlist.save()
 
+        id = hashlist.id
 
+        hashlist.delete()
+        objs = Hashlist.objects.filter(hashlistId=id)
+
+        assert objs == []
+
+    def test_exception(self):
+        p = Path(__file__).parent.joinpath('create_hashlist_002.json')
+        payload = json.loads(p.read_text('UTF-8'))
+        hashlist = Hashlist(**payload)
+        hashlist.save()
+
+        with self.assertRaises(AttributeError):
+            hashlist.id
+
+        objs = Hashlist.objects.filter(hashtlistName=payload.get('name'))
+        assert objs == []
+
+    #TODO: Expand support
+    # def test_expand(self):
+    #     p = Path(__file__).parent.joinpath('create_hashlist_001.json')
+    #     payload = json.loads(p.read_text('UTF-8'))
+    #     hashlist = Hashlist(**payload)
+    #     hashlist.save()
+
+    #     objs = Hashlist.objects
 
 if __name__ == '__main__':
     unittest.main()
