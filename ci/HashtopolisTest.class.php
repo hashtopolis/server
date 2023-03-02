f8f9851e3e651a9188b27567f7adab0dd8fd6223
Romke van Dijk <github.com@ocbios.nl>
2023-03-02 08:00:53 +0100

    Add test for rulesplit bug. WIP

--- ci/HashtopolisTest.class.php
+++ ci/HashtopolisTest.class.php
@@ -87,10 +87,10 @@ abstract class HashtopolisTest {
     Factory::getAccessGroupUserFactory()->save($accessGroup);
     $this->apiKey = new ApiKey(null, 0, time() + 3600, 'mykey', 0, $this->user->getId(), 1);
     $this->apiKey = Factory::getApiKeyFactory()->save($this->apiKey);
-    $versionStore = new StoredValue("version", ($version == 'master') ? explode("+", $VERSION)[0] : $version);
-    Factory::getStoredValueFactory()->save($versionStore);
-    $buildStore = new StoredValue("build", ($version == 'master') ? Util::getGitCommit(true) : $this->RELEASES[$version]);
-    Factory::getStoredValueFactory()->save($buildStore);
+    // $versionStore = new StoredValue("version", ($version == 'master') ? explode("+", $VERSION)[0] : $version);
+    // Factory::getStoredValueFactory()->save($versionStore);
+    // $buildStore = new StoredValue("build", ($version == 'master') ? Util::getGitCommit(true) : $this->RELEASES[$version]);
+    // Factory::getStoredValueFactory()->save($buildStore);
   }
   
   abstract function run();
---
+++ ci/tests/integration/RuleSplitTest.class.php
@@ -0,0 +1,265 @@
+<?php
+
+
+class RuleSplitTest extends HashtopolisTest {
+  protected $minVersion = "0.12.0";
+  protected $maxVersion = "master";
+  protected $runType    = HashtopolisTest::RUN_FAST;
+
+  public function init($version) {
+    HashtopolisTestFramework::log(HashtopolisTestFramework::LOG_INFO, "Initializing " . $this->getTestName() . "...");
+    parent::init($version);
+  }
+
+  private function prepare() {
+    $status = true;
+    // add some files
+    $status &= $this->addFile("example.dict", 0);
+    $status &= $this->addFile("best64.rule", 1);
+
+    if (!$status) {
+      HashtopolisTestFramework::log(HashtopolisTestFramework::LOG_ERROR, "Some initialization failed, most likely tests will fail!");
+    }
+  }
+
+  private function cleanup() {
+    $status = true;
+    // delete the created tasks
+    $status &= $this->deleteTaskIfExists("task-1");
+    $status &= $this->deleteTaskIfExists("task-2");
+
+    // remove the added files
+    $status &= $this->deleteFileIfExists("example.dict");
+    $status &= $this->deleteFileIfExists("best64.rule");
+
+    if (!$status) {
+      HashtopolisTestFramework::log(HashtopolisTestFramework::LOG_ERROR, "Some cleanup failed, deleting task or deleting files not succesful!");
+    }
+  }
+
+  public function run() {
+    HashtopolisTestFramework::log(HashtopolisTestFramework::LOG_INFO, "Running " . $this->getTestName() . "...");
+    $this->prepare();
+    try {
+      $this->testRuleSplit();
+    }
+    finally {
+      $this->cleanup();
+    }
+    HashtopolisTestFramework::log(HashtopolisTestFramework::LOG_INFO, $this->getTestName() . " completed");
+  }
+
+  private function testRuleSplit() {
+    # example.dict / best64.rule
+    $file_id1 = $this->getFile('example.dict');
+    $file_id2 = $this->getFile('best64.rule');
+
+    # Create hashlist
+    $response = $this->addHashlist(["isSecure" => false]);
+    $hashlistId = $response["hashlistId"];
+  
+    # Create task with rule/wordlist
+    $response = $this->createTask([
+      "name" => "task-1",
+      "hashlistId" => $hashlistId,
+      "attackCmd" => "#HL# -a 0 -r best64.rule example.dict",
+      "priority" => 100,
+      "color" => "FFFFFF",
+      "crackerVersionId" => 1,
+      "files" => [$file_id1, $file_id2]]
+    );
+
+    $task1Id = $response["taskId"];
+
+    # Enable rulesplit
+    $this->setConfig('ruleSplitDisable', '0');
+
+    # Create agent
+    $agent = $this->createAgent("agent-1");
+
+    HashtopolisTestFramework::doRequest(["action" => "getTask", "token" => $agent["token"]]);
+
+    # keyspace
+    $response = HashtopolisTestFramework::doRequest([
+      "action" => "sendKeyspace",
+      "taskId" => $task1Id,
+      "token" => $agent["token"],
+      "keyspace" => 10000000]
+    );
+    # benchmark
+    $response = HashtopolisTestFramework::doRequest([
+      "action" => "sendBenchmark",
+      "taskId" => $task1Id,
+      "token" => $agent["token"],
+      "type" => "speed",
+      "result" => '2000:50']
+    );
+    # getChunk
+    $response = HashtopolisTestFramework::doRequest([
+      "action" => "getChunk",
+      "taskId" => $task1Id,
+      "token" => $agent["token"]]
+    );
+    $chunkId = $response['chunkId'];
+
+    # probeer de file te splitten
+    $response = HashtopolisTestFramework::doRequest([
+      "action" => "sendProgress",
+      "chunkId" => $chunkId,
+      "token" => $agent["token"],
+      "keyspaceProgress" => 500,
+      "relativeProgress" => "4545",
+      "speed" => 500,
+      "state" => 3,
+      "cracks" => [],
+      "gpuTemp" => [67],
+      "gpuUtil" => [99]
+    ]);
+
+    $this->testSuccess("RuleSplitTest:testRuleSplit()");
+  }
+
+  private function createTask($values = []) {
+    $query = [
+      "section" => "task",
+      "request" => "createTask",
+      "name" => "",
+      "hashlistId" => 0,
+      "attackCmd" => "",
+      "chunksize" => 600,
+      "statusTimer" => 5,
+      "benchmarkType" => "speed",
+      "color" => "",
+      "isCpuOnly" => false,
+      "isSmall" => false,
+      "skip" => 0,
+      "crackerVersionId" => 0,
+      "files" => [],
+      "priority" => 0,
+      "maxAgents" => 0,
+      "preprocessorId" => 0,
+      "preprocessorCommand" => "",
+      "accessKey" => "mykey"
+    ];
+    foreach ($values as $key => $value) {
+      $query[$key] = $value;
+    }
+    return HashtopolisTestFramework::doRequest($query, HashtopolisTestFramework::REQUEST_UAPI);
+  }
+
+  private function addHashlist($values = []) {
+    $data = base64_encode(file_get_contents(dirname(__FILE__) . "/../../files/example0.hash"));
+    $query = [
+      "section" => "hashlist",
+      "request" => "createHashlist",
+      "name" => "Test Rule Split",
+      "isSalted" => false,
+      "isSecret" => true,
+      "isHexSalt" => false,
+      "separator" => ":",
+      "format" => 0,
+      "hashtypeId" => 0,
+      "accessGroupId" => 1,
+      "data" => $data,
+      "useBrain" => false,
+      "brainFeatures" => 0,
+      "accessKey" => "mykey"
+    ];
+    foreach ($values as $key => $value) {
+      $query[$key] = $value;
+    };
+    return HashtopolisTestFramework::doRequest($query, HashtopolisTestFramework::REQUEST_UAPI);
+  }
+
+  private function getFile($name) {
+    $response = HashtopolisTestFramework::doRequest([
+      "section" => "file",
+      "request" => "listFiles",
+      "accessKey" => "mykey"
+    ], HashtopolisTestFramework::REQUEST_UAPI
+    );
+    if ($response['response'] == 'OK') {
+      foreach ($response['files'] as $file) {
+        if ($file['filename'] == $name) {
+          return $file['fileId'];
+        }
+      }
+    }
+    return false;
+  }
+
+  private function addFile($name, $type) {
+    $response = HashtopolisTestFramework::doRequest([
+      "section" => "file",
+      "request" => "addFile",
+      "filename" => $name,
+      "fileType" => $type,
+      "source" => "inline",
+      "data" => base64_encode(file_get_contents(dirname(__FILE__) . "/../../files/$name")),
+      "accessGroupId" => 1,
+      "accessKey" => "mykey"
+    ], HashtopolisTestFramework::REQUEST_UAPI
+    );
+    if ($response === false) {
+      return false;
+    }
+    else if ($response['response'] != 'OK') {
+      return false;
+    }
+    return true;
+  }
+
+  private function createAgent($name) {
+    $voucher = "voucher-" . $name;
+    $response = HashtopolisTestFramework::doRequest([
+      "section" => "agent",
+      "request" => "createVoucher",
+      "voucher" => $voucher,
+      "accessKey" => "mykey"
+    ], HashtopolisTestFramework::REQUEST_UAPI);
+    $response = HashtopolisTestFramework::doRequest([
+      "action" => "register",
+      "voucher" => $voucher,
+      "name" => $name]);
+    $token = $response['token'];
+    $response = HashtopolisTestFramework::doRequest([
+      "section" => "agent",
+      "request" => "listAgents",
+      "accessKey" => "mykey"
+    ], HashtopolisTestFramework::REQUEST_UAPI);
+    $agent = current(array_filter($response["agents"], function($a) use ($name) {
+      return $a["name"] == $name;
+    }));
+    $response = HashtopolisTestFramework::doRequest([
+      "section" => "agent",
+      "request" => "setTrusted",
+      "accessKey" => "mykey",
+      "agentId" => $agent["agentId"],
+      "trusted" => true
+    ], HashtopolisTestFramework::REQUEST_UAPI);
+    return array("agentId" => $agent["agentId"], "token" => $token);
+  }
+
+  private function setConfig($item, $value, $force = false) {
+    $response = HashtopolisTestFramework::doRequest([
+      "section" => "config",
+      "request" => "setConfig",
+      "configItem" => $item,
+      "value" => $value,
+      "force" => $force,
+      "accessKey" => "mykey"
+    ], HashtopolisTestFramework::REQUEST_UAPI
+    );
+    return $response;
+  }
+
+  private function sendProgress($token, $chunkId, $keyspaceProgress, $relateiveProgress, $speed) {
+
+  }
+  
+  public function getTestName() {
+    return "Rule Split Test";
+  }
+}
+
+HashtopolisTestFramework::register(new  RuleSplitTest());
