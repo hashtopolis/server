c2c054e2d7c47c37284de5b1b4518554ce5dddd9
Romke van Dijk <github.com@ocbios.nl>
2023-03-20 13:23:17 +0000

    Fix check if the zap or hash have been set larger than TEXT

--- src/install/updates/update_v0.13.x_v0.x.x.php
+++ src/install/updates/update_v0.13.x_v0.x.x.php
@@ -12,9 +12,18 @@ if (!isset($TEST)) {
 require_once(dirname(__FILE__) . "/../../inc/defines/config.php");
 require_once(dirname(__FILE__) . "/../../inc/defines/log.php");
 
+
 if (!isset($PRESENT["v0.13.x_hash_length"])) {
-  Factory::getAgentFactory()->getDB()->query("ALTER TABLE `Hash` MODIFY `hash` MEDIUMTEXT NOT NULL;");
-  Factory::getAgentFactory()->getDB()->query("ALTER TABLE `Zap` MODIFY `hash` MEDIUMTEXT NOT NULL;");
+  $conn = Factory::getAgentFactory()->getDB();
+
+  $hash_column_length = $conn->query("SELECT hash FROM Hash")->getColumnMeta(0)['len'];
+  $zap_column_length = $conn->query("SELECT hash FROM Zap")->getColumnMeta(0)['len'];
+  // TEXT == 65535
+  if ($hash_column_length <= 65535) {
+    Factory::getAgentFactory()->getDB()->query("ALTER TABLE `Hash` MODIFY `hash` MEDIUMTEXT NOT NULL;");
+  }
+  if ($zap_column_length <= 65535) {
+    Factory::getAgentFactory()->getDB()->query("ALTER TABLE `Zap` MODIFY `hash` MEDIUMTEXT NOT NULL;");
+  }
   $EXECUTED["v0.13.x_hash_length"] = true;
 }
-
