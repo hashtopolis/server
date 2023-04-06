ee6899b552d3d0421221a376771b90828ce6671f
Romke van Dijk <github.com@ocbios.nl>
2023-04-06 17:21:55 +0200

    Add test for user endpoint for patching and deleting

--- ci/apiv2/test_users.py
+++ ci/apiv2/test_users.py
@@ -9,7 +9,8 @@ import unittest
 import time
 from pathlib import Path
 
-from hashtopolis import User 
+from hashtopolis import User
+from hashtopolis import GlobalPermissionGroup
 
 
 class UserTest(unittest.TestCase):
@@ -25,18 +26,96 @@ class UserTest(unittest.TestCase):
 
         obj = User.objects.get(id=user.id)
         assert obj.name == user.name
+
+        user.delete()
     
     def test_patch_user(self):
-        pass
+        stamp = int(time.time())
+        username = f'test-{stamp}'
+        groupname = f'gpg-{stamp}'
 
-    def test_hidden_user(self):
-        pass
+        user = User(
+            name = username,
+            email = 'test@example.com',
+            globalPermissionGroupId = 1
+        )
+        user.save()
+        
+        globalpermissiongroup = GlobalPermissionGroup(
+            name = groupname
+        )
+        globalpermissiongroup.save()
+
+        user.globalPermissionGroupId = globalpermissiongroup.id
+        user.save()
+
+        obj = user.objects.get(id=user.id)
+
+        assert obj.globalPermissionGroupId == globalpermissiongroup.id
+
+        user.delete()
+        globalpermissiongroup.delete()
+    
+    def test_disable_enable_user(self):
+        stamp = int(time.time())
+        username = f'test-{stamp}'
+
+        user = User(
+            name = username,
+            email = 'test@example.com',
+            globalPermissionGroupId = 1,
+        )
+        user.save()
+        id = user.id
+
+        # Test disabling user
+        user.isValid = False
+        user.save()
+
+        obj = User.objects.get(id=id)
+        assert obj.isValid == False
+
+        # Test disabling disabled user
+        user = User.objects.get(id=id)
+        user.isValid = False
+        user.save()
+
+        obj = User.objects.get(id=id)
+        assert obj.isValid == False
+
+        # Test enabling user
+        user = User.objects.get(id=id)
+        user.isValid = True
+        user.save()
+
+        obj = User.objects.get(id=id)
+        assert obj.isValid == True
+
+        # Test enabling enabled user
+        user = User.objects.get(id=id)
+        user.isValid = True
+        user.save()
+
+        obj = User.objects.get(id=id)
+        assert obj.isValid == True
+
 
     def test_delete_user(self):
-        pass
+        stamp = int(time.time())
+        username = f'test-{stamp}'
 
-    def test_exception_user(self):
-        pass
+        user = User(
+            name = username,
+            email = 'test@example.com',
+            globalPermissionGroupId = 1,
+        )
+        user.save()
+        id = user.id
+
+        user.delete()
+
+        objs = user.objects.filter(id=id)
+        assert len(objs) == 0
 
     def test_expand_user(self):
         pass
