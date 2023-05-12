5fdea64494ef9d5e4eae37d2bd3d15655b7f914c
Romke van Dijk <github.com@ocbios.nl>
2023-05-12 16:16:12 +0200

    Renaming conf.php to confv2.php

--- src/api/v2/index.php
+++ src/api/v2/index.php
@@ -113,7 +113,7 @@ $container->set("classMapper", function() {
 
 /* API token validation */
 $container->set("JwtAuthentication", function (\Psr\Container\ContainerInterface $container) {
-    include(dirname(__FILE__) . '/../../inc/conf.php');
+    include(dirname(__FILE__) . '/../../inc/confv2.php');
     return new JwtAuthentication([
         "path" => "/",
         "ignore" => ["/api/v2/auth/token", "/api/v2/ui/openapi.json"],
