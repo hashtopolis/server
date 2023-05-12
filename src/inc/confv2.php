a0c7a09ddd3a0c81c619dc39787665e0d75b7931
Romke van Dijk <github.com@ocbios.nl>
2023-05-12 11:59:44 +0200

    Moving settings to seperate php file

---
+++ src/inc/confv2.php
@@ -0,0 +1,47 @@
+<?php
+
+if (file_exists(dirname(__FILE__) . "/conf.php")) {
+  // this is either an existing setup, or a new setup without docker
+  include(dirname(__FILE__) . "/conf.php");
+  
+  // check if directories is set, otherwise set the defaults for it
+  if (!isset($DIRECTORIES)) {
+    $DIRECTORIES = [
+      "files" => dirname(__FILE__) . "/../files/",
+      "import" => dirname(__FILE__) . "/../import/",
+      "log" => dirname(__FILE__) . "/../log/",
+      "config" => dirname(__FILE__) . "/../config/"
+    ];
+  }
+  
+  // if a pepper is set from an older version, we have to save it to the new file location
+  if (isset($PEPPER) && !file_exists($DIRECTORIES['config'] . "/pepper.json")) {
+    file_put_contents($DIRECTORIES['config'] . "/pepper.json", json_encode($PEPPER));
+  }
+}
+else {
+  // read env variables (when running with docker-compose)
+  $CONN['user'] = getenv('HASHTOPOLIS_DB_USER');
+  $CONN['pass'] = getenv('HASHTOPOLIS_DB_PASS');
+  $CONN['server'] = getenv('HASHTOPOLIS_DB_HOST');
+  $CONN['db'] = getenv('HASHTOPOLIS_DB_DATABASE');
+  $CONN['port'] = 3306;
+  
+  $DIRECTORIES = [
+    "files" => "/usr/local/share/hashtopolis/files",
+    "import" => "/usr/local/share/hashtopolis/import",
+    "log" => "/usr/local/share/hashtopolis/log",
+    "config" => "/usr/local/share/hashtopolis/config/"
+  ];
+  
+  // update from env if set
+  if (getenv('HASHTOPOLIS_FILES_PATH') !== false) {
+    $DIRECTORIES["files"] = getenv('HASHTOPOLIS_FILES_PATH');
+  }
+  if (getenv('HASHTOPOLIS_IMPORT_PATH') !== false) {
+    $DIRECTORIES["import"] = getenv('HASHTOPOLIS_IMPORT_PATH');
+  }
+  if (getenv('HASHTOPOLIS_LOG_PATH') !== false) {
+    $DIRECTORIES["log"] = getenv('HASHTOPOLIS_LOG_PATH');
+  }
+}
--- src/inc/load.php
+++ src/inc/load.php
@@ -14,51 +14,7 @@ session_start();
 
 require_once(dirname(__FILE__) . "/info.php");
 
-if (file_exists(dirname(__FILE__) . "/conf.php")) {
-  // this is either an existing setup, or a new setup without docker
-  include(dirname(__FILE__) . "/conf.php");
-  
-  // check if directories is set, otherwise set the defaults for it
-  if (!isset($DIRECTORIES)) {
-    $DIRECTORIES = [
-      "files" => dirname(__FILE__) . "/../files/",
-      "import" => dirname(__FILE__) . "/../import/",
-      "log" => dirname(__FILE__) . "/../log/",
-      "config" => dirname(__FILE__) . "/../config/"
-    ];
-  }
-  
-  // if a pepper is set from an older version, we have to save it to the new file location
-  if (isset($PEPPER) && !file_exists($DIRECTORIES['config'] . "/pepper.json")) {
-    file_put_contents($DIRECTORIES['config'] . "/pepper.json", json_encode($PEPPER));
-  }
-}
-else {
-  // read env variables (when running with docker-compose)
-  $CONN['user'] = getenv('HASHTOPOLIS_DB_USER');
-  $CONN['pass'] = getenv('HASHTOPOLIS_DB_PASS');
-  $CONN['server'] = getenv('HASHTOPOLIS_DB_HOST');
-  $CONN['db'] = getenv('HASHTOPOLIS_DB_DATABASE');
-  $CONN['port'] = 3306;
-  
-  $DIRECTORIES = [
-    "files" => "/usr/local/share/hashtopolis/files",
-    "import" => "/usr/local/share/hashtopolis/import",
-    "log" => "/usr/local/share/hashtopolis/log",
-    "config" => "/usr/local/share/hashtopolis/config/"
-  ];
-  
-  // update from env if set
-  if (getenv('HASHTOPOLIS_FILES_PATH') !== false) {
-    $DIRECTORIES["files"] = getenv('HASHTOPOLIS_FILES_PATH');
-  }
-  if (getenv('HASHTOPOLIS_IMPORT_PATH') !== false) {
-    $DIRECTORIES["import"] = getenv('HASHTOPOLIS_IMPORT_PATH');
-  }
-  if (getenv('HASHTOPOLIS_LOG_PATH') !== false) {
-    $DIRECTORIES["log"] = getenv('HASHTOPOLIS_LOG_PATH');
-  }
-}
+include(dirname(__FILE__) . "/confv2.php");
 
 // include all .class.php files in inc dir
 $dir = scandir(dirname(__FILE__));
