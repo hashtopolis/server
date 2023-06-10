bdf889b7cf441fccd288ccc99b8bf54a268e0fb5
Sein Coray <s3inlc@hashes.org>
2023-06-10 11:09:12 +0200

    added initialization process in docker entrypoint to avoid user race-conditions on first page loads

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,4 +9,22 @@ for path in ${paths[@]}; do
   fi
 done
 
+echo "Testing database."
+MYSQL="mysql -u${HASHTOPOLIS_DB_USER} -p${HASHTOPOLIS_DB_PASS} -h ${HASHTOPOLIS_DB_HOST}"
+$MYSQL -e "SELECT 1"
+ERROR=$?
+
+while [ $ERROR -ne 0 ];
+do
+  echo "Failed connecting to the database. Sleeping 5s."
+  sleep 5
+  $MYSQL -e "SELECT 1"
+  ERROR=$?
+done
+
+# required to trigger the initialization
+echo "Start initialization process..."
+php -f /var/www/html/inc/load.php
+echo "Initialization complete!"
+
 docker-php-entrypoint apache2-foreground
