04b4eda3bba5ffbf33cfe6e1ca626ab64f533352
Romke van Dijk <github.com@ocbios.nl>
2023-06-23 10:26:37 +0200

    Adding tests for individual expanding per #905 Fixing failing test because devcontainer user was renamed from root to admin

--- ci/apiv2/test_expand.py
+++ ci/apiv2/test_expand.py
@@ -14,6 +14,7 @@ import sys
 from hashtopolis import Hashlist as Hashlist_v2
 from hashtopolis import Task as Task_v2
 from hashtopolis import AccessGroup as AccessGroup_v2
+from hashtopolis import HashtopolisConnector, HashtopolisConfig
 
 class Expand(unittest.TestCase):
 
@@ -47,4 +48,31 @@ class Expand(unittest.TestCase):
         objects = obj.objects.all(expand='userMembers')
 
         # Check the default account
-        assert objects[0].userMembers_set[0].name == 'root'
+        assert objects[0].userMembers_set[0].name == 'admin'
+
+
+
+    def test_individual_object_expanding(self):
+        # The individual object expanding is broken in the API.
+        # Because the normal python bindings uses filtering, this test
+        # forces the use of HTTP methods to test the expand functionality.
+
+        # Create hashlist
+        p = Path(__file__).parent.joinpath('create_hashlist_001.json')
+        payload = json.loads(p.read_text('UTF-8'))
+        hashlist_v2 = Hashlist_v2(**payload)
+        hashlist_v2.save()
+
+        config = HashtopolisConfig()
+        conn = HashtopolisConnector('/ui/hashlists', config)
+        conn.authenticate()
+        headers = conn._headers
+        uri = conn._api_endpoint + conn._model_uri
+        uri = uri + '/' + str(hashlist_v2._id) + '?expand=hashes'
+        
+        r = requests.get(uri, headers=headers, data=json.dumps({}))
+
+        assert r.json().get('hashes')[0].get('hash') == 'cc03e747a6afbbcbf8be7668acfebee5'
+
+        hashlist_v2.delete()
+
