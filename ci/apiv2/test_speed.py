39ce6ed2230297857fc9b3f82fc26b8988a005ce
Rick van der Zwet <r.van.der.zwet@nfi.nl>
2023-06-29 13:18:15 +0000

    Implements speeds API call

---
+++ ci/apiv2/test_speed.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+from basetest import BaseTest
+from hashtopolis import Speed
+
+from test_hashlists import do_create_hashlist
+from test_tasks import do_create_task
+from test_agent import do_create_agent
+
+
+class SpeedTest(BaseTest):
+    def test_speed_value(self):
+        dummy_agent, agent = do_create_agent()
+        self.delete_after_test(agent)
+
+        hashlist = do_create_hashlist()
+        self.delete_after_test(hashlist)
+
+        task = do_create_task(hashlist)
+        self.delete_after_test(task)
+
+        dummy_agent.get_task()
+        # TODO: Validate if agent is actually working on assigned task
+        dummy_agent.get_hashlist()
+
+        dummy_agent.get_chunk()
+        dummy_agent.send_keyspace()
+        dummy_agent.get_chunk()
+        dummy_agent.send_benchmark()
+        dummy_agent.get_chunk()
+        dummy_agent.send_process(progress=50)
+
+        self.assertTrue(Speed.objects.filter(taskId=task.id))
---
+++ src/inc/apiv2/speeds.routes.php
@@ -0,0 +1,67 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+use Slim\Routing\RouteCollectorProxy;
+
+use DBA\Speed;
+use DBA\Supertask;
+use DBA\Factory;
+use DBA\QueryFilter;
+use DBA\OrderFilter;
+
+require_once(dirname(__FILE__) . "/shared.inc.php");
+
+
+class SpeedAPI extends AbstractBaseAPI {
+    public static function getBaseUri(): string {
+      return "/api/v2/ui/speeds";
+    }
+
+    public static function getAvailableMethods(): array {
+      return ['GET'];
+    }
+
+    public function getPermission(): string {
+      // TODO: Find proper permission
+      return DAccessControl::CREATE_HASHLIST_ACCESS;
+    }
+
+    public static function getDBAclass(): string {
+      return Speed::class;
+    }
+
+    protected function getFactory(): object {
+      return Factory::getSpeedFactory();
+    }
+
+    public function getExpandables(): array {
+      return [ 'agent', 'task' ];
+    }
+
+    protected function getFilterACL(): array {
+      return [];
+    }
+
+    public function getFormFields(): array {
+      // TODO Form declarations in more generic class to allow auto-generated OpenAPI specifications
+      return [];
+    }
+
+    protected function checkPermission(object $object): bool
+    {
+      return true;
+    }
+    
+
+    protected function createObject($QUERY): int {
+      assert(False, "Speeds cannot be created via API");
+      return -1;
+   }
+
+   protected function deleteObject(object $object): void {
+     assert(False, "Speeds cannot be deleted via API");
+   }
+}
+
+SpeedAPI::register($app);
