0c2509b4095e398d8d8125a18b3b149015472477
jessevanzutphen7@hotmail.com <kali@kali>
2023-07-05 07:27:47 -0400

    Added update script for database update

---
+++ src/install/updates/update_v0.14.x_v0.14.0.php
@@ -0,0 +1,38 @@
+<?php /** @noinspection SqlNoDataSourceInspection */
+use DBA\Factory;
+
+if (!Util::databaseTableExists("HardwareGroup")) {
+  Factory::getAgentFactory()->getDB()->query("CREATE TABLE `HardwareGroup` (
+      `hardwareGroupId`    INT(11)      NOT NULL,
+      `devices`            VARCHAR(65000) NOT NULL,
+  ) ENGINE=InnoDB;"
+  );
+  Factory::getAgentFactory()->getDB()->query("ALTER TABLE `HardwareGroup` MODIFY `hardwareGroupId` int(11) NOT NULL AUTO_INCREMENT");
+}
+
+//change Agent table
+Factory::getAgentFactory()->getDB()->query("ALTER TABLE `Agent`
+  DROP COLUMN devices;
+");
+Factory::getAgentFactory()->getDB()->query("ALTER TABLE `Agent`
+  ADD COLUMN hardwareGroupId INT(11);
+");
+Factory::getAgentFactory()->getDB()->query("ALTER TABLE `Agent` ADD CONSTRAINT FOREIGN KEY (`hardwareGroupId`) REFERENCES `HardwareGroup` (`hardwareGroupId`);");
+
+
+if (!Util::databaseTableExists("Benchmark")) {
+  Factory::getAgentFactory()->getDB()->query("CREATE TABLE `Benchmark` (
+      `benchmarkId`         INT(11)      NOT NULL,
+      `benchmarkValue`      VARCHAR(256) NOT NULL,
+      `hardwareGroupId`      INT(11) NOT NULL,
+      `attackParameters`     VARCHAR(256) NOT NULL,
+      `ttl`                  INT(11) NULL,
+      `hashMode`             INT(11) NULL,
+      `benchmarkType`       VARCHAR(10) NULL,
+    ) ENGINE=InnoDB;"
+  );
+  Factory::getAgentFactory()->getDB()->query("ALTER TABLE `Benchmark` MODIFY `benchmarkId` int(11) NOT NULL AUTO_INCREMENT");
+}
+Factory::getAgentFactory()->getDB()->query("ALTER TABLE `Benchmark` ADD CONSTRAINT FOREIGN KEY (`hardwareGroupId`) REFERENCES `HardwareGroup` (`hardwareGroupId`);");
+
+?>
