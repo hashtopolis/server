a1e8b7bdd433f578f976e4f96a5efa6f0c88e72b
jessevanzutphen7@hotmail.com <kali@kali>
2023-07-09 12:09:14 -0400

    updated the update script

--- src/install/updates/update_v0.14.x_v0.14.0.php
+++ src/install/updates/update_v0.14.x_v0.14.0.php
@@ -4,10 +4,11 @@ use DBA\Factory;
 if (!Util::databaseTableExists("HardwareGroup")) {
   Factory::getAgentFactory()->getDB()->query("CREATE TABLE `HardwareGroup` (
       `hardwareGroupId`    INT(11)      NOT NULL,
-      `devices`            VARCHAR(65000) NOT NULL,
+      `devices`            VARCHAR(65000) NOT NULL
   ) ENGINE=InnoDB;"
   );
-  Factory::getAgentFactory()->getDB()->query("ALTER TABLE `HardwareGroup` MODIFY `hardwareGroupId` int(11) NOT NULL AUTO_INCREMENT");
+  Factory::getAgentFactory()->getDB()->query("ALTER TABLE `HardwareGroup` MODIFY `hardwareGroupId` int(11) NOT NULL AUTO_INCREMENT;");
+  Factory::getAgentFactory()->getDB()->query("ALTER TABLE `HardwareGroup` ADD PRIMARY KEY (`hardwareGroupId`);");
 }
 
 //change Agent table
@@ -25,14 +26,27 @@ if (!Util::databaseTableExists("Benchmark")) {
       `benchmarkId`         INT(11)      NOT NULL,
       `benchmarkValue`      VARCHAR(256) NOT NULL,
       `hardwareGroupId`      INT(11) NOT NULL,
-      `attackParameters`     VARCHAR(256) NOT NULL,
+      `attackParameters`     VARCHAR(512) NOT NULL,
       `ttl`                  INT(11) NULL,
       `hashMode`             INT(11) NULL,
-      `benchmarkType`       VARCHAR(10) NULL,
+      `benchmarkType`       VARCHAR(10) NULL
     ) ENGINE=InnoDB;"
   );
-  Factory::getAgentFactory()->getDB()->query("ALTER TABLE `Benchmark` MODIFY `benchmarkId` int(11) NOT NULL AUTO_INCREMENT");
+
+  Factory::getAgentFactory()->getDB()->query("ALTER TABLE `Benchmark` MODIFY `benchmarkId` int(11) NOT NULL AUTO_INCREMENT;");
+
+  Factory::getAgentFactory()->getDB()->query("
+    ALTER TABLE `Benchmark`
+    ADD PRIMARY KEY (`benchmarkId`),
+    ADD KEY `hardwareGroupId` (`hardwareGroupId`);");
+
+  Factory::getAgentFactory()->getDB()->query("ALTER TABLE `Benchmark`
+  ADD CONSTRAINT `Benchmark_ibfk_1` FOREIGN KEY (`hardwareGroupId`) REFERENCES `HardwareGroup` (`hardwareGroupId`);"); 
 }
-Factory::getAgentFactory()->getDB()->query("ALTER TABLE `Benchmark` ADD CONSTRAINT FOREIGN KEY (`hardwareGroupId`) REFERENCES `HardwareGroup` (`hardwareGroupId`);");
+
+Factory::getAgentFactory()->getDB()->query("ALTER TABLE `Agent` ADD CONSTRAINT `Agent_ibfk_2` 
+      FOREIGN KEY (`hardwareGroupId`) REFERENCES `HardwareGroup` (`hardwareGroupId`);");
+Factory::getAgentFactory()->getDB()->query("ALTER TABLE `Benchmark` ADD CONSTRAINT `Benchmark_ibfk_1` 
+      FOREIGN KEY (`hardwareGroupId`) REFERENCES `HardwareGroup` (`hardwareGroupId`);");
 
 ?>
