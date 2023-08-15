ce027523f56c466574ed5d43e2269a139c79c3be
Rick van der Zwet <r.van.der.zwet@nfi.nl>
2023-08-15 11:12:21 +0000

    Re-generate all auto-generated models

--- src/dba/models/HashType.class.php
+++ src/dba/models/HashType.class.php
@@ -27,7 +27,7 @@ class HashType extends AbstractModel {
   
   static function getFeatures() {
     $dict = array();
-    $dict['hashTypeId'] = ['read_only' => True, "type" => "int", "subtype" => "unset", "null" => False, "pk" => True, "protected" => True, "private" => False, "alias" => "hashTypeId"];
+    $dict['hashTypeId'] = ['read_only' => True, "type" => "int", "subtype" => "unset", "null" => False, "pk" => True, "protected" => False, "private" => False, "alias" => "hashTypeId"];
     $dict['description'] = ['read_only' => False, "type" => "str(256)", "subtype" => "unset", "null" => False, "pk" => False, "protected" => False, "private" => False, "alias" => "description"];
     $dict['isSalted'] = ['read_only' => False, "type" => "bool", "subtype" => "unset", "null" => False, "pk" => False, "protected" => False, "private" => False, "alias" => "isSalted"];
     $dict['isSlowHash'] = ['read_only' => False, "type" => "bool", "subtype" => "unset", "null" => False, "pk" => False, "protected" => False, "private" => False, "alias" => "isSlowHash"];
