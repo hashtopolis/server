d5a3920d285ce45eea736355856237d1a71ea0bb
Rick van der Zwet <r.van.der.zwet@nfi.nl>
2023-08-15 11:10:46 +0200

    Fix test_tasks leaving test models behind
    Re-factor to use the garbage collector. While here also cleanup
    assert statements to use the unittest style instead

--- ci/apiv2/test_tasks.py
+++ ci/apiv2/test_tasks.py
@@ -5,177 +5,114 @@
 # Written in python to work on creation of hashtopolis APIv2 python binding.
 #
 import json
-import unittest
 import datetime
 from pathlib import Path
 
-from hashtopolis import Hashlist
+from utils import BaseTest
 from hashtopolis import Task
-from hashtopolis import HashtopolisConnector, HashtopolisConfig
-
-import requests
 
 from test_hashlists import do_create_hashlist
 
 
-def do_create_task(hashlist):
-    p = Path(__file__).parent.joinpath('create_task_001.json')
+def do_create_task(hashlist, file_id='001'):
+    p = Path(__file__).parent.joinpath(f'create_task_{file_id}.json')
     payload = json.loads(p.read_text('UTF-8'))
-    payload['hashlistId'] = int(hashlist._id)
+    payload['hashlistId'] = int(hashlist.id)
     obj = Task(**payload)
     obj.save()
     return obj
 
 
-class TaskTest(unittest.TestCase):
+class TaskTest(BaseTest):
     def test_create_task(self):
         hashlist = do_create_hashlist()
-        obj = do_create_task(hashlist)
+        self.delete_after_test(hashlist)
 
-        obj.delete()
-        hashlist.delete()
+        task = do_create_task(hashlist)
+        self.delete_after_test(task)
 
     def test_expand_hashlists(self):
-        p = Path(__file__).parent.joinpath('create_hashlist_001.json')
-        payload = json.loads(p.read_text('UTF-8'))
-        hashlist = Hashlist(**payload)
-        hashlist.save()
+        hashlist = do_create_hashlist()
+        self.delete_after_test(hashlist)
 
-        for p in sorted(Path(__file__).parent.glob('create_task_001.json')):
-            payload = json.loads(p.read_text('UTF-8'))
-            payload['hashlistId'] = int(hashlist._id)
-            obj = Task(**payload)
-            obj.save()
+        task = do_create_task(hashlist)
+        self.delete_after_test(task)
 
-        obj_test = Task().objects.filter(taskId=obj.id, expand='hashlist')[0]
-        assert obj_test.hashlist_set.name == hashlist.name
-
-        obj.delete()
-        hashlist.delete()
+        obj_test = Task().objects.filter(taskId=task.id, expand='hashlist')[0]
+        self.assertEqual(obj_test.hashlist_set.name, hashlist.name)
 
     def test_patch(self):
-        p = Path(__file__).parent.joinpath('create_hashlist_001.json')
-        payload = json.loads(p.read_text('UTF-8'))
-        hashlist = Hashlist(**payload)
-        hashlist.save()
+        hashlist = do_create_hashlist()
+        self.delete_after_test(hashlist)
 
-        for p in sorted(Path(__file__).parent.glob('create_task_001.json')):
-            payload = json.loads(p.read_text('UTF-8'))
-            payload['hashlistId'] = int(hashlist._id)
-            task = Task(**payload)
-            task.save()
+        task = do_create_task(hashlist)
+        self.delete_after_test(task)
 
         stamp = datetime.datetime.now().isoformat()
 
-        obj = Task.objects.get_first()
-        obj.taskName = f'Dummy Task - {stamp}'
-        obj.save()
-
-        obj = Task.objects.get_first()
-
-        self.assertEqual(obj.taskName, f'Dummy Task - {stamp}')
-
-    def test_patch_color_null(self):
-        p = Path(__file__).parent.joinpath('create_hashlist_001.json')
-        payload = json.loads(p.read_text('UTF-8'))
-        hashlist = Hashlist(**payload)
-        hashlist.save()
-
-        for p in sorted(Path(__file__).parent.glob('create_task_001.json')):
-            payload = json.loads(p.read_text('UTF-8'))
-            payload['hashlistId'] = int(hashlist._id)
-            task = Task(**payload)
-            task.save()
-
-        config = HashtopolisConfig()
-        conn = HashtopolisConnector(f'/ui/tasks/{task.id}', config)
-        conn.authenticate()
-        uri = conn._api_endpoint + conn._model_uri
-        headers = conn._headers
-        payload = {"color": None}
-        r = requests.patch(uri, headers=headers, data=json.dumps(payload))
-
-        assert r.status_code == 201
+        task_name = f'Dummy Task - {stamp}'
+        task.taskName = task_name
+        task.save()
 
         obj = Task.objects.get(taskId=task.id)
+        self.assertEqual(obj.taskName, task_name)
 
-        assert obj.color == ''
+    def test_patch_color_null(self):
+        hashlist = do_create_hashlist()
+        self.delete_after_test(hashlist)
 
-        hashlist.delete()
+        task = do_create_task(hashlist)
+        self.delete_after_test(task)
+
+        task.color = None
+        task.save()
+
+        obj = Task.objects.get(taskId=task.id)
+        self.assertEqual(obj.color, '')
 
     def test_runtime(self):
-        p = Path(__file__).parent.joinpath('create_hashlist_001.json')
-        payload = json.loads(p.read_text('UTF-8'))
-        hashlist = Hashlist(**payload)
-        hashlist.save()
+        hashlist = do_create_hashlist()
+        self.delete_after_test(hashlist)
 
-        p = Path(__file__).parent.joinpath('create_task_002.json')
-        payload = json.loads(p.read_text('UTF-8'))
-        payload['hashlistId'] = int(hashlist._id)
-        task = Task(**payload)
-        task.save()
+        task = do_create_task(hashlist, '002')
+        self.delete_after_test(task)
 
         obj = Task.objects.get(taskId=task.id)
         self.assertEqual(obj.useNewBench, 0)
 
-        obj.delete()
-        hashlist.delete()
-
     def test_speed(self):
-        p = Path(__file__).parent.joinpath('create_hashlist_001.json')
-        payload = json.loads(p.read_text('UTF-8'))
-        hashlist = Hashlist(**payload)
-        hashlist.save()
+        hashlist = do_create_hashlist()
+        self.delete_after_test(hashlist)
 
-        p = Path(__file__).parent.joinpath('create_task_001.json')
-        payload = json.loads(p.read_text('UTF-8'))
-        payload['hashlistId'] = int(hashlist._id)
-        task = Task(**payload)
-        task.save()
+        task = do_create_task(hashlist, '001')
+        self.delete_after_test(task)
 
         obj = Task.objects.get(taskId=task.id)
         self.assertEqual(obj.useNewBench, 1)
 
-        obj.delete()
-        hashlist.delete()
-
     def test_preprocessor(self):
-        p = Path(__file__).parent.joinpath('create_hashlist_001.json')
-        payload = json.loads(p.read_text('UTF-8'))
-        hashlist = Hashlist(**payload)
-        hashlist.save()
+        hashlist = do_create_hashlist()
+        self.delete_after_test(hashlist)
 
-        p = Path(__file__).parent.joinpath('create_task_003.json')
-        payload = json.loads(p.read_text('UTF-8'))
-        payload['hashlistId'] = int(hashlist._id)
-        task = Task(**payload)
-        task.save()
+        task = do_create_task(hashlist, '001')
+        self.delete_after_test(task)
 
-        id = task.id
-
-        obj = Task.objects.get(taskId=id)
+        obj = Task.objects.get(taskId=task.id)
         self.assertEqual(obj.preprocessorCommand, "this-is-prepressor")
         self.assertEqual(obj.preprocessorId, 1)
 
-        obj.delete()
-        hashlist.delete()
-
-
     def test_archive_filter(self):
         hashlist = do_create_hashlist()
-        obj = do_create_task(hashlist)
+        self.delete_after_test(hashlist)
 
-        obj.isArchived = True
-        obj.save()
+        task = do_create_task(hashlist, '001')
+        self.delete_after_test(task)
+
+        task.isArchived = True
+        task.save()
 
         # Use filter to get archived tasks
-        test_obj = Task.objects.filter(taskId=obj.id, isArchived=True)
+        test_obj = Task.objects.filter(taskId=task.id, isArchived=True)
 
-        assert len(test_obj) == 1
-        assert test_obj[0].isArchived == True
-
-        obj.delete()
-        hashlist.delete()
-
-if __name__ == '__main__':
-    unittest.main()
+        self.assertEqual(len(test_obj), 1)
+        self.assertTrue(test_obj[0].isArchived)
