3849695e9c9013a04c64b29a5b3619f13b34aa46
jessevanzutphen7@hotmail.com <kali@kali>
2023-08-23 03:08:49 -0400

    cleaned up benchmark handler

--- src/inc/handlers/BenchmarkHandler.class.php
+++ src/inc/handlers/BenchmarkHandler.class.php
@@ -1,28 +1,15 @@
 <?php
-
-use DBA\Factory;
-
-class BenchmarkHandler implements Handler {
-  private $agent;
-  
-  public function __construct($agentId = null) {
-    if ($agentId == null) {
-      $this->agent = null;
-      return;
-    }
-    
-    $this->agent = Factory::getAgentFactory()->get($agentId);
-    if ($this->agent == null) {
-      UI::printError("FATAL", "Agent with ID $agentId not found!");
-    }
+class BenchmarkHandler implements Handler {  
+  public function __construct($benchmarkId = null) {
+        //we need nothing to load
   }
+  
 
   public function handle($action) {
     try {
       switch ($action) {
         case DBenchmarkAction::DELETE_BENCHMARK:
           AccessControl::getInstance()->checkPermission(DBenchmarkAction::DELETE_BENCHMARK);
-          $benchId = $_POST['benchmarkId'];
           BenchmarkUtils::delete($_POST['benchmarkId']);
           break;
         default:
