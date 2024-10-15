e589417307b20e577f61a4bffc3340d4a73d3359
gluafamichl <>
2024-10-15 15:03:49 +0200

    mod: change DB field from mediumtext (16MB) to text (64kB)

--- src/install/updates/update_v0.14.x_v0.14.3.php
+++ src/install/updates/update_v0.14.x_v0.14.3.php
@@ -18,8 +18,8 @@ if (!isset($PRESENT["v0.14.x_agentBinaries"])) {
 
 
 if (!isset($PRESENT["v0.14.x_attackCmd"])) {
-  Factory::getAgentFactory()->getDB()->query("ALTER TABLE `Task` MODIFY `attackCmd` mediumtext;");
-  Factory::getAgentFactory()->getDB()->query("ALTER TABLE `Pretask` MODIFY `attackCmd` mediumtext;");
-  Factory::getAgentFactory()->getDB()->query("ALTER TABLE `HealthCheck` MODIFY `attackCmd` mediumtext;");
+  Factory::getAgentFactory()->getDB()->query("ALTER TABLE `Task` MODIFY `attackCmd` TEXT NOT NULL;");
+  Factory::getAgentFactory()->getDB()->query("ALTER TABLE `Pretask` MODIFY `attackCmd` TEXT NOT NULL;");
+  Factory::getAgentFactory()->getDB()->query("ALTER TABLE `HealthCheck` MODIFY `attackCmd` TEXT NOT NULL;");
   $EXECUTED["v0.14.x_attackCmd"] = true;
 }
