f80542a219b4fcbea24259429d248c8fb7696796
Sein Coray <s3inlc@hashes.org>
2024-11-14 14:58:58 +0100

    added release tag for testing

--- ci/HashtopolisTest.class.php
+++ ci/HashtopolisTest.class.php
@@ -35,7 +35,8 @@ abstract class HashtopolisTest {
     "0.13.1" => "fc4b5226c1d36dc0197f4d17d216298972055c09",
     "0.14.0" => "0af7193310c9c1f37a274578e159ab3ae0a6caad",
     "0.14.1" => "375f2ce022c4b3e0780abf9dcca1e6af8e966c1a",
-    "0.14.2" => "d397e4b8ec1d2591b93fa44c8660edc314401da5"
+    "0.14.2" => "d397e4b8ec1d2591b93fa44c8660edc314401da5",
+    "0.14.3" => "878b6db5f05075e68e1f89cc94efaa33d98b9f8c"
   ];
   
   public function initAndUpgrade($fromVersion) {
