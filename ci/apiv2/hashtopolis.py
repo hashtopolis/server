fd5ba50dc6c4d95f03700c46dddd9bff4845ec54
jessevz <jessevanzutphen7@hotmail.com>
2024-11-21 12:03:04 +0100

    Bug/999 download file via new api (#1130)
    * FEAT added getFile helper endpoint to download files in new API
    
    * FEAT add etag support on getFile
    
    * cleaned up getFile helper
    
    * FEAT added test for getFile helper
    
    * FEAT added tests for Range request in getFile helper
    
    * Fixed code style suggestion in ci/apiv2/hashtopolis.py

--- ci/apiv2/hashtopolis.py
+++ ci/apiv2/hashtopolis.py
@@ -818,6 +818,22 @@ class Helper(HashtopolisConnector):
         else:
             return self.resp_to_json(r)
 
+    def _helper_get_request_file(self, helper_uri, payload, range=None):
+        self.authenticate()
+        uri = self._api_endpoint + self._model_uri + helper_uri
+        headers = self._headers
+        if range:
+            headers["Range"] = range
+
+        logging.debug(f"Sending GET request to {uri}, with params:{payload}")
+        r = requests.get(uri, headers=headers, params=payload)
+        if range is None:
+            assert r.status_code == 200
+        else:
+            assert r.status_code == 206
+        logging.debug(f"received file contents: \n {r.text}")
+        return r.text
+
     def _test_authentication(self, username, password):
         auth_uri = self._api_endpoint + '/auth/token'
         auth = (username, password)
@@ -898,6 +914,11 @@ class Helper(HashtopolisConnector):
         response = self._helper_request("importCrackedHashes", payload)
         return response['data']
 
+    def get_file(self, file, range=None):
+        payload = {
+            'file': file.id
+        }
+        return self._helper_get_request_file("getFile", payload, range)
 
     def recount_file_lines(self, file):
         payload = {
