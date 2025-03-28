cd9f5b65ae36d98981fecbf3a6b9123167a77d9b
Sein Coray <sein@coray.org>
2025-03-28 12:24:04 +0100

    fix fatal issue on update script

--- src/install/updates/update_v0.14.3_v0.14.4.php
+++ src/install/updates/update_v0.14.3_v0.14.4.php
@@ -4,22 +4,6 @@ use DBA\Config;
 use DBA\Factory;
 use DBA\QueryFilter;
 
-if (!isset($PRESENT["v0.14.3_pagination"])) {
-  $qF = new QueryFilter(Config::ITEM, DConfig::DEFAULT_PAGE_SIZE, "=");
-  $item = Factory::getConfigFactory()->filter([Factory::FILTER => $qF], true);
-  if (!$item) {
-    $config = new Config(null, 3, DConfig::DEFAULT_PAGE_SIZE, '10000');
-    Factory::getConfigFactory()->save($config);
-  }
-  $qF = new QueryFilter(Config::ITEM, DConfig::MAX_PAGE_SIZE, "=");
-  $item = Factory::getConfigFactory()->filter([Factory::FILTER => $qF], true);
-  if (!$item) {
-    $config = new Config(null, 3, DConfig::MAX_PAGE_SIZE, '50000');
-    Factory::getConfigFactory()->save($config);
-  }
-  $EXECUTED["v0.14.3_pagination"] = true;
-}
-
 if (!isset($PRESENT["v0.14.3_agentBinaries"])) {
   Util::checkAgentVersion("python", "0.7.3", true);
   $EXECUTED["v0.14.3_agentBinaries"] = true;
