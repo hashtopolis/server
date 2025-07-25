b05476b688bd517acf2bd5689977b2360342bc10
jessevz <jesse.van.zutphen@nfi.nl>
2025-07-25 08:31:58 +0200

    Undone comment

--- src/api/v2/index.php
+++ src/api/v2/index.php
@@ -248,7 +248,7 @@ $customErrorHandler = function (
   
   return errorResponse($response, $exception->getMessage(), $code);
 };
-// $errorMiddleware->setDefaultErrorHandler($customErrorHandler);
+$errorMiddleware->setDefaultErrorHandler($customErrorHandler);
 $app->addRoutingMiddleware(); //Routing middleware has to be added after the default error handler
 $errorMiddlewareMethodNotAllowed = $app->addErrorMiddleware(true, true, true);
 $errorMiddlewareMethodNotAllowed->setErrorHandler(HttpMethodNotAllowedException::class, function (
