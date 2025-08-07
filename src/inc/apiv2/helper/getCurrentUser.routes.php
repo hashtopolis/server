38cb70ea2823e85cd831cb711f04571482264561
jessevz <jesse.van.zutphen@nfi.nl>
2025-08-07 17:24:23 +0200

    Created helper endpoint to retrieve current logged in user, regardless of permissions

---
+++ src/inc/apiv2/helper/getCurrentUser.routes.php
@@ -0,0 +1,75 @@
+<?php
+
+use JetBrains\PhpStorm\NoReturn;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+require_once(dirname(__FILE__) . "/../common/AbstractHelperAPI.class.php");
+
+class getCurrentUserHelperAPI extends AbstractHelperAPI {
+  public static function getBaseUri(): string {
+    return "/api/v2/helper/getCurrentUser";
+  }
+  
+  public static function getAvailableMethods(): array {
+    return ['GET'];
+  }
+  
+  public function getRequiredPermissions(string $method): array {
+    return [];
+  }
+  
+  public function getFormFields(): array {
+    return [];
+  }
+  
+  /**
+   * @throws NotFoundExceptionInterface
+   * @throws HttpForbidden
+   * @throws ContainerExceptionInterface
+   * @throws HTException
+   * @throws JsonException
+   */
+  public function handleGet(Request $request, Response $response): Response {
+    $this->preCommon($request);
+    $user = $this->getCurrentUser();
+    $userResource = self::obj2Resource($user);
+    
+    $ret = self::createJsonResponse(data: $userResource);
+    
+    $body = $response->getBody();
+    $body->write($this->ret2json($ret));
+    
+    return $response->withStatus(200)
+      ->withHeader("Content-Type", 'application/vnd.api+json;');
+  }
+  
+  /**
+   * @param $data
+   * @return object|array|null
+   */
+  #[NoReturn] public function actionPost($data): object|array|null {
+    assert(False, "GetCurrentUser has no POST");
+  }
+  
+  static public function register($app): void {
+    $baseUri = getCurrentUserHelperAPI::getBaseUri();
+    
+    /* Allow CORS preflight requests */
+    $app->options($baseUri, function (Request $request, Response $response): Response {
+      return $response;
+    });
+    $app->get($baseUri, "getCurrentUserHelperAPI:handleGet");
+  }
+  
+  /**
+   * getAccessGroups is different because it returns via another function
+   */
+  public static function getResponse(): array|string|null {
+    return null;
+  }
+}
+
+getCurrentUserHelperAPI::register($app);
