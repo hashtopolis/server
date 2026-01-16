7a4a4a1107b1e7d5ea524d43e77c0c089116e9ce
s3inlc <sein@coray.org>
2026-01-16 11:38:44 +0100

    fixed postgres migration

--- src/migrations/postgres/20260116102500_bigint-keys-stats.sql
+++ src/migrations/postgres/20260116102500_bigint-keys-stats.sql
@@ -1,3 +1,8 @@
-ALTER TABLE AgentStat ALTER COLUMN agentStatId TYPE BIGSERIAL USING agentStatId::bigint;
-ALTER TABLE Speed ALTER COLUMN speedId TYPE BIGSERIAL USING speedId::bigint;
-ALTER TABLE LogEntry ALTER COLUMN logEntryId TYPE BIGSERIAL USING logEntryId::bigint;
+ALTER TABLE AgentStat ALTER COLUMN agentStatId TYPE BIGINT USING agentStatId::bigint;
+ALTER SEQUENCE agentstat_agentstatid_seq AS BIGINT;
+
+ALTER TABLE Speed ALTER COLUMN speedId TYPE BIGINT USING speedId::bigint;
+ALTER SEQUENCE speed_speedid_seq AS BIGINT;
+
+ALTER TABLE LogEntry ALTER COLUMN logEntryId TYPE BIGINT USING logEntryId::bigint;
+ALTER SEQUENCE logentry_logentryid_seq AS BIGINT;
