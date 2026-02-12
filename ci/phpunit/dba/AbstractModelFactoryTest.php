cc3c5f579666fdae501fc7256ff1bc8d3388ba2d
s3inlc <sein@coray.org>
2026-02-12 14:40:19 +0100

    added test for columnFilter function

--- ci/phpunit/dba/AbstractModelFactoryTest.php
+++ ci/phpunit/dba/AbstractModelFactoryTest.php
@@ -1,6 +1,8 @@
 <?php
 namespace Tests\DBA;
 
+use DBA\Hashlist;
+use DBA\OrderFilter;
 use Exception;
 use PHPUnit\Framework\TestCase;
 use DBA\Factory;
@@ -33,5 +35,33 @@ final class AbstractModelFactoryTest extends TestCase {
     
     $user = Factory::getUserFactory()->filter([Factory::FILTER => $qF]);
     $this->assertSame([], $user);
+  }
+  
+  /**
+   * Tests the columnFilter function which returns an array of values of the given column of matching rows
+   *
+   * @return void
+   */
+  public function testColumnFilter(): void {
+    // add some data
+    $hashlist_1 = new Hashlist(null, "hashlist 1", \DHashlistFormat::PLAIN, 0, 0, ':', 0, 0, 0, 0, \AccessUtils::getOrCreateDefaultAccessGroup()->getId(), "", 0, 0, 0);
+    $hashlist_2 = new Hashlist(null, "hashlist 2", \DHashlistFormat::PLAIN, 0, 0, ':', 0, 0, 0, 1, \AccessUtils::getOrCreateDefaultAccessGroup()->getId(), "", 0, 0, 0);
+    $hashlist_3 = new Hashlist(null, "hashlist 3", \DHashlistFormat::PLAIN, 0, 0, ':', 0, 0, 0, 0, \AccessUtils::getOrCreateDefaultAccessGroup()->getId(), "", 0, 0, 0);
+    $hashlist_1 = Factory::getHashlistFactory()->save($hashlist_1);
+    $hashlist_2 = Factory::getHashlistFactory()->save($hashlist_2);
+    $hashlist_3 = Factory::getHashlistFactory()->save($hashlist_3);
+    
+    $oF = new OrderFilter(Hashlist::HASHLIST_ID, "ASC");
+    
+    // test column filter to retrieve some of their IDs
+    $qF = new QueryFilter(Hashlist::IS_SALTED, 0, "=");
+    $ids = Factory::getHashlistFactory()->columnFilter([Factory::FILTER => $qF, Factory::ORDER => $oF], Hashlist::HASHLIST_ID);
+    
+    // hashlist 1 and 3 should be returned
+    $this->assertSame([$hashlist_1->getId(), $hashlist_3->getId()], $ids);
+    
+    $qF = new QueryFilter(Hashlist::CRACKED, 0, ">");
+    $ids = Factory::getHashlistFactory()->columnFilter([Factory::FILTER => $qF, Factory::ORDER => $oF], Hashlist::HASHLIST_ID);
+    $this->assertSame([], $ids);
   }
 }
