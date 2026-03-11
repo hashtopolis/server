6f737d57efd2261f08337f03eb68244a7132475e
jessevz <jesse.van.zutphen@nfi.nl>
2026-03-11 14:21:15 +0100

    Fixed sql migrations

--- src/migrations/mysql/20260309164000_api-key.sql
+++ src/migrations/mysql/20260309164000_api-key.sql
@@ -1,6 +1,11 @@
 CREATE TABLE JwtApiKey (
-    JwtApiKeyId SERIAL PRIMARY KEY,
+    JwtApiKeyId INT NOT NULL,
     userId INTEGER REFERENCES htp_User(id),
     startValid bigint,
     endValid bigint,
-    isRevoked BOOLEAN DEFAULT FALSE);
+    isRevoked BOOLEAN DEFAULT FALSE,
+    PRIMARY KEY (`JwtApiKeyId`),
+    KEY `idx_JwtApiKey_userId` (`userId`),
+    CONSTRAINT `fk_JwtApiKey_user`
+        FOREIGN KEY (`userId`) REFERENCES `htp_User`(`userId`)
+);
--- src/migrations/postgres/20260309164000_api-key.sql
+++ src/migrations/postgres/20260309164000_api-key.sql
@@ -1,6 +1,11 @@
 CREATE TABLE JwtApiKey (
-    JwtApiKeyId SERIAL PRIMARY KEY,
+    JwtApiKeyId SERIAL NOT NULL PRIMARY KEY,
     userId INTEGER REFERENCES htp_User(id),
     startValid bigint,
     endValid bigint,
-    isRevoked BOOLEAN DEFAULT FALSE);
+    isRevoked BOOLEAN DEFAULT FALSE,
+    CONSTRAINT fk_JwtApiKey_user
+        FOREIGN KEY (userId) REFERENCES htp_User(id),
+    CONSTRAINT idx_JwtApiKey_userId
+        INDEX (userId)
+);
