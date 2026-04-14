5f77a3a7a95751f553a03b58a38631d106d9f21d
jessevz <jesse.van.zutphen@nfi.nl>
2026-04-14 10:16:49 +0200

    Made sql keywords to uppercase

--- src/migrations/mysql/20260413140000_task-view.sql
+++ src/migrations/mysql/20260413140000_task-view.sql
@@ -1,10 +1,10 @@
-create view TaskWrapperDisplay as select 
-    tw.taskWrapperId as taskWrapperId, tw.priority as taskWrapperPriority, tw.maxAgents as taskWrapperMaxAgents,
-    tw.taskType as taskType, tw.hashlistId as hashlistId, tw.accessGroupId as accessGroupId,
-    tw.taskWrapperName as taskWrapperName, tw.isArchived as taskWrapperIsArchived, tw.cracked as cracked,
-    t.taskId as taskId, t.taskName as taskName, t.attackCmd as attackCmd, t.chunkTime as chunkTime,
-    t.statusTimer as statusTimer, t.keyspace as keyspace, t.keyspaceProgress as keyspaceProgress,
-    t.priority as taskPriority, t.maxAgents as taskMaxAgents, t.isArchived as taskIsArchived,
-    t.isSmall as isSmall, t.isCpuTask as isCpuTask, t.usePreprocessor as taskUsePreprocessor,
-    CASE WHEN tw.taskType = 0 THEN t.taskName ELSE tw.taskWrapperName END as displayName
-FROM TaskWrapper tw LEFT JOIN Task t on tw.taskType = 0 AND t.taskWrapperId = tw.taskWrapperId;
+CREATE VIEW TaskWrapperDisplay AS SELECT 
+    tw.taskWrapperId AS taskWrapperId, tw.priority AS taskWrapperPriority, tw.maxAgents AS taskWrapperMaxAgents,
+    tw.taskType AS taskType, tw.hashlistId AS hashlistId, tw.accessGroupId AS accessGroupId,
+    tw.taskWrapperName AS taskWrapperName, tw.isArchived AS taskWrapperIsArchived, tw.cracked AS cracked,
+    t.taskId AS taskId, t.taskName AS taskName, t.attackCmd AS attackCmd, t.chunkTime AS chunkTime,
+    t.statusTimer AS statusTimer, t.keyspace AS keyspace, t.keyspaceProgress AS keyspaceProgress,
+    t.priority AS taskPriority, t.maxAgents AS taskMaxAgents, t.isArchived AS taskIsArchived,
+    t.isSmall AS isSmall, t.isCpuTask AS isCpuTask, t.usePreprocessor AS taskUsePreprocessor,
+    CASE WHEN tw.taskType = 0 THEN t.taskName ELSE tw.taskWrapperName END AS displayName
+FROM TaskWrapper tw LEFT JOIN Task t ON tw.taskType = 0 AND t.taskWrapperId = tw.taskWrapperId;
