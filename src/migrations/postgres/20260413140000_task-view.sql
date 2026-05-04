c082535d4adc9539f5adb7bb25a5f65f9fb0cccb
inadequate777 <Devalue9255@proton.me>
2026-05-04 09:45:58 +0000

    updated color fields for takwrapper display

--- src/migrations/postgres/20260413140000_task-view.sql
+++ src/migrations/postgres/20260413140000_task-view.sql
@@ -2,7 +2,7 @@ CREATE VIEW TaskWrapperDisplay AS SELECT
     tw.taskWrapperId AS taskWrapperId, tw.priority AS taskWrapperPriority, tw.maxAgents AS taskWrapperMaxAgents,
     tw.taskType AS taskType, tw.hashlistId AS hashlistId, tw.accessGroupId AS accessGroupId,
     tw.taskWrapperName AS taskWrapperName, tw.isArchived AS taskWrapperIsArchived, tw.cracked AS cracked,
-    t.taskId AS taskId, t.taskName AS taskName, t.attackCmd AS attackCmd, t.chunkTime AS chunkTime,
+    t.taskId AS taskId, t.taskName AS taskName, t.color AS color, t.attackCmd AS attackCmd, t.chunkTime AS chunkTime,
     t.statusTimer AS statusTimer, t.keyspace AS keyspace, t.keyspaceProgress AS keyspaceProgress,
     t.priority AS taskPriority, t.maxAgents AS taskMaxAgents, t.isArchived AS taskIsArchived,
     t.isSmall AS isSmall, t.isCpuTask AS isCpuTask, t.usePreprocessor AS taskUsePreprocessor,
