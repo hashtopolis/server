50f640d3bc912c972b36b9cd3ed673bd76936ef6
inadequate777 <Devalue9255@proton.me>
2026-05-07 11:49:29 +0000

    create test object for future tests

--- ci/apiv2/test_taskwrapperdisplay.py
+++ ci/apiv2/test_taskwrapperdisplay.py
@@ -3,11 +3,16 @@ from utils import BaseTest
 class TaskWrapperDisplayTest(BaseTest):
     model_class = TaskWrapperDisplay
     
-    def test_task_wrapper_display_should_return_color_field(self):
+    def create_test_object(self, *nargs, delete=True, **kwargs):
+        # Always cleanup hashlist when done, this is potentially confusing,
+        # since it will also remove the related task
         hashlist = self.create_hashlist()
-        task = self.create_task(hashlist)
-        expected_color_value = str(task.color)
-        task_wrapper_Display_object = TaskWrapperDisplay.objects.get(pk=task.taskWrapperId)
-        self.assertIsNotNone(task_wrapper_Display_object.color)
-        self.assertEqual(task_wrapper_Display_object.color, expected_color_value)
-        self.assertNotEqual("FFFFFF", task_wrapper_Display_object.color)
+        task = self.create_task(hashlist, delete=delete)
+        return TaskWrapperDisplay.objects.get(pk=task.taskWrapperId)
+    
+    def test_task_wrapper_display_should_return_color_field(self):
+        task_wrapper_display_object = self.create_test_object()
+        expected_color_value = str(task_wrapper_display_object.color)
+        self.assertIsNotNone(task_wrapper_display_object.color)
+        self.assertEqual(task_wrapper_display_object.color, expected_color_value)
+        self.assertNotEqual("FFFFFF", task_wrapper_display_object.color)
