605a92646aa7ce9a624c23f81a4bb170dd1f06a8
inadequate777 <Devalue9255@proton.me>
2026-05-07 11:44:13 +0000

    pk instad of id

--- ci/apiv2/test_taskwrapperdisplay.py
+++ ci/apiv2/test_taskwrapperdisplay.py
@@ -7,7 +7,7 @@ class TaskWrapperDisplayTest(BaseTest):
         hashlist = self.create_hashlist()
         task = self.create_task(hashlist)
         expected_color_value = str(task.color)
-        task_wrapper_Display_object = TaskWrapperDisplay.objects.get(id=task.taskWrapperId)
+        task_wrapper_Display_object = TaskWrapperDisplay.objects.get(pk=task.taskWrapperId)
         self.assertIsNotNone(task_wrapper_Display_object.color)
         self.assertEqual(task_wrapper_Display_object.color, expected_color_value)
         self.assertNotEqual("FFFFFF", task_wrapper_Display_object.color)
