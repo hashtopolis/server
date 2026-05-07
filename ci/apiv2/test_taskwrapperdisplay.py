dd881b7f6070826f7c35bfaa4fdd3ea6bed39d4d
inadequate777 <Devalue9255@proton.me>
2026-05-07 09:18:40 +0000

    new slimmed down  test

--- ci/apiv2/test_taskwrapperdisplay.py
+++ ci/apiv2/test_taskwrapperdisplay.py
@@ -1,28 +1,13 @@
-from hashtopolis import TaskWrapperDisplay, TaskWrapperDisplayHelper
+from hashtopolis import TaskWrapperDisplay
 from utils import BaseTest
-
-
 class TaskWrapperDisplayTest(BaseTest):
     model_class = TaskWrapperDisplay
-    def test_toast(self):
+    
+    def test_task_wrapper_display_should_return_color_field(self):
         hashlist = self.create_hashlist()
         task = self.create_task(hashlist)
         expected_color_value = str(task.color)
-        display = TaskWrapperDisplay.objects.get(id=task.taskWrapperId)
-        self.assertEqual(display.color, expected_color_value)
-
-    def test_taskwrapperdisplays_returns_color_field(self):
-        hashlist = self.create_hashlist()
-        task = self.create_task(hashlist)
-        values = TaskWrapperDisplayHelper().get_task_wrapper_display()
-        data_items = values.get('data') or []
-        expected_id = str(task.taskWrapperId)
-        color_value = None
-        expected_color_value = str(task.color)
-        for item in data_items:
-            if str(item.get('id')) == expected_id:
-                color_value = item.get('attributes', {}).get('color')
-                break
-        self.assertIsNotNone(color_value)
-        self.assertEqual(expected_color_value, color_value)
-        self.assertNotEqual("ff0000", color_value)
+        task_wrapper_Display_object = TaskWrapperDisplay.objects.get(id=task.taskWrapperId)
+        self.assertIsNotNone(task_wrapper_Display_object.color)
+        self.assertEqual(task_wrapper_Display_object.color, expected_color_value)
+        self.assertNotEqual("ff0000", task_wrapper_Display_object.color)
