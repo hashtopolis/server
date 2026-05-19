d382243cd3096ed91b62a43a34c04f6eff3b6bf7
Sein Coray <s3inlc@hashes.org>
2026-05-19 13:48:28 +0200

    Phpunit Tests refactoring (#2091)
    * restructured namespaces to be psr-4 compatible and moved mocks to root dir of test sources
    
    * rewrite of the testbase class to allow some support in cleaning up database objects

--- ci/phpunit/inc/utils/UserUtilsTest.php
+++ ci/phpunit/inc/utils/UserUtilsTest.php
@@ -1,75 +1,31 @@
 <?php
 
-namespace Hashtopolis\inc\templating {
-  class Template {
-    private string $name;
-
-    public function __construct(string $name) {
-      $this->name = $name;
-    }
-
-    public function render($data): string {
-      return $this->name . ':' . json_encode($data);
-    }
-  }
-}
-
-namespace Tests\Inc\Utils {
+namespace inc\utils;
 
 use Hashtopolis\dba\Factory;
-use Hashtopolis\dba\QueryFilter;
-use Hashtopolis\dba\models\AccessGroupUser;
 use Hashtopolis\dba\models\RightGroup;
 use Hashtopolis\dba\models\User;
+use Hashtopolis\dba\QueryFilter;
 use Hashtopolis\inc\utils\UserUtils;
 use Hashtopolis\inc\apiv2\error\InternalError;
-use PHPUnit\Framework\TestCase;
+use TestBase;
 
-require_once(dirname(__FILE__) . '/../TestMocks.php');
+require_once(dirname(__FILE__) . '/../../TestBase.php');
 require_once(dirname(__FILE__) . '/../../../../src/inc/startup/include.php');
 
-final class UserUtilsTest extends TestCase {
-  /** @var string[] */
-  private array $createdUsernames = [];
-
-  /** @var int[] */
-  private array $createdRightGroupIds = [];
-
+final class UserUtilsTest extends TestBase {
   protected function setUp(): void {
     parent::setUp();
-
+    
     $_SERVER['HTTP_HOST'] = $_SERVER['HTTP_HOST'] ?? 'localhost';
     $_SERVER['SERVER_PORT'] = $_SERVER['SERVER_PORT'] ?? 80;
     \hashtopolis_clear_test_mocks();
   }
-
-  protected function tearDown(): void {
-    foreach ($this->createdUsernames as $username) {
-      $qF = new QueryFilter(User::USERNAME, $username, '=');
-      $users = Factory::getUserFactory()->filter([Factory::FILTER => $qF]);
-      foreach ($users as $user) {
-        $memberFilter = new QueryFilter(AccessGroupUser::USER_ID, $user->getId(), '=');
-        Factory::getAccessGroupUserFactory()->massDeletion([Factory::FILTER => $memberFilter]);
-        Factory::getUserFactory()->delete($user);
-      }
-    }
-
-    foreach ($this->createdRightGroupIds as $rightGroupId) {
-      $group = Factory::getRightGroupFactory()->get($rightGroupId);
-      if ($group !== null) {
-        Factory::getRightGroupFactory()->delete($group);
-      }
-    }
-
-    \hashtopolis_clear_test_mocks();
-
-    parent::tearDown();
-  }
-
+  
   public function testCreateUserDoesNotCallSendMailWhenMailIsNotConfigured(): void {
     $mailCallCount = 0;
     $username = $this->uniqueUsername('mail_disabled');
-
+    
     \hashtopolis_set_test_mock('Hashtopolis\\inc\\is_file', static function ($path): bool {
       return false;
     });
@@ -77,18 +33,18 @@ final class UserUtilsTest extends TestCase {
       $mailCallCount++;
       return true;
     });
-
-    $createdUser = UserUtils::createUser($username, $username . '@example.com', $this->createRightGroup()->getId(), $this->createAdminUser());
-    $this->createdUsernames[] = $username;
-
+    
+    $createdUser = UserUtils::createUser($username, $username . '@example.com', $this->createRightGroup()->getId(), $this->adminUser);
+    $this->registerDatabaseObject(Factory::getUserFactory(), $createdUser);
+    
     $this->assertSame($username, $createdUser->getUsername());
     $this->assertSame(0, $mailCallCount);
   }
-
+  
   public function testCreateUserCallsSendMailWhenMailIsConfigured(): void {
     $mailCalls = [];
     $username = $this->uniqueUsername('mail_enabled');
-
+    
     \hashtopolis_set_test_mock('Hashtopolis\\inc\\is_file', static function ($path): bool {
       return true;
     });
@@ -96,19 +52,19 @@ final class UserUtilsTest extends TestCase {
       $mailCalls[] = [$to, $subject, $message, $additionalHeaders, $additionalParams];
       return true;
     });
-
-    UserUtils::createUser($username, $username . '@example.com', $this->createRightGroup()->getId(), $this->createAdminUser());
-    $this->createdUsernames[] = $username;
-
+    
+    $createdUser = UserUtils::createUser($username, $username . '@example.com', $this->createRightGroup()->getId(), $this->adminUser);
+    $this->registerDatabaseObject(Factory::getUserFactory(), $createdUser);
+    
     $this->assertCount(1, $mailCalls);
     $this->assertSame($username . '@example.com', $mailCalls[0][0]);
     $this->assertSame('Account at ' . APP_NAME, $mailCalls[0][1]);
   }
-
+  
   public function testCreateUserThrowsWhenConfiguredSendMailFails(): void {
     $mailCallCount = 0;
     $username = $this->uniqueUsername('mail_failure');
-
+    
     \hashtopolis_set_test_mock('Hashtopolis\\inc\\is_file', static function ($path): bool {
       return true;
     });
@@ -116,29 +72,25 @@ final class UserUtilsTest extends TestCase {
       $mailCallCount++;
       return false;
     });
-
-    $this->createdUsernames[] = $username;
     
     $this->expectException(InternalError::class);
     try {
-      UserUtils::createUser($username, $username . '@example.com', $this->createRightGroup()->getId(), $this->createAdminUser());
-    } finally {
+      $createdUser = UserUtils::createUser($username, $username . '@example.com', $this->createRightGroup()->getId(), $this->adminUser);
+      $this->registerDatabaseObject(Factory::getUserFactory(), $createdUser);
+    }
+    finally {
       $this->assertSame(1, $mailCallCount);
+      $this->registerDatabaseObject(Factory::getUserFactory(), Factory::getUserFactory()->filter([Factory::FILTER => new QueryFilter(User::USERNAME, $username, "=")], true));
     }
   }
-
-  private function createAdminUser(): User {
-    return new User(1, 'admin', 'admin@example.com', 'hash', 'salt', 1, 0, 0, time(), 3600, 1, '', '', '', '', '');
-  }
-
+  
   private function createRightGroup(): RightGroup {
-    $group = Factory::getRightGroupFactory()->save(new RightGroup(null, 'phpunit-' . uniqid('', true), '[]'));
-    $this->createdRightGroupIds[] = $group->getId();
+    $group = $this->createDatabaseObject(Factory::getRightGroupFactory(), new RightGroup(null, 'phpunit-' . uniqid('', true), '[]'));
+    $this->assertTrue($group instanceof RightGroup);
     return $group;
   }
-
+  
   private function uniqueUsername(string $prefix): string {
     return $prefix . '_' . uniqid();
   }
-}
 }
