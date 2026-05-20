41ece2cbf276770dd0f4f805e5cf8b0efe7a32fb
jessevz <jessevanzutphen7@hotmail.com>
2026-05-20 09:29:20 +0200

    2019 enhancement backend endpoint for hash heatmap (#2068)
    * Added a backend helper for the cracks heathmap
    
    * moved timeseries filter into abstractmodelfactory and created unittest for it
    
    * fix inconsistency between generator and models
    
    ---------
    
    Co-authored-by: jessevz <jesse.van.zutphen@nfi.nl>
    Co-authored-by: s3inlc <sein@coray.org>

--- ci/phpunit/dba/AbstractModelFactoryTest.php
+++ ci/phpunit/dba/AbstractModelFactoryTest.php
@@ -2,6 +2,8 @@
 
 namespace dba;
 
+use Hashtopolis\dba\models\Hash;
+use Hashtopolis\inc\utils\AccessGroupUtils;
 use TestBase;
 use Hashtopolis\dba\models\Hashlist;
 use Hashtopolis\dba\OrderFilter;
@@ -63,5 +65,66 @@ final class AbstractModelFactoryTest extends TestBase {
     $qF = new QueryFilter(Hashlist::CRACKED, 0, ">");
     $ids = Factory::getHashlistFactory()->columnFilter([Factory::FILTER => $qF, Factory::ORDER => $oF], Hashlist::HASHLIST_ID);
     $this->assertSame([], $ids);
+  }
+  
+  public function testTimeseriesFilterEmpty(): void {
+    $counts = Factory::getHashFactory()->columnTimeseriesFilter([], Hash::TIME_CRACKED);
+    
+    $this->assertSame([], $counts);
+  }
+  
+  public function testTimeseriesFilterNoneCracked(): void {
+    $timeLimit = time() - 3600*24*30; // one month back
+    
+    $hashlist = $this->createDatabaseObject(Factory::getHashlistFactory(), new Hashlist(null, 'hashlist', DHashlistFormat::PLAIN, 0, 100, ':', 0, 0, 0, 0, 1, '', 0, 0, 0));
+    $hashTemplate = new Hash(null, $hashlist->getId(), 'hash', 'salt', '', 0, null, 0, 0);
+    
+    for($i = 0; $i < 1000; $i++) {
+      $this->createDatabaseObject(Factory::getHashFactory(), clone $hashTemplate);
+    }
+    
+    $qF1 = new QueryFilter(Hash::IS_CRACKED, 1, "=");
+    $qF2 = new QueryFilter(Hash::TIME_CRACKED, $timeLimit, ">");
+    $counts = Factory::getHashFactory()->columnTimeseriesFilter([Factory::FILTER => [$qF1, $qF2]], Hash::TIME_CRACKED);
+    
+    $this->assertSame([], $counts);
+  }
+  
+  public function testTimeseriesFilter(): void {
+    $timeLimit = time() - 3600*24*30; // one month back
+    
+    $hashlist = $this->createDatabaseObject(Factory::getHashlistFactory(), new Hashlist(null, 'hashlist', DHashlistFormat::PLAIN, 0, 100, ':', 0, 0, 0, 0, 1, '', 0, 0, 0));
+    $hashTemplate = new Hash(null, $hashlist->getId(), 'hash', 'salt', 'plaintext', 0, null, 1, 0);
+    
+    $hashes = [];
+    for($i = 0, $j = 0; $i < 1000; $i++) {
+      $hash = clone $hashTemplate;
+      $hash->setTimeCracked($timeLimit + $i - 10 + $j * 3600 * 24); // 10 hashes will fall out of the tested timeseries range
+      $hash->setIsCracked(($i % 10) > 0); // every tenth hash is not cracked
+      $hashes[] = $this->createDatabaseObject(Factory::getHashFactory(), $hash);
+      if ($i % 23 == 0){
+        $j++;
+      }
+    }
+    
+    $qF1 = new QueryFilter(Hash::IS_CRACKED, 1, "=");
+    $qF2 = new QueryFilter(Hash::TIME_CRACKED, $timeLimit, ">");
+    $counts = Factory::getHashFactory()->columnTimeseriesFilter([Factory::FILTER => [$qF1, $qF2]], Hash::TIME_CRACKED);
+    
+    // build the array on our own to compare
+    $expected = [];
+    foreach($hashes as $hash) {
+      if($hash->getisCracked() != 1) {
+        continue;
+      }
+      $day = date('Y-m-d', $hash->getTimeCracked());
+      if(!isset($expected[$day])){
+        $expected[$day] = 0;
+      }
+      $expected[$day]++;
+    }
+    
+    $this->assertEquals(array_sum($expected), array_sum($counts));
+    $this->assertSame($expected, $counts);
   }
 }
---
+++ src/inc/apiv2/helper/GetCracksPerDayHelperAPI.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Hashtopolis\inc\apiv2\helper;
+
+use Exception;
+use Hashtopolis\inc\apiv2\common\AbstractHelperAPI;
+use Hashtopolis\inc\apiv2\error\HttpError;
+use Hashtopolis\dba\Factory;
+use Hashtopolis\dba\models\Hash;
+use Hashtopolis\dba\models\HashBinary;
+use Hashtopolis\dba\models\Hashlist;
+use Hashtopolis\dba\QueryFilter;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use stdClass;
+
+use function PHPUnit\Framework\isEmpty;
+
+class GetCracksPerDayHelperAPI extends AbstractHelperAPI {
+  public static function getBaseUri(): string {
+    return "/api/v2/helper/getCracksPerDay";
+  }
+
+  public static function getAvailableMethods(): array {
+    return ['GET'];
+  }
+
+  public function getRequiredPermissions(string $method): array {
+    return [Hashlist::PERM_READ, Hash::PERM_READ];
+  }
+
+  public static function getResponse(): null {
+    return null;
+  }
+
+  public function actionPost(array $data): object|array|null {
+    throw new HttpError("getCracksPerDay has no POST");
+  }
+
+  public function getParamsSwagger(): array {
+    return [];
+  }
+  
+  /**
+   * Returns a map of date -> crack count for days with at least one crack from
+   * January 1st of the current year up to and including today. Days with no
+   * cracks are omitted from the response.
+   * @throws Exception
+   */
+  public function handleGet(Request $request, Response $response): Response {
+    $this->preCommon($request);
+
+    $yearStart = mktime(0, 0, 0, 1, 1, (int) date('Y'));
+    $qF1 = new QueryFilter(Hash::IS_CRACKED, 1, "=");
+    $qF2 = new QueryFilter(Hash::TIME_CRACKED, $yearStart, ">");
+    $counts = Factory::getHashFactory()->columnTimeseriesFilter([Factory::FILTER => [$qF1, $qF2]], Hash::TIME_CRACKED);
+    $counts2 = Factory::getHashBinaryFactory()->columnTimeseriesFilter([Factory::FILTER => [$qF1, $qF2]], Hash::TIME_CRACKED);
+    foreach ($counts2 as $key => $value) {
+      $counts[$key] = ($counts[$key] ?? 0) + $value;
+    }
+    
+    $ret = self::createJsonResponse(meta: $counts);
+    if(empty($counts)) {
+      $ret["meta"] = new stdClass();
+    }
+    
+    $body = $response->getBody();
+    $body->write($this->ret2json($ret));
+
+    return $response->withStatus(200)
+      ->withHeader("Content-Type", 'application/vnd.api+json;');
+  }
+
+  public static function register($app): void {
+    $baseUri = self::getBaseUri();
+
+    $app->options($baseUri, function (Request $request, Response $response): Response {
+      return $response;
+    });
+    $app->get($baseUri, self::class . ":handleGet");
+  }
+}
