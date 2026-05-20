e91166ed719c3944af26379ab05725fdf5f54686
s3inlc <sein@coray.org>
2026-05-20 15:12:09 +0200

    do not change the type to avoid migration problems

--- src/migrations/mysql/20260520145000_mysql-autoincrement.sql
+++ src/migrations/mysql/20260520145000_mysql-autoincrement.sql
@@ -1,5 +1,5 @@
--- Make that HashtType also uses AUTO_INCREMENT to be consistent with the SERIAL we have in postgres.
+-- Make that HashType also uses AUTO_INCREMENT to be consistent with the SERIAL we have in postgres.
 -- But it is set to higher than you would ever have in hc modes to avoid collisions.
 ALTER TABLE `HashType`
-    MODIFY `hashTypeId` AUTO_INCREMENT,
+    MODIFY `hashTypeId` INT(11) NOT NULL AUTO_INCREMENT,
     AUTO_INCREMENT = 100000;
