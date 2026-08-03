55ff3dca0be29cac5234d3fc106229941d20f801
Sein Coray <s3inlc@hashes.org>
2026-08-03 17:39:07 +0200

    added test for a public attribute

--- ci/apiv2/test_permissions.py
+++ ci/apiv2/test_permissions.py
@@ -18,3 +18,23 @@ class PermissionsTest(BaseTest):
         )
         self.assertEqual(denied_response.status_code, 403, denied_response.text)
         self.assertIn('permAgentRead', denied_response.text)
+
+    def test_api_token_user_read_scope_public_attributes(self):
+        allowed_token = self.create_apitoken(extra_payload={'scopes': ['permUserRead']})
+        allowed_response = request_with_api_token(
+            allowed_token.token,
+            '/ui/users?page[size]=1',
+        )
+        self.assertEqual(allowed_response.status_code, 200, allowed_response.text)
+        allowed_attributes = allowed_response.json()['data'][0]['attributes']
+        self.assertIn('name', allowed_attributes)
+        self.assertIn('email', allowed_attributes)
+
+        public_only_token = self.create_apitoken(extra_payload={'scopes': ['permHashlistRead']})
+        public_only_response = request_with_api_token(
+            public_only_token.token,
+            '/ui/users?page[size]=1',
+        )
+        self.assertEqual(public_only_response.status_code, 200, public_only_response.text)
+        public_only_attributes = public_only_response.json()['data'][0]['attributes']
+        self.assertEqual(set(public_only_attributes), {'name'})
