b61589a9b9d85e2671ca4dc6a3e5a86f9f984681
Sein Coray <s3inlc@hashes.org>
2026-09-03 16:04:06 +0200

    adding POST functionality to upload cracker binary

--- src/inc/apiv2/model/CrackerBinaryAPI.php
+++ src/inc/apiv2/model/CrackerBinaryAPI.php
@@ -10,6 +10,8 @@ use Hashtopolis\dba\models\CrackerBinaryType;
 use Hashtopolis\dba\models\Task;
 use Hashtopolis\inc\apiv2\common\AbstractModelAPI;
 use Hashtopolis\inc\apiv2\error\HttpError;
+use Hashtopolis\inc\apiv2\error\HttpForbidden;
+use Hashtopolis\inc\apiv2\error\ResourceNotFoundError;
 use Hashtopolis\inc\HTException;
 
 
@@ -20,9 +22,26 @@ class CrackerBinaryAPI extends AbstractModelAPI {
   public static function getBaseUri(): string {
     return "/api/v2/ui/crackers";
   }
-  
+
   public static function getDBAclass(): string {
     return CrackerBinary::class;
+  }
+
+  /**
+   * Extra fields which are valid for creation of object. With one of the source
+   * fields given, the archive of the cracker binary is uploaded to the server
+   * instead of referencing it with an external download url.
+   */
+  public function getFormFields(): array {
+    return [
+      "sourceType" => ['type' => 'str', 'null' => True,
+        'choices' => [
+          "inline" => "Archive provided as base64 data in sourceData",
+          "import" => "Archive taken from the import directory, sourceData is the filename",
+          "url" => "Archive fetched from an http(s) url given in sourceData"
+        ]],
+      "sourceData" => ['type' => 'str', 'null' => True]
+    ];
   }
   
   public static function getToOneRelationships(): array {
@@ -52,6 +71,25 @@ class CrackerBinaryAPI extends AbstractModelAPI {
    * @throws HTException
    */
   protected function createObject(array $data): int {
+    if (isset($data["sourceType"])) {
+      if (isset($data[CrackerBinary::DOWNLOAD_URL])) {
+        throw new HttpError("downloadUrl cannot be provided when the archive is uploaded with sourceType!");
+      }
+      if (!isset($data["sourceData"])) {
+        throw new HttpError("sourceData is required when sourceType is provided!");
+      }
+      $binary = CrackerUtils::createBinaryFromUpload(
+        $data[CrackerBinary::VERSION],
+        $data[CrackerBinary::BINARY_NAME],
+        $data[CrackerBinary::CRACKER_BINARY_TYPE_ID],
+        $data["sourceType"],
+        $data["sourceData"]
+      );
+      return $binary->getId();
+    }
+    if (!isset($data[CrackerBinary::DOWNLOAD_URL]) || strlen($data[CrackerBinary::DOWNLOAD_URL]) == 0) {
+      throw new HttpError("Please provide all information!");
+    }
     $binary = CrackerUtils::createBinary(
       $data[CrackerBinary::VERSION],
       $data[CrackerBinary::BINARY_NAME],
@@ -60,12 +98,33 @@ class CrackerBinaryAPI extends AbstractModelAPI {
     );
     return $binary->getId();
   }
-  
+
   /**
    * @param CrackerBinary $object
    * @throws HTException
    */
   protected function deleteObject(AbstractModel $object): void {
     CrackerUtils::deleteBinary($object->getId());
+  }
+
+  /**
+   * The download url of locally stored binaries is owned by the server, so it
+   * cannot be overwritten with a patch.
+   *
+   * @param int $objectId
+   * @param array $data
+   * @throws HttpError
+   * @throws HttpForbidden
+   * @throws ResourceNotFoundError
+   * @throws HTException
+   */
+  protected function updateObject(int $objectId, array $data): void {
+    if (array_key_exists(CrackerBinary::DOWNLOAD_URL, $data)) {
+      $binary = CrackerUtils::getBinary($objectId);
+      if ($binary->getFilename() !== null) {
+        throw new HttpError("The download url of a locally stored cracker binary cannot be changed!");
+      }
+    }
+    parent::updateObject($objectId, $data);
   }
 }
